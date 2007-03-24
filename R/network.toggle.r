05b37c57899930dee79971020c7fe4f0b3bc0989
Mark S. Handcock <handcock@stat.ucla.edu>
2007-03-24 19:58:51 +0000

    Fixed typos. Tried to decrease memory use in network.toggle
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet/trunk@63 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/network.toggle.r
+++ R/network.toggle.r
@@ -26,3 +26,31 @@ network.toggle<-function(nw,nws,timestep=NULL)
    nw
   }
 }
+network.accumulate<-function(nw,nws,timestep=NULL)
+{
+  if(is.null(timestep)){
+   if(is.network(nws)){
+    nws <- as.matrix.network(nws,"edgelist")
+   }else{
+    matrix.type <- which.matrix.type(nws)
+    if(nrow(nws)==0){matrix.type <- "edgelist"}
+    if(matrix.type!="edgelist"){ 
+      stop("network.accumulate requires an edgelist or a network series")
+    }
+   }
+  }else{
+   if(class(nws)!="network.series"){ 
+      stop("network.accumulate requires an edgelist or a network series")
+   }
+   timesteps <- nws$changed[,1,drop=FALSE]
+   nws <- nws$changed[timesteps==timestep,2:3,drop=FALSE]
+  }
+  if(nrow(nws) > 0){
+#  for(i in 1:nrow(nws)){  
+#   nw[nws[i,1],nws[i,2]] <- 1-nw[nws[i,1],nws[i,2]] 
+#  }
+   accumulate.edges(nw,tail=nws[,1],head=nws[,2])
+  }else{
+   nw
+  }
+}
