c01610711e8f01062624ab87fcd61e5056480dfc
David R. Hunter <dhunter@stat.psu.edu>
2008-03-03 20:28:38 +0000

    Added three necessary aliases to man/ergm-internal.Rd
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@705 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/mple_largenetwork.R
+++ tests/mple_largenetwork.R
@@ -1,8 +1,10 @@
 library(ergm)
 
+# Note:  n can be made larger if a more stringent test is desired
+
 # First, a test for sparse networks with n edges, so the
 # MLE should be log(n / (n*(n-1)-n)) or log(n) - 2*log(n-1)                 
-for(n in 5000) { # n must be even
+for(n in 500) { # n must be even
   m <- cbind(1:n,n:1) #n must be even here
   e <- ergm(network(m, directed=TRUE)~edges)
   eta <- log(n) - 2*log(n-1)  # What the coefficient estimate should be
@@ -16,7 +18,7 @@ for(n in 5000) { # n must be even
 # node has n/100 (out)edges.  Thus the total number of edges is
 # n^2/100, so the MLE is log(n^2/100) - log(n(n-1)-n^2/100),
 # or 2*log(n) - log(99n^2-100n) = log(n) - log(99n-100)
-for(n in 5000) { # n should be a multiple of 100
+for(n in 500) { # n should be a multiple of 100
   s <- sample(n-1, n/100)
   m <- cbind(rep(1:n,each=n/100), rep(s, n))
   m[,2] <- m[,2] + apply(m,1,function(x) x[1]<=x[2])
