d6eec16afbc0600b6bf332082b2f936ca5799462
Mark S. Handcock <handcock@stat.ucla.edu>
2008-05-10 22:12:18 +0000

    Converted initial 'development' version import to 2.1 release version using hand redaction. This is not exactly the version in JSS but very close.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/2.1@795 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/edgetree.h
+++ src/edgetree.h
@@ -1,3 +1,19 @@
+/*
+ *  File ergm/src/edgeTree.h
+ *  Part of the statnet package, http://statnetproject.org
+ *
+ *  This software is distributed under the GPL-3 license.  It is free,
+ *  open source, and has the attribution requirements (GPL Section 7) in
+ *    http://statnetproject.org/attribution
+ *
+ * Copyright 2003 Mark S. Handcock, University of Washington
+ *                David R. Hunter, Penn State University
+ *                Carter T. Butts, University of California - Irvine
+ *                Steven M. Goodreau, University of Washington
+ *                Martina Morris, University of Washington
+ * Copyright 2007 The statnet Development Team
+ */
+
 #ifndef EDGETREE_H
 #define EDGETREE_H
 
@@ -116,12 +132,3 @@ Edge DesignMissing (Vertex a, Vertex b, Network *mnwp);
 Edge EdgeTree2EdgeList(Vertex *heads, Vertex *tails, Network *nwp, Edge nmax);
 
 #endif
-
-
-
-
-
-
-
-
-
