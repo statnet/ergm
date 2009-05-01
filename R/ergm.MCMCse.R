ee9db838bb568dfeda3e639c1613a15d6fbb445d
Mark S. Handcock <handcock@stat.ucla.edu>
2009-05-01 13:21:09 +0000

    Fixed typo in prior fix
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@1001 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.MCMCse.R
+++ R/ergm.MCMCse.R
@@ -119,7 +119,7 @@ ergm.MCMCse<-function(theta, theta0, statsmatrix, statsmatrix.miss,
        hessian0 <- - V
      }else{
 #      hessian0 <-  V.miss-V
-       hessian0 <- robust.inverse(var(xsim[,!novar]))+robust.inverse(var(xsim.miss[,!novar]))
+       hessian0 <- -robust.inverse(var(xsim[,!novar]))-robust.inverse(var(xsim.miss[,!novar]))
 #      hessian0 <- -var(xsim[,!novar])+var(xsim.miss[,!novar])
      }
     }else{
