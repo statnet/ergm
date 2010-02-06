5ec8801c77cf4b9375fcbab9fea41ab54a4d0fb8
David R. Hunter <dhunter@stat.psu.edu>
2010-02-06 18:21:20 +0000

    removed deprecated function ergm.mcmcslave after checking that no existing function calls it
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/2.2@10806 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.getMCMCsample.R
+++ R/ergm.getMCMCsample.R
@@ -147,39 +147,3 @@ ergm.getMCMCsample <- function(Clist, MHproposal, eta0, MCMCparams, verbose=FALS
   return(list(statsmatrix = statsmatrix, newedgelist = newedgelist))
 }
 
-
-# ergm.mcmcslave should now be unnecessary; the ergm.getMCMCsample function
-# now takes its place (as of version 2.2-3)
-
-# Function the slaves will call to perform a validation on the
-# mcmc equal to their slave number.
-# Assumes: Clist MHproposal eta0 MCMCparams maxedges verbose
-ergm.mcmcslave <- function(Clist,MHproposal,eta0,MCMCparams,maxedges,verbose) {
-  warning("Using deprecated function ergm.mcmcslave!")
-  z <- .C("MCMC_wrapper",
-  as.integer(Clist$heads), as.integer(Clist$tails),
-  as.integer(Clist$nedges), as.integer(Clist$maxpossibleedges), as.integer(Clist$n),
-  as.integer(Clist$dir), as.integer(Clist$bipartite),
-  as.integer(Clist$nterms),
-  as.character(Clist$fnamestring),
-  as.character(Clist$snamestring),
-  as.character(MHproposal$name), as.character(MHproposal$package),
-  as.double(Clist$inputs), as.double(eta0),
-  as.integer(MCMCparams$samplesize),
-#  s = as.double(t(MCMCparams$stats)),
-  s = double(MCMCparams$samplesize * length(MCMCparams$stats)),
-  as.integer(MCMCparams$burnin), 
-  as.integer(MCMCparams$interval),
-  newnwheads = integer(maxedges),
-  newnwtails = integer(maxedges),
-  as.integer(verbose), as.integer(MHproposal$bd$attribs),
-  as.integer(MHproposal$bd$maxout), as.integer(MHproposal$bd$maxin),
-  as.integer(MHproposal$bd$minout), as.integer(MHproposal$bd$minin),
-  as.integer(MHproposal$bd$condAllDegExact), as.integer(length(MHproposal$bd$attribs)),
-  as.integer(maxedges),
-  as.integer(MCMCparams$Clist.miss$heads), as.integer(MCMCparams$Clist.miss$tails),
-  as.integer(MCMCparams$Clist.miss$nedges),
-  PACKAGE="ergm")
-  # save the results
-  list(s=z$s, newnwheads=z$newnwheads, newnwtails=z$newnwtails)
-}
