71b14321cebc2f653052d0328380c3f1a68f88b3
Pavel N. Krivitsky <pavel@uow.edu.au>
2010-04-18 00:41:17 +0000

    Made the MPLE meanstats test possible to pass.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@10863 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/mple_meanstats.R
+++ tests/mple_meanstats.R
@@ -1,6 +1,19 @@
 library(ergm)
-n<-20
+library(coda)
+n<-500
 base.net <- network.initialize(n=n,directed=FALSE)
-n*c(1,.2,.4)
-ergm.fit<-ergm(base.net~edges+degree(c(0,1)),meanstats=n*c(1,.2,.4))
+norm.stats<-c(.7,.1,.5)
+meanstats<-norm.stats*n
+print(meanstats)
+cat("Structural check:")
+cat("Mean degree:", norm.stats[1]*2,".\n")
+cat("Average degree among nodes with degree 2 or higher:", (2*norm.stats[1]-norm.stats[3])/(1-norm.stats[2]-norm.stats[3]),".\n")
+
+ergm.fit<-ergm(base.net~edges+degree(c(0,1)),meanstats=n*norm.stats)
 summary(ergm.fit)
+ergm.sim<-simulate(ergm.fit,nsim=1000,statsonly=TRUE)
+
+meanstats.sim<-apply(ergm.sim,2,mean)
+print(meanstats.sim)
+print(effectiveSize(mcmc(ergm.sim)))
+print((meanstats.sim-meanstats)/sqrt(apply(ergm.sim,2,var)/effectiveSize(mcmc(ergm.sim))))
