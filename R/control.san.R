f74a5c5ee09a8b42ca1c88fc88fae1d26ebbac5d
David R. Hunter <dhunter@stat.psu.edu>
2010-04-21 18:54:14 +0000

    tagged CRAN version 2.2-4
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/tags/2.2-4@10868 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/control.san.R
+++ R/control.san.R
@@ -1,6 +1,5 @@
-control.san<-function(prop.weights="default",prop.args=NULL,
+control.san<-function(prop.weights="default", prop.args=NULL,
                       drop=FALSE,
-                      summarizestats=FALSE,
                       maxchanges=1000000){
   control<-list()
   for(arg in names(formals(sys.function())))
@@ -8,12 +7,4 @@ control.san<-function(prop.weights="default",prop.args=NULL,
   control
 }
 
-control.san.ergm<-function(prop.weights=NULL,prop.args=NULL,
-                           drop=FALSE,
-                           summarizestats=FALSE,
-                           maxchanges=1000000){
-  control<-list()
-  for(arg in names(formals(sys.function())))
-    control[[arg]]<-get(arg)
-  control
-}
+
