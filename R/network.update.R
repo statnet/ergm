d2a84da971e11012666f074047ddec537a0a0872
David R. Hunter <dhunter@stat.psu.edu>
2010-07-19 11:12:55 +0000

    added as.network.uncompressed and as.edgelist compressed functions from trunk; fixed codoc mismatch in simulate.ergm.Rd
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/2.2@10882 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/network.update.R
+++ R/network.update.R
@@ -1,7 +1,7 @@
 network.update<-function(nw,newmatrix,matrix.type=NULL,output="network")
 {
 #  print(paste("old:",network.edgecount(nw)," new:", nrow(newmatrix),collapse=" "))
-  unw <- network.copy(nw)
+  unw <- network.copy(as.network.uncompressed(nw))
   if(is.null(matrix.type)){
     warning("Don't leave matrix type to chance! Pass matrix.type to network.update!")
     matrix.type <- which.matrix.type(newmatrix)
@@ -25,5 +25,68 @@ network.update<-function(nw,newmatrix,matrix.type=NULL,output="network")
     add.edges(unw,head=newmatrix[,2],tail=newmatrix[,1])
    }
   }
+  if(output=="as.edgelist.compressed") unw <- as.edgelist.compressed(unw)
   unw
+}
+#Force the input into edgelist form.  Network size, directedness, and vertex
+#names are stored as attributes, since they cannot otherwise be included
+#A copy of as.edgelist.sna
+as.edgelist.compressed<-function(x, attrname=NULL, force.bipartite=FALSE){
+  #In case of lists, process independently
+  if(is.list(x)&&(!(class(x)%in%c("network"))))
+    return(lapply(x,as.edgelist.compressed, attrname=attrname, force.bipartite=force.bipartite))
+  #Begin with network objects
+  if(class(x)=="network"){
+    require("network")  #Must have network library to process network objects
+    out<-as.matrix.network.edgelist(x,attrname=attrname)
+#   if(!is.directed(x)){
+#    out <- out[1:(nrow(x)/2),]
+#   }
+    if(NCOL(out)==2)                        #If needed, add edge values
+      out<-cbind(out,rep(1,NROW(out)))
+    attr(out,"n")<-network.size(x)
+    attr(out,"directed")<-is.directed(x)
+    attr(out,"vnames")<-network.vertex.names(x)
+    van<-list.vertex.attributes(x)
+    if(length(van)>0){
+     va <- vector(mode = "list", length(van))
+     for (i in (1:length(van))){ 
+      va[[i]]<-get.vertex.attribute(x,van[i],unlist=TRUE)
+     }
+     names(va)<-van
+     attr(out,"vertex.attributes")<-va
+    }
+    if(is.bipartite(x))
+      attr(out,"bipartite")<-get.network.attribute(x,"bipartite")
+    else if(force.bipartite)
+      out<-as.edgelist.compressed(out,attrname=attrname,force.bipartite=force.bipartite)
+  }else{
+    warning("as.edgelist.compressed input must be network, or list thereof.\n Returning the original object.\n")
+    return(x)
+  }
+  #Return the result
+  out
+}
+as.network.uncompressed<-function(x, 
+        na.rm=FALSE, edge.check=FALSE){
+  #Initialize the network object
+  if(class(x)=="network"){return(x)}
+  if(is.null(attr(x,"vnames"))){
+   warning("as.network.uncompressed input must be a compressed network, or a network.\n Returning the original object.\n")
+   return(x)
+  }
+  n<-attr(x,"n")
+  directed<-attr(x,"directed")
+  g<-network.initialize(n,directed=directed)
+  #Call the specific coercion routine, depending on matrix type
+# g<-network.edgelist(x,g,na.rm=na.rm,edge.check=edge.check)
+  g<-add.edges(g,as.list(x[,1]),as.list(x[,2]),edge.check=edge.check)
+  va <- attr(x,"vertex.attributes")
+  if(length(va)>0){
+   for (i in (1:length(va))){ 
+    g <- set.vertex.attribute(g,names(va)[i], va[[i]])
+   }
+  }
+  #Return the result
+  g
 }
