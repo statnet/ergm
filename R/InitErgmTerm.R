14d38dcb661253e91b938fa97b10de980dccb777
David R. Hunter <dhunter@stat.psu.edu>
2011-01-06 19:53:24 +0000

    Added help for "transitiveties" term; seems to work okay in tests but "diff" argument is not currently implemented.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/2.3@11032 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitErgmTerm.R
+++ R/InitErgmTerm.R
@@ -3228,12 +3228,12 @@ InitErgmTerm.transitive<-function (nw, arglist, drop=TRUE, ...) {
 #################################################################################
 InitErgmTerm.transitiveties<-function (nw, arglist, drop=TRUE, ...) {
   a <- check.ErgmTerm(nw, arglist, directed=TRUE,
-                      varnames = c("attrname", "diff"),
-                      vartypes = c("character", "logical"),
-                      defaultvalues = list(NULL, FALSE),
-                      required = c(FALSE, FALSE))
+                      varnames = c("attrname"),#, "diff"),
+                      vartypes = c("character"),#, "logical"),
+                      defaultvalues = list(NULL),#, FALSE),
+                      required = c(FALSE))#, FALSE))
   attrname <- a$attrname
-  diff <- a$diff
+#  diff <- a$diff
   if(!is.null(attrname)) {
     nodecov <- get.node.attr(nw, attrname, "transitiveties")
     u<-sort(unique(nodecov))
@@ -3244,35 +3244,35 @@ InitErgmTerm.transitiveties<-function (nw, arglist, drop=TRUE, ...) {
       stop ("Attribute given to transitiveties() has only one value", call.=FALSE)
     if(drop){
       triattr <- summary(as.formula(paste('nw ~ transitiveties(','"',attrname,
-                                          '",diff=',diff,')',sep="")),
-                         drop=FALSE) == 0
-      if(diff){
-        if(any(triattr)){
-          dropterms <- paste(paste("transitiveties",attrname,sep="."),
-                             u[triattr],sep="")
-      cat(" ")
-          cat(paste("Warning: The count of", dropterms, "is extreme;\n",
-                 " the corresponding coefficient has been fixed at its MLE of negative infinity.\n",sep=" "))
-          u <- u[!triattr] 
-          ui <- ui[!triattr] 
-        }
-      }else{
+                                                               '",diff=',diff,')',sep="")),
+                              drop=FALSE) == 0
+#      if(diff){
+#        if(any(triattr)){
+#          dropterms <- paste(paste("transitiveties",attrname,sep="."),
+#                             u[triattr],sep="")
+#      cat(" ")
+#          cat(paste("Warning: The count of", dropterms, "is extreme;\n",
+#                 " the corresponding coefficient has been fixed at its MLE of negative infinity.\n",sep=" "))
+#          u <- u[!triattr] 
+#          ui <- ui[!triattr] 
+#        }
+#      }else{
         if(triattr){
           dropterms <- paste(paste("transitiveties",attrname,sep="."),sep="")
       cat(" ")
           cat(paste("Warning: The count of", dropterms, "is extreme;\n",
                  " the corresponding coefficient has been fixed at its MLE of negative infinity.\n",sep=" "))
         }
-      }
+#      }
     }
-    if (!diff) {
+#    if (!diff) {
       coef.names <- paste("transitiveties",attrname,sep=".")
       inputs <- c(nodecov)
-     } else { 
-       coef.names <- paste("transitiveties",attrname, u, sep=".")
-       inputs <- c(ui, nodecov)
-       attr(inputs, "ParamsBeforeCov") <- length(ui)
-     }
+#     } else { 
+#       coef.names <- paste("transitiveties",attrname, u, sep=".")
+#       inputs <- c(ui, nodecov)
+#       attr(inputs, "ParamsBeforeCov") <- length(ui)
+#     }
   }else{
     coef.names <- "transitiveties"
     inputs <- NULL
