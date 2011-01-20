e3ac82278a96f025967d6a351f0ac504843bcbea
Pavel N. Krivitsky <pavel@uow.edu.au>
2011-01-20 06:54:26 +0000

    Fixed a bug in Cprepare related to handling of weights on empty networks.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/Generalized@11051 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.Cprepare.R
+++ R/ergm.Cprepare.R
@@ -57,9 +57,9 @@ ergm.Cprepare <- function(nw, m, response=NULL)
     Clist$nedges<-0
     Clist$heads<-NULL
     Clist$tails<-NULL
-    ## Make sure weights attribute exists, even if it's empty, since
+    ## Make sure weights is NULL, even if it's empty, since
     ## it's used to decide whether MCMC or WtMCMC is called.
-    Clist$weights<-as.numeric(NULL)
+    Clist$weights<-NULL
   }else{
     if(!is.matrix(e)){e <- matrix(e, ncol=2+!is.null(response))}
 
