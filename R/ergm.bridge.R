0b522110164ab3542b0605e7eb7cb62b23e831f7
Pavel N. Krivitsky <pavel@uow.edu.au>
2011-04-28 06:46:21 +0000

    Implemented bridge sampling for log-likelihood-ratio calculations, starting either at 0 or at an arbitrary dyad-independent model.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11138 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ R/ergm.bridge.R
@@ -0,0 +1,144 @@
+## The workhorse function: Uses bridge sampling to estimate the
+## log-likelihood-ratio between two configurations `to' and `from' for
+## a model `object', using `nsteps' MCMC samples. If llronly==TRUE,
+## returns only the estimate. Otherwise, returns a list with more
+## details. Other parameters are same as simulate.ergm.
+ergm.bridge.llr<-function(object, response=NULL, from, to, nsteps, sample.size=10000, basis=NULL, verbose=FALSE, llronly=FALSE, ...){
+
+  ## Here, we need to get the model object to get the likelihood and gradient functions.
+
+  # If basis is not null, replace network in formula by basis.
+  # In either case, let nw be network object from formula.
+  if(is.null(nw <- basis)) {
+    nw <- ergm.getnetwork(object)
+  }
+
+  nw <- as.network(nw)
+  if(!is.network(nw)){
+    stop("A network object on the LHS of the formula or via",
+         " the 'basis' argument must be given")
+  }
+
+  # New formula (no longer use 'object'):
+  form <- ergm.update.formula(object, nw ~ .)
+  
+  m <- ergm.getmodel(form, nw, drop=FALSE, response=response)
+
+
+  ## Generate the path.
+
+  path<-t(rbind(sapply(seq(from=0+1/2/(nsteps+1),to=1-1/2/(nsteps+1),length.out=nsteps),function(u) cbind(to*u + from*(1-u)))))
+
+  obs<-summary(form,response=response)
+  
+  stats<-t(rbind(apply(path,1,function(theta) apply(simulate(form, theta0=theta, nsim=ceiling(sample.size/nsteps), response=response, basis=basis, statsonly=TRUE, verbose=verbose, ...),2,mean)-obs)))
+  
+  Dtheta.Du<-to-from
+
+  llrs<--sapply(seq_len(nsteps), function(i) crossprod(Dtheta.Du,ergm.etagradmult(path[i,],stats[i,],m$etamap)))/nsteps
+  llr<-sum(llrs)
+  if(llronly) llr
+  else list(llr=llr,llrs=llrs,path=path,stats=stats,Dtheta.Du=Dtheta.Du)
+}
+
+## A convenience wrapper around ergm.bridge.llr: returns the
+## log-likelihood of configuration `theta' *relative to the reference
+## measure*. That is, the configuration with theta=0 is defined as
+## having log-likelihood of 0.
+ergm.bridge.0.llk<-function(object, response=response, theta, nsteps, llkonly=TRUE, ...){
+  br<-ergm.bridge.llr(object, from=rep(0,length(theta)), to=theta, nsteps=nsteps, response=response, ...)
+  if(llkonly) br$llr
+  else br
+}
+
+## A wrapper around ergm.bridge.llr that uses a specified
+## dyad-independence model `dind` (specified as RHS-only formula),
+## either at the its MLE (the default) or at a value specified by
+## theta.dind, as a starting point for the bridge sampling. The terms
+## in the dyad-independent model may overlap with the terms in the
+## model whose likelihood is being evaluated, but don't have to.
+ergm.bridge.dindstart.llk<-function(object, response=NULL, theta, nsteps, dind=~edges, theta.dind=NULL,  basis=NULL, llkonly=TRUE, ...){
+  if(!is.null(response)) stop("Only binary ERGMs are supported at this time.")
+  # If basis is not null, replace network in formula by basis.
+  # In either case, let nw be network object from formula.
+  if(is.null(nw <- basis)) {
+    nw <- ergm.getnetwork(object)
+  }
+  
+  nw <- as.network(nw)
+  if(!is.network(nw)){
+    stop("A network object on the LHS of the formula or via",
+         " the 'basis' argument must be given")
+  }
+
+  dind<-ergm.update.formula(dind,nw~.)  
+  ergm.dind<-ergm(dind,MPLEonly=TRUE)
+
+  if(is.null(ergm.dind$theta1$independent) || any(!ergm.dind$theta1$independent))
+    stop("Reference model `dind' must be dyad-independent!")
+
+  if(is.null(theta.dind)){
+    coef.dind<-coef(ergm.dind)
+    llk.dind<--ergm.dind$glm$deviance/2
+  }else{
+    coef.dind<-theta.dind
+    lin.pred <- model.matrix(ergm.dind$glm) %*% theta.dind
+    llk.dind<- crossprod(lin.pred,ergm.dind$glm$y*ergm.dind$glm$prior.weights)-sum(log1p(exp(lin.pred))*ergm.dind$glm$prior.weights)
+  }
+  
+  
+  ## Replace the LHS with a . and append a . to RHS.
+  dind.template<-dind
+  dind.template[[2]]<-as.name(".")
+  dind.template[[3]]<-call("+",dind.template[[3]],as.name("."))
+  ## Construct the augmented formula.
+  form.aug<-ergm.update.formula(object, dind.template)
+
+  from<-c(coef.dind,rep(0,length(theta)))
+  to<-c(rep(0,length(coef.dind)),theta)
+
+  br<-ergm.bridge.llr(form.aug, response=response, from=from, to=to, basis=basis, nsteps=nsteps, ...)
+  
+  if(llkonly) llk.dind + br$llr
+  else c(br,llk.dind=llk.dind, llk=llk.dind + br$llr)
+}
+
+## A wrapper around ergm.bridge.llr that uses a model with a Hamming
+## distance to the LHS network itself as a starting point, either with
+## a specified coefficient `hamming.start' or with a coefficient such
+## that the log-likelihood for it is llk.guess.
+##
+## The idea is to use the Hamming term as "scaffolding", which is
+## slowly removed as the real model terms approach their objective
+## values.
+ergm.bridge.hammingstart.llk<-function(object, response=NULL, theta, nsteps, hamming.start=NULL, llk.guess=NULL, basis=NULL, llkonly=TRUE, ...){
+  if(!is.null(response)) stop("Only binary ERGMs are supported at this time.")
+  # If basis is not null, replace network in formula by basis.
+  # In either case, let nw be network object from formula.
+  if(is.null(nw <- basis)) {
+    nw <- ergm.getnetwork(object)
+  }
+  
+  nw <- as.network(nw)
+  if(!is.network(nw)){
+    stop("A network object on the LHS of the formula or via",
+         " the 'basis' argument must be given")
+  }
+
+  if(is.null(hamming.start)){
+    if(is.null(llk.guess))  llk.guess<-ergm(nw~edges)$mle.lik
+
+    hamming.start<-log(expm1(-llk.guess/network.dyadcount(nw)))
+  }
+
+  form.aug<-ergm.update.formula(object, . ~ . + hamming(nw))
+  from<-c(rep(0,length(theta)), hamming.start)
+  to<-c(theta,0)
+  
+  llk.hamming<--network.dyadcount(nw)*log1p(exp(hamming.start))
+  br<-ergm.bridge.llr(form.aug, response=response, from=from, to=to, basis=basis, nsteps=nsteps, ...)
+
+  if(llkonly) llk.hamming + br$llr
+  else c(br,llk.hamming=llk.hamming, llk=llk.hamming + br$llr) 
+}
+
