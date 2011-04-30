47172536e0f9faf538ec988b51b586062b3fa451
Mark S. Handcock <handcock@stat.ucla.edu>
2011-04-30 20:26:10 +0000

    Made the computation of observed statistics in the sequential sampling more robust. This mirrors the change just made to the trunk.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/2.3@11154 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.mainfitloop.R
+++ R/ergm.mainfitloop.R
@@ -117,7 +117,8 @@ ergm.mainfitloop <- function(theta0, nw, model, Clist,
         nw <- nw.returned
         nw.obs <- summary(model$formula, basis=nw)
         namesmatch <- match(names(MCMCparams$meanstats), names(nw.obs))
-        statshift <- nw.obs[namesmatch]-Clist$meanstats
+        statshift <- Clist$meanstats
+        statshift[!is.na(namesmatch)] <- statshift[!is.na(namesmatch)] - nw.obs[namesmatch[!is.na(namesmatch)]]
       }
     }
     
