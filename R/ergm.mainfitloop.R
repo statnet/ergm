140f0a4454fe88a294a30b5004a109aa4d35652b
Mark S. Handcock <handcock@stat.ucla.edu>
2011-05-03 01:05:48 +0000

    Fixed very bad bug that meant the trunk ergm.estimate was incorrect and non-convergent. It is unclear how long it has been wrong, but it look like a long time :-(
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11162 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.mainfitloop.R
+++ R/ergm.mainfitloop.R
@@ -120,8 +120,8 @@ ergm.mainfitloop <- function(theta0, nw, model, Clist,
         nw <- nw.returned
         nw.obs <- summary(model$formula, basis=nw, response=response)
         namesmatch <- match(names(MCMCparams$meanstats), names(nw.obs))
-        statshift <- Clist$meanstats
-        statshift[!is.na(namesmatch)] <- statshift[!is.na(namesmatch)] - nw.obs[namesmatch[!is.na(namesmatch)]]
+        statshift <- -Clist$meanstats
+        statshift[!is.na(namesmatch)] <- statshift[!is.na(namesmatch)] + nw.obs[namesmatch[!is.na(namesmatch)]]
       }
     }
     
