45c900b658557098308f58c083085c7f1d8ab759
Pavel N. Krivitsky <pavel@uow.edu.au>
2011-05-03 20:18:51 +0000

    Merged the ergm.update.formula tests.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11165 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/ergm.update.formula.R
+++ tests/ergm.update.formula.R
@@ -12,3 +12,52 @@ lhs.subst.san <- function(rhs,meanstats) {
 
 
 stopifnot(summary(lhs.subst.san(~edges,20)~edges)==20)
+
+# Test code created by Nicola Soriani
+# depends on proper functioning of ergm.update.formula
+
+predict.ergm.model<- function(model)  # only for Directed Network
+{
+ net<-model$network
+ n<-network.size(net)
+ beta<-model$coef
+ eta <- matrix(0, n, n)
+
+
+ for(i in 1:n)
+	for(j in 1:n)
+	{
+	 alternative<-net
+	 if(is.na(net[i,j]))
+	  {
+	   net[i,j]<-1
+	   alternative[i,j]<-1 - net[i,j]
+	   u2<- summary(ergm.update.formula(model$formula, alternative ~ .))
+	   delta<-u2-u1
+	   prob<-1/(1+exp(sum(beta*delta)))
+	   if (net[i,j]==1) eta[i,j]<- prob
+       else eta[i,j]<-1-prob
+       if (i==j) eta[i,j]<-0
+       net[i,j]<-NA
+       }
+    else
+     {
+      alternative[i,j]<-1 - net[i,j]
+      u1<-summary(ergm.update.formula(model$formula, net ~ .))
+      u2<-summary(ergm.update.formula(model$formula, alternative ~ .))
+      delta<- u2-u1
+      prob<- 1/(1+exp(sum(beta*delta)))
+      if (net[i,j]==1) eta[i,j]<- prob
+      else eta[i,j]<-1-prob
+      if (i==j) eta[i,j]<-0
+      }
+    }
+  return(eta)
+}
+
+data(florentine)
+
+Ergm<-ergm(flobusiness~edges+gwesp(1,fixed=TRUE))
+
+stopifnot(all(diag(predict.ergm.model(Ergm))==0))
+
