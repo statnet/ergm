8e12e4258b496c494c4ddf11ec4145022d1a4db6
Pavel N. Krivitsky <pavel@uow.edu.au>
2011-05-13 21:10:44 +0000

    Added reweighting for the ergm$sample to match the MLE closer.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11199 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/autoboot.ergm.R
+++ R/autoboot.ergm.R
@@ -2,30 +2,30 @@
 ## parameter estimates for a fitted ergm.
 ##
 ## It takes a dyad-dependent ERGM object, `object`, a bootstrap
-## resample size `S` that defaults to MCMCsamplesize of the ergm
+## resample size `R` that defaults to MCMCsamplesize of the ergm
 ## object, and an optional control.ergm list.  Note that the
-## optimization step takes a fair amount of time (linear in `S` and
+## optimization step takes a fair amount of time (linear in `R` and
 ## otherwise depending on MCMCsamplesize and number of canonical
-## statistics), so `S` should probably be smaller than the default for
+## statistics), so `R` should probably be smaller than the default for
 ## most uses.
 ##
 ## The return value is simply a data frame with coefficient names in
 ## the columns and replicates in the rows.
 ##
 ## The basic idea is that for each draw of an ERGM's sufficient
-## statistics under the MLE (contained in ergm$sample), take 1 MCMC
-## MLE optimization step from the actual MLE to evaluate what the MLE
-## would have been had that value been the observed statistic.
-## Provided these new MLEs are not too far from actual MLE, this
-## should be equivalent to fitting an ERGM to each of a sample of
-## networks drawn from the MLE distribution, a parametric bootstrap
-## procedure.
+## statistics under the MLE (contained in the reweighted ergm$sample),
+## take 1 MCMC MLE optimization step from the actual MLE to evaluate
+## what the MLE would have been had that value been the observed
+## statistic.  Provided these new MLEs are not too far from actual
+## MLE, this should be equivalent to fitting an ERGM to each of a
+## sample of networks drawn from the MLE distribution, a parametric
+## bootstrap procedure.
 ##
 ## It should work perfectly well for curved models, but does not, at
 ## present, support partially observed ERGMs and may or may not work
 ## for models that have been drop-ed.
 
-autoboot.ergm<-function(object, S=nrow(object$sample), control=control.ergm()){
+autoboot.ergm<-function(object, R, control=control.ergm()){
   
   if(is.dyad.independent(object))
     stop(paste("ERGM fit `",deparse(substitute(object)),"` is dyad-independent. Standard errors returned by the object are exact.",sep=""))
@@ -33,11 +33,31 @@ autoboot.ergm<-function(object, S=nrow(object$sample), control=control.ergm()){
   m<-ergm.getmodel(object$formula, ergm.getnetwork(object$formula), drop=FALSE, response=object$response)
 
   samp<-object$sample
-  if(S>nrow(samp))stop("Bootstrap sample size cannot be greater than the ERGM sample size.")
+  S<-nrow(samp)
+  
+  ## Because the MCMC sample that came with the ergm is drawn not from
+  ## the MLE, but from the iteration prior to the MLE, we need to
+  ## reweight the realizations for the bootstrap sample:
+  
+  theta.samp<-object$MCMCtheta
+  eta.samp<-ergm.eta(theta.samp, m$etamap)
+  theta.mle<-coef(object)
+  eta.mle<-ergm.eta(theta.mle, m$etamap)
 
-  theta0<-object$MCMCtheta
-  theta.boot<-apply(samp[sample(seq_len(nrow(samp)),S),],1,function(stat){
-    v<-ergm.estimate(theta0=theta0,model=m,statsmatrix=sweep(samp,2,stat),statsmatrix.obs=NULL,
+  eta.diff<-eta.mle-eta.samp
+
+  samp.w<-exp(samp %*% cbind(eta.diff))
+  samp.w<-samp.w/sum(samp.w)
+
+  ## Now, the expensive part of this procedure is the many
+  ## optimization steps, so resample and compress:
+
+  resamp<-sample.int(S,R,replace=TRUE,prob=samp.w)
+  resamp.wt<-tabulate(resamp, S)
+  resamp.l<-which(resamp.wt!=0)
+    
+  theta.boot<-apply(samp[resamp.l,],1,function(stat){
+    v<-ergm.estimate(theta0=theta.samp,model=m,statsmatrix=sweep(samp,2,stat),statsmatrix.obs=NULL,
                      epsilon=control$epsilon,
                      nr.maxit=control$nr.maxit,
                      nr.reltol=control$nr.reltol,
@@ -48,5 +68,7 @@ autoboot.ergm<-function(object, S=nrow(object$sample), control=control.ergm()){
                      estimateonly=TRUE)
     v$coef
   })
-  as.data.frame(t(theta.boot))
+
+  resamp.ind<-rep(seq_along(resamp.l),resamp.wt[resamp.wt!=0])
+  as.data.frame(t(theta.boot)[resamp.ind,])
 }
