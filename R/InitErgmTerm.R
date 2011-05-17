5955d073b00c15d79ccc17edbf8513b14f0cb3a0
David R. Hunter <dhunter@stat.psu.edu>
2011-05-17 10:53:24 +0000

    Updated 2.4/inst/NEWS.Rd with list of new things since 2.3 Added ergm.allstats and ergm.exact to 2.4; added documentation to trunk and 2.4 Removed InitErgmTerm_degcrossprod.R (moved these functions to InitErgmTerm.R)
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11203 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitErgmTerm.R
+++ R/InitErgmTerm.R
@@ -2,7 +2,7 @@
 # This file contains the following 74 new, easier-to-write ergm-term
 # initialization functions (each prepended with "InitErgmTerm"):
 #   A:   <absdiff>          <absdiffcat>      <altkstar>
-#        <asymmetric>
+#        <asymmetric>       <adegcor>
 #   B:   <b1concurrent>     <b1degree>        <b1factor>
 #        <b1star>           <b1starmix>       <b1twostar>
 #        <b2concurrent>     <b2degree>        <b2factor>         
@@ -10,7 +10,7 @@
 #        <balance>
 #   C:   <concurrent>       <cycle>           <ctriple>=<ctriad> 
 #   D:   <degree>           <density>         <dsp>
-#        <dyadcov>
+#        <dyadcov>          <degcrossprod>    <degcor>
 #   E:   <edgecov>          <edges>           <esp>
 #   G:   <gwb1degree>       <gwb2degree>      <gwdegree>
 #        <gwdsp>            <gwesp>           <gwidegree>
@@ -26,7 +26,8 @@
 #        <nodemix>          <nodeocov>        <nodeofactor>       
 #        <nsp>
 #   O:   <odegree>          <ostar>           <outdegreepopularity>  
-#   R:   <receiver>
+#   P:   <pdegcor>
+#   R:   <receiver>         <rdegcor>
 #   S:   <sender>           <simmelian>       <simmelianties>
 #        <smalldiff>        <sociality>
 #   T:   <threepath>        <transitive>      <triangles>=<triangle>
@@ -169,6 +170,25 @@ InitErgmTerm.absdiffcat <- function(nw, arglist, ...) {
        )
 }
 
+
+
+################################################################################
+InitErgmTerm.adegcor<-function (nw, arglist, drop=TRUE, ...) {
+  a <- check.ErgmTerm(nw, arglist, directed=FALSE) 
+
+  deg=summary(nw ~ sociality(base=0))
+  el=as.matrix.network.edgelist(nw)
+  deg1<-deg[el[,1]]
+  deg2<-deg[el[,2]]
+  alldeg<-c(deg1,deg2)
+  sigma2<-(sum(alldeg*alldeg)-length(alldeg)*(mean(alldeg)^2))
+  ### Construct the list to return
+  list(name="adegcor",                            #name: required
+       coef.names = "adegcor",                    #coef.names: required
+       inputs=sigma2,
+       dependence = TRUE # So we don't use MCMC if not necessary
+       )
+}
 
 
 ################################################################################
@@ -1021,6 +1041,35 @@ InitErgmTerm.cycle <- function(nw, arglist, drop=TRUE, ...) {
 
 #=======================InitErgmTerm functions:  D============================#
 
+
+################################################################################
+InitErgmTerm.degcor<-function (nw, arglist, drop=TRUE, ...) {
+  a <- check.ErgmTerm(nw, arglist, directed=FALSE) 
+
+  deg=summary(nw ~ sociality(base=0))
+  el=as.matrix.network.edgelist(nw)
+  deg1<-deg[el[,1]]
+  deg2<-deg[el[,2]]
+  alldeg<-c(deg1,deg2)
+  sigma2<-(sum(alldeg*alldeg)-length(alldeg)*(mean(alldeg)^2))
+  ### Construct the list to return
+  list(name="degcor",                            #name: required
+       coef.names = "degcor",                    #coef.names: required
+       inputs=sigma2,
+       dependence = TRUE # So we don't use MCMC if not necessary
+       )
+}
+
+################################################################################
+InitErgmTerm.degcrossprod<-function (nw, arglist, drop=TRUE, ...) {
+  a <- check.ErgmTerm(nw, arglist, directed=FALSE) 
+  ### Construct the list to return
+  list(name="degcrossprod",                            #name: required
+       coef.names = "degcrossprod",                    #coef.names: required
+       inputs=2*summary(nw ~ edges),
+       dependence = TRUE # So we don't use MCMC if not necessary
+       )
+}
 
 ################################################################################
 InitErgmTerm.degree<-function(nw, arglist, drop=TRUE, ...) {
@@ -2912,8 +2961,50 @@ InitErgmTerm.ostar<-function(nw, arglist, drop=TRUE, ...) {
 }
 
 
+#=======================InitErgmTerm functions:  P============================#
+
+################################################################################
+InitErgmTerm.pdegcor<-function (nw, arglist, drop=TRUE, ...) {
+  a <- check.ErgmTerm(nw, arglist, directed=TRUE) 
+
+  el=as.matrix.network.edgelist(nw)
+  deg1<-summary(nw ~ sender(base=0))[el[,1]]
+  deg2<-summary(nw ~ receiver(base=0))[el[,2]]
+  deg12na<-is.na(deg1)|is.na(deg2)
+  deg1<-deg1[!deg12na]
+  deg2<-deg2[!deg12na]
+  sigma1<-(sum(deg1*deg1)-length(deg1)*(mean(deg1)^2))
+  sigma2<-(sum(deg2*deg2)-length(deg2)*(mean(deg2)^2))
+  sigma1 <- 0
+  sigma2 <- 0
+  ### Construct the list to return
+  list(name="pdegcor",                            #name: required
+       coef.names = "pdegcor",                    #coef.names: required
+       inputs=sigma2,
+       dependence = TRUE # So we don't use MCMC if not necessary
+       )
+}
+
 
 #=======================InitErgmTerm functions:  R============================#
+
+################################################################################
+InitErgmTerm.rdegcor<-function (nw, arglist, drop=TRUE, ...) {
+  a <- check.ErgmTerm(nw, arglist, directed=FALSE) 
+
+  deg=summary(nw ~ sociality(base=0))
+  el=as.matrix.network.edgelist(nw)
+  deg1<-deg[el[,1]]
+  deg2<-deg[el[,2]]
+  alldeg<-c(deg1,deg2)
+  sigma2<-(sum(alldeg*alldeg)-length(alldeg)*(mean(alldeg)^2))
+  ### Construct the list to return
+  list(name="rdegcor",                            #name: required
+       coef.names = "rdegcor",                    #coef.names: required
+       inputs=sigma2,
+       dependence = TRUE # So we don't use MCMC if not necessary
+       )
+}
 
 ################################################################################
 InitErgmTerm.receiver<-function(nw, arglist, drop=FALSE, ...) {
