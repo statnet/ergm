959fb3cc85003e99811b53bc32d691f661b58924
Mark S. Handcock <handcock@stat.ucla.edu>
2011-06-27 00:23:56 +0000

    Added stopcluster command
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11262 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/simulate.ergm.R
+++ R/simulate.ergm.R
@@ -247,6 +247,7 @@ simulate.formula.ergm <- function(object, nsim=1, seed=NULL, theta0, response=NU
      }
      out.mat[i,] <- curstats + z$statsmatrix
     }
+    stopCluster(cl)
   }
   
   if (statsonly)
