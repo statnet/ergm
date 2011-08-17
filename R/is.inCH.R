feb427c93d5bfc495c4b68250e6f396e5211b30c
David R. Hunter <dhunter@stat.psu.edu>
2011-08-17 22:36:25 +0000

    fixed bug in is.inCH.R; added steppingtest.R; other small stuff
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11271 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/is.inCH.R
+++ R/is.inCH.R
@@ -19,15 +19,19 @@ is.inCH <- function(p, M) {
   if (length(p) != NCOL(M)) 
     stop("Number of columns in matrix (2nd argument) is not equal to dimension ",
          "of first argument.")
-  library(Rglpk) # Needed in order to solve the linear program
-	R = NROW(M)
-	C=length(p)+1
-	ans <- Rglpk_solve_LP(obj=c(p,-1), mat=cbind(rbind(p,M),-1), 
-						  dir=as.vector(rep("<=",R+1)), rhs=as.vector(c(1,rep(0,R))), 
-						  max=TRUE, bounds=list(lower=list(ind=1:C,val=rep(-Inf,C))))
-	if(ans$optimum==0){x<-TRUE}  #if the max is zero, the point q is in the CH of the points p
-	else{x<-FALSE}              #if the max is strictly positive, q is not in the CH of p
-	x
+  R = NROW(M)
+  C=length(p)+1
+  if(require("Rglpk", quietly = TRUE)) {
+	ans <- Rglpk_solve_LP(obj=c(p,-1), mat=cbind(rbind(p,M),-1),
+                              dir=as.vector(rep("<=",R+1)), rhs=as.vector(c(1,rep(0,R))),
+                              max=TRUE, bounds=list(lower=list(ind=1:C,val=rep(-Inf,C))))
+  }else{
+     stop("The 'Rglpk' package must be installed if the ergm control parameter 'style'='stepping' ")
+  }
+
+  if(ans$optimum==0){x<-TRUE}  #if the max is zero, the point q is in the CH of the points p
+  else{x<-FALSE}              #if the max is strictly positive, q is not in the CH of p
+  x
 }
 
 
