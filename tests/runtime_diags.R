1e21d78f8f59eda3b3d71b408ac401f3966f1647
Pavel N. Krivitsky <pavel@uow.edu.au>
2011-10-29 00:26:33 +0000

    Fixed a typo in the runtime diagnostics test.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11305 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/runtime_diags.R
+++ tests/runtime_diags.R
@@ -2,7 +2,7 @@ library(ergm)
 data(florentine)
 
 gest <- ergm(flomarriage ~ kstar(1:2) + absdiff("wealth") + triangle,
-             control=control.ergm(burnin.retry=10,runtime.traceplot=TRUE),burnin=200)
+             control=control.ergm(burnin.retries=10,runtime.traceplot=TRUE),burnin=200)
 
 gest <- ergm(flomarriage ~ kstar(1:2) + absdiff("wealth") + triangle,
              control=control.ergm(runtime.traceplot=TRUE))
