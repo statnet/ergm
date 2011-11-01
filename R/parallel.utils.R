927e94fda6565cc9663f167413042f39936dbeb4
Pavel N. Krivitsky <pavel@uow.edu.au>
2011-11-01 02:17:38 +0000

    When parallel>0, ergm.getCluster now works harder to find a parallelizing method, falling back to MPI, then to Socket.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11307 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/parallel.utils.R
+++ R/parallel.utils.R
@@ -1,25 +1,48 @@
 # Makes a heroic effort to get a cluster of desired size.
 ergm.getCluster <- function(control, verbose=FALSE){
   capture.output(require(snow, quietly=TRUE, warn.conflicts = FALSE))
-#
-# Start PVM if necessary
-#
-  if(getClusterOption("type")=="PVM"){
-    if(verbose){cat("Engaging warp drive using PVM ...\n")}
-    capture.output(require(rpvm, quietly=TRUE, warn.conflicts = FALSE))
-    PVM.running <- try(.PVM.config(), silent=TRUE)
-    if(inherits(PVM.running,"try-error")){
-      hostfile <- paste(Sys.getenv("HOME"),"/.xpvm_hosts",sep="")
-      .PVM.start.pvmd(hostfile)
-      cat("no problem... PVM started by ergm...\n")
+
+  reqs<-list("PVM"="rpvm", "MPI"="rmpi")
+    
+  for(type in unique(c(getClusterOption("type"),"PVM","MPI","SOCK"))){
+    if(verbose) cat("Attempting ",type,"... ", sep="")
+    #   Start Cluster
+
+
+    if(type %in% names(reqs)){
+      if(! reqs[[type]] %in% rownames(installed.packages())){
+        if(verbose) cat("Failed.\n")
+        next
+      }else{
+        if(!require(reqs[[type]], quietly=TRUE, warn.conflicts = FALSE, character.only=TRUE))
+          next
+      }
     }
-  }else{
-    if(verbose){cat("Engaging warp drive using MPI ...\n")}
+    
+    if(type=="PVM")
+      try({
+        PVM.running <- try(.PVM.config(), silent=TRUE)
+        if(inherits(PVM.running,"try-error")){
+          hostfile <- paste(Sys.getenv("HOME"),"/.xpvm_hosts",sep="")
+          .PVM.start.pvmd(hostfile)
+          cat("PVM not running. Attempting to start.\n")
+        }},silent=TRUE) # OK if fails.
+    
+    
+    capture.output({
+      cl<-try(makeCluster(control$parallel,type=type),silent=TRUE)
+    })
+    
+    if(inherits(cl,"try-error")){
+      if(verbose) cat("Failed.\n")
+    }else{
+      if(verbose) cat("Success!\n")
+      break
+    }
+    
   }
-  #
-  #   Start Cluster
-  #
-  cl<-makeCluster(control$parallel)
+
+  # Set things up. 
   clusterSetupRNG(cl)
   if("ergm" %in% control$packagenames){
     clusterEvalQ(cl,library(ergm))
