83dca0da7091685c83bce7f48f776e3f468a5c08
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-03-06 01:49:14 +0000

    as.edgelist() internal function in ergm now guarantees that tail < head for undirected networks.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11436 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/as.edgelist.R
+++ R/as.edgelist.R
@@ -1,1 +1,5 @@
-as.edgelist <- as.matrix.network.edgelist
+as.edgelist <- function(nw, attrname = NULL, as.sna.edgelist = TRUE,...){
+  el <- as.matrix.network.edgelist(nw, attrname=attrname, as.sna.edgelist=as.sna.edgelist,...)
+  if(!is.directed(nw)) el[,1:2] <- cbind(pmin(el[,1],el[,2]),pmax(el[,1],el[,2]))
+  el
+}
