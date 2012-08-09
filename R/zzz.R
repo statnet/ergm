c85ebab903f097b463dc0bc7fbd94e9ba2f0605e
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-08-09 06:22:30 +0000

    Forgot to remove STERGM MHP table entries from the ergm's zzz.R file when tergm was split out.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11614 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -15,12 +15,6 @@
 }
 
 .RegisterMHPs <- function(){
-  ergm.MHP.table("f", "Bernoulli", "",  0, "random", "formation")
-  ergm.MHP.table("f", "Bernoulli", "bd",  0, "random", "formation")
-  ergm.MHP.table("f", "Bernoulli", "",  1, "TNT", "formationTNT")
-  ergm.MHP.table("f", "Bernoulli", "bd",  1, "TNT", "formationTNT")
-  ergm.MHP.table("d", "Bernoulli", "",  0, "random", "dissolution")
-  ergm.MHP.table("d", "Bernoulli", "bd",  0, "random", "dissolution")
   ergm.MHP.table("c", "Bernoulli", "",  0, "random", "randomtoggle")
   ergm.MHP.table("c", "Bernoulli", "bd",  0, "random", "randomtoggle")
   ergm.MHP.table("c", "Bernoulli", "",  1, "TNT", "TNT")
