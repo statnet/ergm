92e3b36f5fa31b34d0fcff5ea58c8f0e3b4dcd1a
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-08-14 04:24:36 +0000

    Small bugfixes to the as.edgelist() functions.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11630 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/as.edgelist.R
+++ R/as.edgelist.R
@@ -1,10 +1,10 @@
 as.edgelist <- function(x, ...) UseMethod("as.edgelist")
 
 as.edgelist.network <- function(x, attrname = NULL, as.sna.edgelist = FALSE,...){
-  as.edgelist(as.matrix.network.edgelist(x, attrname=attrname, as.sna.edgelist=as.sna.edgelist,...))
+  as.edgelist(as.matrix.network.edgelist(x, attrname=attrname, as.sna.edgelist=as.sna.edgelist,...), directed=is.directed(x))
 }
 
 as.edgelist.matrix <- function(x, directed=TRUE, ...){
   if(!directed) x[,1:2] <- cbind(pmin(x[,1],x[,2]),pmax(x[,1],x[,2]))
-  x[order(x[,1],x[,2]),,drop=FALSE]
+  unique(x[order(x[,1],x[,2]),,drop=FALSE])
 }
