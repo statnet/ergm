4f1c555da56bddc13119da234b30e1a27cd70069
Pavel N. Krivitsky <pavel@uow.edu.au>
2012-10-12 18:58:47 +0000

    * A typographical fix in the lasttoggle documentation. * Moved some code out of ergm.el.lasttoggle() into mk.edge.to.pos.lasttoggle.f() that constructs a function that maps a vector c(tail, head) onto the position in the lasttoggle list appropriate for the given network.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11799 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.Cprepare.R
+++ R/ergm.Cprepare.R
@@ -123,15 +123,19 @@ ergm.Cprepare.el<-function(x, attrname=NULL, directed=if(is.network(x)) is.direc
 }
 
 # Note: this converter must be kept in sync with whatever edgetree.c does.
+mk.edge.to.pos.lasttoggle.f <- function(nw){
+  if(is.bipartite(nw))
+    function(e) (e[2]-b-1)*b + e[1]
+  else if(is.directed(nw))
+    function(e) (e[2]-1)*(n - 1) + e[1] - (e[1]>e[2])
+  else function(e) (e[2] - 1)*(e[2] - 2)/2 + e[1]
+}
+
 ergm.el.lasttoggle <- function(nw){
   n <- network.size(nw)
   b <- if(is.bipartite(nw)) nw %n% "bipartite"
-  edge.to.pos <-
-    if(is.bipartite(nw))
-      function(e) (e[2]-b-1)*b + e[1]
-    else if(is.directed(nw))
-      function(e) (e[2]-1)*(n - 1) + e[1] - (e[1]>e[2])
-    else function(e) (e[2] - 1)*(e[2] - 2)/2 + e[1]
+
+  edge.to.pos <- mk.edge.to.pos.lasttoggle.f(nw)
 
   el <- as.edgelist(nw)
   cbind(el,(nw %n% "lasttoggle")[apply(el,1,edge.to.pos)])
