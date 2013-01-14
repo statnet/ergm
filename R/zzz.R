2d171d2e9c6d8b2bd38c425beb0997f6b35d6cf9
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-01-14 02:31:57 +0000

    3.1 branches only: * Renamed mkStartupMessage() to statnetStartupMessage(). * Documented statnetStartupMessage(). * 3.1 branches of all the Statnet Commons packages now use the statnetStartupMessage(pkgname, friends, nofriends) approach. * statnetStartupMessage() prints the "wall of text" unconditionally. (For now.) * Version numbers have been set to 3.1-0.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/3.1@11875 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,6 @@
 .onAttach <- function(lib, pkg){
-  packageStartupMessage(mkStartupMessage("ergm"))
+  sm <- statnetStartupMessage("ergm", c("ergm.count","tergm"), FALSE)
+  if(!is.null(sm)) packageStartupMessage(sm)
   
   # If the following have already been defined in the latentnet package, don't duplicate. Otherwise, assign them.
   IFNOTEXISTS <- c("robust.inverse","mcmc.diagnostics","mcmc.diagnostics.default","gof","gof.default")
