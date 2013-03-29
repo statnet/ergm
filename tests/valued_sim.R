28e6a0bcce7b94814a3d2d32428aec2d10d809f0
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-03-29 06:35:36 +0000

    * Valued simulation test now has a set seed. * Shortened the discrete and continuous uniform simulation tests.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11929 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/valued_sim.R
+++ tests/valued_sim.R
@@ -1,3 +1,4 @@
+library(statnet.common)
 library(ergm)
 
 ## StdNormal-reference
@@ -89,6 +90,8 @@ print(matrix(c(NA,
 
 opttest({
 
+set.seed(0)
+
 ## DiscUnif-reference
 cat("======== Discrete-uniform-reference ERGM with minimum of -1 and maxium of 5\n")
 
@@ -98,24 +101,24 @@ E <- function(q, a, b) sum((a:b)*exp(q*(a:b))/sum(exp(q*(a:b))))
 
 V <- function(q, a, b) sum(((a:b)-E(q,a,b))^2*exp(q*(a:b))/sum(exp(q*(a:b))))
 
-a <- -1; b <- 5; coefs <- c(runif(5,-3,3),0)
+a <- -1; b <- 5; coefs <- c(runif(1,-3,3),0)
 
 for(coef in coefs){
   cat("==== statsonly=TRUE, coef=",coef,"\n",sep="")
   s <- simulate(testnet3d~sum, nsim=1000, reference=~DiscUnif(a,b), response="w", coef=coef, statsonly=TRUE, control=control.simulate(MCMC.burnin=100))
   test <- approx.hotelling.diff.test(s/6,mu0=E(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (mean).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (mean).")}
   test <- approx.hotelling.diff.test((s-E(coef,a,b)*6)^2/6,mu0=V(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (variance).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (variance).")}
 
   
   cat("==== statsonly=FALSE, coef=",coef,"\n",sep="")
   s.full<-simulate(testnet3d~sum, nsim=1000, reference=~DiscUnif(a,b), response="w", coef=coef, statsonly=FALSE, control=control.simulate(MCMC.burnin=100))
   s <- sapply(s.full, function(x) sum(as.matrix(x,m="a",a="w")),simplify=TRUE)
   test <- approx.hotelling.diff.test(s/6,mu0=E(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (mean).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (mean).")}
   test <- approx.hotelling.diff.test((s-E(coef,a,b)*6)^2/6,mu0=V(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (variance).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (variance).")}
 }
   
 ## Unif-reference
@@ -124,23 +127,23 @@ cat("======== Continuous-uniform-reference ERGM with minimum of -1 and maxium of
 E <- function(q, a, b) if(isTRUE(all.equal(q,0))) (b+a)/2 else ((b*q-1)*exp(b*q)-(a*q-1)*exp(a*q))/(q*exp(b*q)-q*exp(a*q))
 V <- function(q, a, b) if(isTRUE(all.equal(q,0))) (b-a)^2/12 else (((-b^2+2*a*b-a^2)*q^2-2)*exp(b*q+a*q)+exp(2*b*q)+exp(2*a*q))/(-2*q^2*exp(b*q+a*q)+q^2*exp(2*b*q)+q^2*exp(2*a*q))
 
-a <- -1; b <- 5; coefs <- c(runif(5,-3,3),0)
+a <- -1; b <- 5; coefs <- c(runif(1,-3,3),0)
 
 for(coef in coefs){
   cat("==== statsonly=TRUE, coef=",coef,"\n",sep="")
   s <- simulate(testnet3d~sum, nsim=1000, reference=~Unif(a,b), response="w", coef=coef, statsonly=TRUE, control=control.simulate(MCMC.burnin=100))
   test <- approx.hotelling.diff.test(s/6,mu0=E(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (mean).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (mean).")}
   test <- approx.hotelling.diff.test((s-E(coef,a,b)*6)^2/6,mu0=V(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (variance).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (variance).")}
 
   
   cat("==== statsonly=FALSE, coef=",coef,"\n",sep="")
   s.full<-simulate(testnet3d~sum, nsim=1000, reference=~Unif(a,b), response="w", coef=coef, statsonly=FALSE, control=control.simulate(MCMC.burnin=100))
   s <- sapply(s.full, function(x) sum(as.matrix(x,m="a",a="w")),simplify=TRUE)
   test <- approx.hotelling.diff.test(s/6,mu0=E(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (mean).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (mean).")}
   test <- approx.hotelling.diff.test((s-E(coef,a,b)*6)^2/6,mu0=V(coef,a,b))
-  if(test$p.value<.001) {print(test); stop("Simulation test failed (variance).")}
+  if(test$p.value<0.001) {print(test); stop("Simulation test failed (variance).")}
 }
 }, "continuous uniform reference")
