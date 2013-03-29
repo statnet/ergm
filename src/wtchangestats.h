549ac6783e597f9b60d2ff4a516f1086b15d74d9
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-03-29 04:21:26 +0000

    Implemented and documented nodematch and nodemix terms for valued ERGM. (Tests still need to be written.)
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11924 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/wtchangestats.h
+++ src/wtchangestats.h
@@ -21,6 +21,8 @@ WtD_CHANGESTAT_FN(d_greaterthan);
 WtD_CHANGESTAT_FN(d_ininterval);
 
 /********************  changestats:   M    ***********/
+WtD_CHANGESTAT_FN(d_mix_nonzero);
+WtD_CHANGESTAT_FN(d_mix_sum);
 WtD_CHANGESTAT_FN(d_mutual_wt_product);
 WtD_CHANGESTAT_FN(d_mutual_wt_geom_mean);
 WtD_CHANGESTAT_FN(d_mutual_wt_min); 
@@ -36,6 +38,10 @@ WtD_CHANGESTAT_FN(d_nodefactor_sum);
 WtD_CHANGESTAT_FN(d_nodeicorr);
 WtD_CHANGESTAT_FN(d_nodeifactor_nonzero);
 WtD_CHANGESTAT_FN(d_nodeifactor_sum);
+WtD_CHANGESTAT_FN(d_nodematch_nonzero);
+WtD_CHANGESTAT_FN(d_nodematch_sum);
+WtD_CHANGESTAT_FN(d_nodemix_nonzero);
+WtD_CHANGESTAT_FN(d_nodemix_sum);
 WtD_CHANGESTAT_FN(d_nodeocorr);
 WtD_CHANGESTAT_FN(d_nodeofactor_nonzero);
 WtD_CHANGESTAT_FN(d_nodeofactor_sum);
