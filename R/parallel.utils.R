bb545e4f67cb3d0ced8139d876d6f1b13b2fe3f1
Mark S. Handcock <handcock@stat.ucla.edu>
2013-04-22 02:06:33 +0000

    Added explicit use of "parallel" calls
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@11968 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/parallel.utils.R
+++ R/parallel.utils.R
@@ -43,20 +43,20 @@ ergm.getCluster <- function(control, verbose=FALSE){
 #                  .PVM.start.pvmd(hostfile)
 #                  cat("PVM not running. Attempting to start.\n")
 #                }
-                 makeCluster(control$parallel,type="PVM")
+                 parallel::makeCluster(control$parallel,type="PVM")
                },
                MPI={
                  # See if a preexisting cluster exists.
-                 if(is.null(getMPIcluster())){
+                 if(is.null(parallel::getMPIcluster())){
                    # Remember that we are responsible for it.
                    ergm.MPIcluster.started(TRUE)
-                   makeCluster(control$parallel,type="MPI")
+                   parallel::makeCluster(control$parallel,type="MPI")
                  }else
                    ergm.MPIcluster.started(FALSE)
-                   getMPIcluster()
+                   parallel::getMPIcluster()
                },
                SOCK={
-                 makeCluster(control$parallel,type="SOCK")
+                 parallel::makeCluster(control$parallel,type="SOCK")
                }
                )
   
@@ -66,21 +66,21 @@ ergm.getCluster <- function(control, verbose=FALSE){
   ergm.MCMC.packagenames(control$MCMC.packagenames)
   for(pkg in ergm.MCMC.packagenames()){
     # Try loading from the same location as the master.
-    attached <- unlist(clusterCall(cl, require,
+    attached <- unlist(parallel::clusterCall(cl, require,
                                    package=pkg,
                                    character.only=TRUE,
                                    lib.loc=myLibLoc()))
     # If something failed, warn and try loading from anywhere.
     if(!all(attached)){
       if(verbose) cat("Failed to attach ergm on the slave nodes from the same location as the master node. Will try to load from anywhere in the library path.\n")
-      attached <- clusterCall(cl, require,
+      attached <- parallel::clusterCall(cl, require,
                             package=pkg,
                             character.only=TRUE)      
       if(!all(attached)) stop("Failed to attach ergm on one or more slave nodes. Make sure it's installed on or accessible from all of them and is in the library path.")
     }
     
     if(control$parallel.version.check){
-      slave.versions <- clusterCall(cl,packageVersion,pkg)
+      slave.versions <- parallel::clusterCall(cl,packageVersion,pkg)
       master.version <- packageVersion(pkg)
 
       if(!all(sapply(slave.versions,identical,master.version)))
@@ -99,12 +99,12 @@ ergm.stopCluster <- function(object, ...)
 ergm.stopCluster.MPIcluster <- function(object, ...){
   if(ergm.MPIcluster.started()){
     ergm.MPIcluster.started(FALSE)
-    stopCluster(object)
+    parallel::stopCluster(object)
   }
 }
 
 ergm.stopCluster.default <- function(object, ...){
-  stopCluster(object)
+  parallel::stopCluster(object)
 }
 
 
