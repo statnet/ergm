d63ad0d535ee8d9ffd7df5d5604539a4344d2275
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-08-21 08:07:56 +0000

    * Fixed the SAN maximum edges bug. * Reconciled SAN code to be more similar to the MCMC code in structure, variable names, etc.. * SAN C code now uses the MCMC_* and MH_* error and status codes.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@12148 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.san.R
+++ R/ergm.san.R
@@ -202,7 +202,8 @@ san.formula <- function(object, response=NULL, reference=~Bernoulli, constraints
                 as.integer(MHproposal$arguments$constraints$bd$minout), as.integer(MHproposal$arguments$constraints$bd$minin),
                 as.integer(MHproposal$arguments$constraints$bd$condAllDegExact),
                 as.integer(length(MHproposal$arguments$constraints$bd$attribs)), 
-                as.integer(maxedges), 
+                as.integer(maxedges),
+                status = integer(1),
                 PACKAGE="ergm")
       }else{
         z <- .C("WtSAN_wrapper",
@@ -232,11 +233,13 @@ san.formula <- function(object, response=NULL, reference=~Bernoulli, constraints
                 as.integer(MHproposal$arguments$constraints$bd$condAllDegExact),
                 as.integer(length(MHproposal$arguments$constraints$bd$attribs)), 
                 as.integer(maxedges), 
+                status = integer(1),
                 PACKAGE="ergm")
       }
 
-      if(z$newnwtails[1] <= maxedges) break
-      maxedges <- 5*maxedges
+      if(z$status==1) maxedges <- 5*maxedges
+      else if(z$status==0) break
+      else stop("Error in SAN C code.")
     }
     #
     #   Next update the network to be the final (possibly conditionally)
