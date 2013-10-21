f8cd77f2f55ed2d4c7643d070699a4bdc4d9c634
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-10-21 07:27:09 +0000

    Changed the network.update() function to construct a new network, copying over vertex and network attributes, rather than copying the whole network, then deleting edges.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@12256 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/network.update.R
+++ R/network.update.R
@@ -26,33 +26,22 @@
 #
 ###############################################################################
 
-network.update<-function(nw, newmatrix, matrix.type=NULL, output="network")
-{
-#  print(paste("old:",network.edgecount(nw)," new:", nrow(newmatrix),collapse=" "))
-  unw <- network.copy(as.network.uncompressed(nw))
+network.update<-function(nw, newmatrix, matrix.type=NULL, output="network", ignore.nattr=c("bipartite","directed","hyper","loops","mnext","multiple","n"), ignore.vattr=c()){
+  unw <- network.initialize(n=network.size(nw), directed = is.directed(nw), hyper = is.hyper(nw), loops = has.loops(nw),
+         multiple = is.multiplex(nw), bipartite = nw %n% "bipartite")
+  for(a in setdiff(list.network.attributes(nw),ignore.nattr)) unw <- set.network.attribute(unw, a, get.network.attribute(nw, a, unlist=FALSE))
+  for(a in setdiff(list.vertex.attributes(nw),ignore.vattr)) unw <- set.vertex.attribute(unw, a, get.vertex.attribute(nw, a, unlist=FALSE))
+
   if(is.null(matrix.type)){
     warning("Don't leave matrix type to chance! Pass matrix.type to network.update!")
     matrix.type <- which.matrix.type(newmatrix)
     if(nrow(newmatrix)==0){matrix.type <- "edgelist"}
   }
+  
   if(matrix.type=="adjacency" && all(newmatrix%in%c(0,1))){
     unw[,] <- newmatrix
-  }else if(matrix.type=="edgelist"){
-#  cnw <- as.matrix.network(nw,matrix.type="edgelist")
-#  unw[cnw[,2],cnw[,1]] <- 0
-#  unw[,] <- 0
-#  eid<-vector()
-#  for(i in 1:network.size(nw)){  
-#    eid <- c(eid,get.edgeIDs(unw,i))
-#  }
-   eid <- c(unlist(unw$iel),unlist(unw$oel))
-   delete.edges(unw,eid)
-   if(!is.null(newmatrix) && nrow(newmatrix)>0){
-#   unw[newmatrix] <- 1
-     
-    # *** don't forget - edges are given as tails -> heads now
+  }else if(matrix.type=="edgelist" && !is.null(newmatrix) && nrow(newmatrix)>0){
     add.edges(unw,tail=newmatrix[,1],head=newmatrix[,2])
-   }
   }
   if(!is.null(output) && output=="edgelist.compressed") 
     {unw <- as.edgelist.compressed(unw)}
