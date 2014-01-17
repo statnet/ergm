aa8f35993f07c2d72ff889cad72660fffe1bf228
Pavel N. Krivitsky <pavel@uow.edu.au>
2014-01-17 10:10:24 +0000

    Expanded the parallel.R test to also test Ticket #44 (Trac: 480).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/3.1@12491 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- tests/parallel.R
+++ tests/parallel.R
@@ -24,8 +24,27 @@ for(type in c("SOCK","MPI","MPI2")){
                eval.loglik=FALSE,
                control=control.ergm(MCMC.burnin=1000, MCMC.interval=10, MCMLE.maxit=2, MCMC.samplesize=1000, force.main=TRUE,
                  parallel=2, parallel.type=type))
+
   print(summary(gest))
   mcmc.diagnostics(gest)
+
+  # FIXME: Set seeds and replace with actual values?
+  sim.STAT.SEQ <- simulate(gest, nsim=5, control=control.simulate.ergm(parallel=2, parallel.type=type), statsonly=TRUE, sequential=TRUE)
+  stopifnot(nrow(sim.STAT.SEQ)==5)
+  print(sim.STAT.SEQ)
+
+  sim.STAT.seq <- simulate(gest, nsim=5, control=control.simulate.ergm(parallel=2, parallel.type=type), statsonly=TRUE, sequential=FALSE)
+  stopifnot(nrow(sim.STAT.seq)==5)
+  print(sim.STAT.seq)
+
+  sim.stat.SEQ <- simulate(gest, nsim=5, control=control.simulate.ergm(parallel=2, parallel.type=type), statsonly=FALSE, sequential=TRUE)
+  stopifnot(length(sim.stat.SEQ)==5)
+  print(sim.stat.SEQ)
+
+  sim.stat.seq <- simulate(gest, nsim=5, control=control.simulate.ergm(parallel=2, parallel.type=type), statsonly=FALSE, sequential=FALSE)
+  stopifnot(length(sim.stat.seq)==5)
+  print(sim.stat.seq)
+  
   if(exists("cl")){
     stopCluster(cl)
     rm(cl)
