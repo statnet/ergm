8ada3c21c31769b642df5de9cc025c233e925395
Jeffrey Horner <jeffrey.horner@gmail.com>
2014-05-30 20:23:09 +0000

    Update InitErgmTerm.edgecov with fix and explanation of non-existent network attributes. Fixes #72 (Trac: 866).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/3.1@12940 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitErgmTerm.R
+++ R/InitErgmTerm.R
@@ -1279,8 +1279,12 @@ InitErgmTerm.edgecov <- function(nw, arglist, ...) {
   ### Process the arguments
   if(is.network(a$x))
     xm<-as.matrix.network(a$x,matrix.type="adjacency",a$attrname)
-  else if(is.character(a$x))
+  else if(is.character(a$x)){
     xm<-get.network.attribute(nw,a$x)
+    if (is.null(xm)){
+      stop("There is no network attributed named ",a$x,call.=FALSE)
+    }
+  }
   else
     xm<-as.matrix(a$x)
   
