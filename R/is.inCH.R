2e6e46f2401801d30f93c789882cc4332ab7f285
Pavel N. Krivitsky <pavel@uow.edu.au>
2014-06-20 07:20:05 +0000

    is.inCH() no longer checks if Rglkpk is available (since it's now Imported).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@13098 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/is.inCH.R
+++ R/is.inCH.R
@@ -21,14 +21,11 @@ is.inCH <- function(p, M) {
          "of first argument.")
   R = NROW(M)
   C=length(p)+1
-  if(require("Rglpk", quietly = TRUE)) {
+  
 	ans <- Rglpk_solve_LP(obj=c(p,-1), mat=cbind(rbind(p,M),-1),
                               dir=as.vector(rep("<=",R+1)), rhs=as.vector(c(1,rep(0,R))),
                               max=TRUE, bounds=list(lower=list(ind=1:C,val=rep(-Inf,C))))
-  }else{
-     stop("The 'Rglpk' package must be installed if the ergm control parameter 'style'='stepping' ")
-  }
-
+  
   if(ans$optimum==0){x<-TRUE}  #if the max is zero, the point q is in the CH of the points p
   else{x<-FALSE}              #if the max is strictly positive, q is not in the CH of p
   x
