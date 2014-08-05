108a99a12dbdfa8520ff11c3a26d99887a4f81a3
Li Wang <lxwang@uw.edu>
2014-08-05 23:32:30 +0000

    Reverting the changes from ticket #71 (Trac: 821). As intended, simulate.ergm, by default, takes the control parameters from the ergm object. However, default ergm MCMC controls are still different from default simulate.formula controls.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/3.1@13222 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/control.simulate.R
+++ R/control.simulate.R
@@ -56,24 +56,21 @@
 #
 #########################################################################
 
+control.simulate<-control.simulate.formula<-control.simulate.formula.ergm<-function(MCMC.burnin=1000,
+                                                     MCMC.interval=1000,
+                                                     MCMC.prop.weights="default",
+                                                     MCMC.prop.args=list(),
 
-control.simulate<-
-  control.simulate.formula<-
-  control.simulate.formula.ergm<-function(MCMC.burnin=1000,
-                                          MCMC.interval=1000,
-                                          MCMC.prop.weights="default",
-                                          MCMC.prop.args=list(),
-                                          
-                                          MCMC.init.maxedges=20000,
-                                          MCMC.packagenames=c(),
-                                          
-                                          MCMC.runtime.traceplot=FALSE,  
-                                          network.output="network",
-                                          
-                                          parallel=0,
-                                          parallel.type=NULL,
-                                          parallel.version.check=TRUE,
-                                          ...){
+                                                     MCMC.init.maxedges=20000,
+                                                     MCMC.packagenames=c(),
+
+                                                     MCMC.runtime.traceplot=FALSE,  
+                                                     network.output="network",
+                                                     
+                                                     parallel=0,
+                                                     parallel.type=NULL,
+                                                     parallel.version.check=TRUE,
+                                                     ...){
   old.controls <- list(
                        maxedges="MCMC.init.maxedges",
                        prop.weights="MCMC.prop.weights",
@@ -99,17 +96,17 @@ control.simulate<-
   set.control.class("control.simulate.formula")
 }
 
-control.simulate.ergm<-function(MCMC.burnin=1000,
-                                MCMC.interval=1000,
-                                MCMC.prop.weights="default",
-                                MCMC.prop.args=list(),
+control.simulate.ergm<-function(MCMC.burnin=NULL,
+                                MCMC.interval=NULL,
+                                MCMC.prop.weights=NULL,
+                                MCMC.prop.args=NULL,
+
+                                MCMC.init.maxedges=NULL,
+                                MCMC.packagenames=NULL,
                                 
-                                MCMC.init.maxedges=20000,
-                                MCMC.packagenames=c(),
-                                
-                                MCMC.runtime.traceplot=FALSE,  
+                                MCMC.runtime.traceplot=FALSE,
                                 network.output="network",
-                                
+
                                 parallel=0,
                                 parallel.type=NULL,
                                 parallel.version.check=TRUE,
