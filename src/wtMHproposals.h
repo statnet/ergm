c45f5953e43f16e307d45a3cb0d72cf959bc35c4
Pavel N. Krivitsky <pavel@uow.edu.au>
2014-08-16 00:15:04 +0000

    Moved some rank-related files I had forgotten to ergm.rank.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@13257 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/wtMHproposals.h
+++ src/wtMHproposals.h
@@ -3,7 +3,6 @@
 
 #include "wtMHproposal.h"
 
-void MH_CompleteOrdering(WtMHproposal *MHp, WtNetwork *nwp);
 void MH_Unif(WtMHproposal *MHp, WtNetwork *nwp);
 void MH_UnifNonObserved(WtMHproposal *MHp, WtNetwork *nwp);
 void MH_DiscUnif(WtMHproposal *MHp, WtNetwork *nwp);
