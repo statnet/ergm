20c7e5f8572bc219d0ed5b55cbbdeba91f23cd3a
Pavel N. Krivitsky <pavel@uow.edu.au>
2014-11-27 11:03:48 +0000

    Some last-minute redactions and fixes: * Redacted experimental autoboot, logp rank, observation process. * Small fixes to NEWS file. * Hummel steplength is not disabled when Hotelling termination criterion is used. * Added a reference to Sahlin (2011) for the ESS burnin.
    Note that of these, only NEWS and references should be "backported" to trunk.
    
    
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/3.2@13494 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/ergm.MCMLE.R
+++ R/ergm.MCMLE.R
@@ -256,12 +256,10 @@ ergm.MCMLE <- function(init, nw, model,
       steplen.hist <- c(steplen.hist, adaptive.steplength)
     }else{
       steplen <- 
-        if(!(control$MCMLE.termination%in%c("Hotelling","none"))) 
         .Hummel.steplength(
           if(control$MCMLE.Hummel.esteq) esteq else statsmatrix.0[,!model$etamap$offsetmap,drop=FALSE], 
           if(control$MCMLE.Hummel.esteq) esteq.obs else statsmatrix.0.obs[,!model$etamap$offsetmap,drop=FALSE],
           control$MCMLE.steplength.margin, control$MCMLE.steplength) 
-        else control$MCMLE.steplength
       
       if(steplen==control$MCMLE.steplength || is.null(control$MCMLE.steplength.margin) || iteration==control$MCMLE.maxit) calc.MCSE <- TRUE
       
