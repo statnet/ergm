41d03ea986e9e390dd17beaa63fee360c08c2c63
Pavel N. Krivitsky <pavel@uow.edu.au>
2015-05-14 08:14:52 +0000

    Attempting to use Hummel or precision termination while setting MCMLE.steplength!=1 or MCMLE.steplength.margin=NULL is now an error. Fixes #187 (Trac: 1169).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@13727 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/control.ergm.R
+++ R/control.ergm.R
@@ -280,6 +280,9 @@ control.ergm<-function(drop=TRUE,
   for(arg in match.arg.pars)
     control[arg]<-list(match.arg(control[[arg]][1],eval(formal.args[[arg]])))
 
+  if((MCMLE.steplength!=1 || is.null(MCMLE.steplength.margin)) && MCMLE.termination %in% c("Hummel", "precision"))
+    stop("Hummel and precision-based termination require non-null MCMLE.steplength.margin and MCMLE.steplength = 1.")
+  
   set.control.class()
 }
 
