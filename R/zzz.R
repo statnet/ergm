c62bd600c1166becaf2c0b35368935d559276b07
Pavel N. Krivitsky <pavel@uow.edu.au>
2016-01-27 07:02:11 +0000

    ergm, ergm.count, and tergm turnks now all register their proposals, constraints, etc. on loading rather than on attachment. (ergm.rank was already doing that.) References #179 (Trac: 1134).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@14243 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/zzz.R
+++ R/zzz.R
@@ -13,7 +13,9 @@
     packageStartupMessage(sm)
     packageStartupMessage(paste(c(strwrap(paste("NOTE: If you use custom ERGM terms based on ",sQuote("ergm.userterms")," version prior to 3.1, you will need to perform a one-time update of the package boilerplate files (the files that you did not write or modify) from ",sQuote("ergm.userterms")," 3.1 or later. See help('eut-upgrade') for instructions.",sep="")),""),collapse="\n"))
   }
-  
+}
+
+.onLoad <- function(lib, pkg){  
   .RegisterMHPs()
   .RegisterConstraintImplications()
   .RegisterInitMethods()
