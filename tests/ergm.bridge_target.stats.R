722d0f24365755e2390f0eee32aef37e2cd99adf
Pavel N. Krivitsky <pavel@uow.edu.au>
2016-12-01 08:39:24 +0000

    Added a test for ergm likelihood calculation for when target.stats are set to an unattainable value.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@14457 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ tests/ergm.bridge_target.stats.R
@@ -0,0 +1,37 @@
+#  File tests/ergm.bridge_target.stats.R in package ergm, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2003-2015 Statnet Commons
+#######################################################################
+library(statnet.common)
+opttest({
+library(ergm)
+logit <- function(p) log(p/(1-p))
+expit <- function(e) exp(e)/(1+exp(e))
+l <- function(nw, ets=NULL, theta=NULL){
+  e <- NVL(ets, network.edgecount(nw))
+  d <- network.dyadcount(nw)
+  theta <- NVL(theta, logit(e/d))
+
+  e*log(expit(theta)) + (d-e)*log(expit(-theta))
+}
+
+data(florentine)
+y <- flomarriage
+
+# Attainable t.s..
+ts <- 3
+print(llk.ergm <- as.vector(logLik(ergm(flomarriage~edges, target.stats=ts))))
+print(llk <- l(y,ts))
+stopifnot(isTRUE(all.equal(llk,llk.ergm, check.attributes=FALSE)))
+
+# Unattainable t.s..
+ts <- 3.5
+print(llk.ergm <- as.vector(logLik(ergm(flomarriage~edges, target.stats=ts))))
+print(llk <- l(y,ts))
+stopifnot(isTRUE(all.equal(llk,llk.ergm, check.attributes=FALSE,tolerance=0.001)))
+}, "bridge sampling with target stats")
