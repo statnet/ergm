4be9f3ea35febb931a3df85b680b85049f39b712
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-02-02 02:14:35 +0000

    Tweaks to the ALLOC_AUX_SOCIOMATRIX macros.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@14553 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/wtchangestat.h
+++ src/wtchangestat.h
@@ -174,6 +174,41 @@ typedef struct WtModelTermstruct {
 #define GET_AUX_STORAGE(stored_type, store_into) stored_type *store_into = (stored_type *) nwp->aux_storage[(unsigned int) INPUT_PARAM[0]];
 #define GET_AUX_STORAGE_NUM(stored_type, store_into, ind) stored_type *store_into = (stored_type *) nwp->aux_storage[(unsigned int) INPUT_PARAM[ind]];
 
+/* Allocate a sociomatrix as auxiliary storage. */
+#define ALLOC_AUX_SOCIOMATRIX(stored_type, store_into)			\
+  /* Note: the following code first sets up a 2D array indexed from 0, then shifts all pointers by -1 so that sm[t][h] would work for vertex IDs. */ \
+  ALLOC_AUX_STORAGE(N_TAILS, stored_type*, store_into);			\
+  Dyad sm_size = BIPARTITE? N_TAILS*N_HEADS : DIRECTED ? N_NODES*N_NODES : N_NODES*(N_NODES+1)/2; /* For consistency, and possible future capabilities, include diagonal: */ \
+  ALLOC_STORAGE(sm_size, stored_type, data); /* A stored_type* to data. */ \
+  Dyad pos = 0;	  /* Start of the next row's data in the data vector. */ \
+  for(Vertex t=0; t<N_TAILS; t++){                                      \
+  /* First set up the pointer to the right location in the data vector, */ \
+  if(BIPARTITE){							\
+  store_into[t] = data+pos - N_TAILS; /* This is so that store_into[t][h=BIPARTITE] would address the 0th element of that row. */ \
+  pos += N_HEADS;							\
+  }else if(DIRECTED){							\
+    store_into[t] = data+pos;						\
+    pos += N_HEADS;							\
+  }else{ /* Undirected. */						\
+    store_into[t] = data+pos - t; /* tail <= head, so this is so that store_into[t][h=t] would address the 0th element of that row. */ \
+    pos += N_HEADS-t+1; /* Each row has N_NODES - t + 1 elements (including diagonal). */ \
+  }									\
+  store_into[t]--; /* Now, shift the pointer by -1. */			\
+  }									\
+									\
+  store_into--; /* Shift the pointer array by -1. */			\
+  nwp->aux_storage[(unsigned int) INPUT_PARAM[0]] = store_into; /* This is needed to make sure the pointer array itself is updated. */
+
+/* Free a sociomatrix in auxiliary storage. */
+#define FREE_AUX_SOCIOMATRIX						\
+  unsigned int myslot = (unsigned int) INPUT_PARAM[0];			\
+  /* If we hadn't shifted the pointers by -1, this would not have been necessary. */ \
+  GET_AUX_STORAGE(void*, sm);						\
+  free(sm + 1);								\
+  nwp->aux_storage[myslot] = NULL;					\
+  /* nwp->storage was not shifted, so it'll be freed automatically. */	
+
+
 #define INIT_STORAGE(stored_type, store_into, initialization_code)	\
   stored_type *store_into;						\
   if(!mtp->storage){							\
