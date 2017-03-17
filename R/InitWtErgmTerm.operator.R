1f6292a7f516649e92e5bb38163ed39d63d4380c
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-03-17 00:44:46 +0000

    Added a "nonzero" option to the import() valued term.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/operator_terms@14643 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitWtErgmTerm.operator.R
+++ R/InitWtErgmTerm.operator.R
@@ -1,17 +1,19 @@
 
 InitWtErgmTerm.import <- function(nw, arglist, response=NULL, ...){
   a <- check.ErgmTerm(nw, arglist,
-                      varnames = c("formula"),
-                      vartypes = c("formula"),
-                      defaultvalues = list(NULL),
-                      required = c(TRUE))
+                      varnames = c("formula", "form"),
+                      vartypes = c("formula", "character"),
+                      defaultvalues = list(NULL, "sum"),
+                      required = c(TRUE, FALSE))
+  form<-match.arg(a$form,c("sum","nonzero"))
+
   f <- a$formula
   if(length(f)==2) f <- nonsimp.update.formula(f, nw~.)
   else nw <- ergm.getnetwork(f)
 
   m <- ergm.getmodel(f, nw,...)
 
-  if(!is.dyad.independent(m)) stop("Only dyad-independent binary terms can be imported at this time.")
+  if(!is.dyad.independent(m) && form=="sum") stop("Only dyad-independent binary terms can be imported with form 'sum'.")
   
   Clist <- ergm.Cprepare(nw, m)
 
@@ -21,15 +23,18 @@ InitWtErgmTerm.import <- function(nw, arglist, response=NULL, ...){
   inputs <- c(Clist$nterms, fnames, snames, Clist$inputs)
 
   gs <- rep(0, Clist$nstats)
-  i <- 1
-  for (j in 1:length(m$terms)) {
-    tmp <- m$terms[[j]]
-    k <- tmp$inputs[2] # Number of statistics for this model term
-    if (!is.null(tmp$emptynwstats)) {
-      gs[i:(i+k-1)] <- gs[i:(i+k-1)] + tmp$emptynwstats
+
+  if(form=="nonzero"){
+    i <- 1
+    for (j in 1:length(m$terms)) {
+      tmp <- m$terms[[j]]
+      k <- tmp$inputs[2] # Number of statistics for this model term
+      if (!is.null(tmp$emptynwstats)) {
+        gs[i:(i+k-1)] <- gs[i:(i+k-1)] + tmp$emptynwstats
+      }
+      i <- i + k
     }
-    i <- i + k
   }
 
-  list(name="import_binary_term_sum", coef.names = paste0('sum.',m$coef.names,''), inputs=inputs, dependence=FALSE, emptynwstats = gs)
+  list(name=paste("import_binary_term",form,sep="_"), coef.names = paste0(form,'(',m$coef.names,')'), inputs=inputs, dependence=!is.dyad.independent(m), emptynwstats = gs)
 }
--- src/wtchangestats_operator.c
+++ src/wtchangestats_operator.c
@@ -1,8 +1,9 @@
-#include "wtchangestat.h"
-#include "wtmodel.h"
 
 #include "changestat.h"
 #include "model.h"
+#include "wtchangestat.h"
+#include "wtmodel.h"
+
 
 typedef struct{Network nw; Model *m;} StoreNetAndModel;
 
@@ -16,6 +17,15 @@ static inline unsigned char *unpack_doubletochar(double **x){
   (*x)+=l+1;
   return s;
 }
+
+/* import_binary_term_sum 
+
+   A term to wrap dyad-independent binary ergm terms by taking their
+   change statistic from an empty network (i.e., their equivalent
+   dyadic covariate value) and multiplying it by the difference
+   between the previous and the new dyad value. 
+
+*/
 
 WtI_CHANGESTAT_FN(i_import_binary_term_sum){
   double *inputs = INPUT_PARAM;
@@ -38,11 +48,12 @@ WtC_CHANGESTAT_FN(c_import_binary_term_sum){
   GET_STORAGE(StoreNetAndModel, store);
   Model *m = store->m;
   Network *mynwp = &(store->nw);
+  double oldweight = GETWT(tail,head);
     
   ChangeStats(1, &tail, &head, mynwp, m);
 
   for(unsigned int i=0; i<N_CHANGE_STATS; i++)
-    CHANGE_STAT[i] = (weight-GETWT(tail,head))*m->workspace[i];
+    CHANGE_STAT[i] = m->workspace[i]*(weight-oldweight);
 }
 
 WtU_CHANGESTAT_FN(u_import_binary_term_sum){
@@ -54,6 +65,74 @@ WtU_CHANGESTAT_FN(u_import_binary_term_sum){
 }
 
 WtF_CHANGESTAT_FN(f_import_binary_term_sum){
+  GET_STORAGE(StoreNetAndModel, store);
+  Model *m = store->m;
+  Network *mynwp = &(store->nw);
+
+  ModelDestroy(m, mynwp);
+  NetworkDestroy(mynwp);
+}
+
+/* import_binary_term_nonzero 
+
+   A term to wrap abitrary binary ergm terms by constructing a binary
+   network that mirrors the valued one in that it has an edge wherever
+   the value is not 0.
+
+   FIXME: Use auxiliary storage so that multiple terms can share the network.
+
+*/
+
+
+WtI_CHANGESTAT_FN(i_import_binary_term_nonzero){
+  double *inputs = INPUT_PARAM;
+  ALLOC_STORAGE(1, StoreNetAndModel, store);
+
+  int n_terms = *(inputs++);
+  char *fnames = (char *) unpack_doubletochar(&inputs);
+  char *snames = (char *) unpack_doubletochar(&inputs);
+  Model *m = store->m = ModelInitialize(fnames, snames, &inputs, n_terms);
+  free(fnames);
+  free(snames);
+
+  store->nw = NetworkInitialize(NULL, NULL, 0, N_NODES, DIRECTED, BIPARTITE, FALSE, 0, NULL, m->n_aux);
+  Network *mynwp = &(store->nw);
+  // FIXME: This is suboptimal, since all trees will be highly
+  // unbalanced.
+  EXEC_THROUGH_NET_EDGES(t, h, e, w, {
+      if(w!=0) ToggleEdge(t, h, mynwp);
+    });
+  
+  InitStats(mynwp, m);
+}
+
+WtC_CHANGESTAT_FN(c_import_binary_term_nonzero){
+  GET_STORAGE(StoreNetAndModel, store);
+  Model *m = store->m;
+  Network *mynwp = &(store->nw);
+  double oldweight = GETWT(tail,head);
+
+  if((weight!=0)!=(oldweight!=0)){ // If going from 0 to nonzero or vice versa...
+    ChangeStats(1, &tail, &head, mynwp, m);
+
+    for(unsigned int i=0; i<N_CHANGE_STATS; i++)
+      CHANGE_STAT[i] = m->workspace[i];
+  }
+}
+
+WtU_CHANGESTAT_FN(u_import_binary_term_nonzero){
+  GET_STORAGE(StoreNetAndModel, store);
+  Model *m = store->m;
+  Network *mynwp = &(store->nw);
+  double oldweight = GETWT(tail,head);
+
+  if((weight!=0)!=(oldweight!=0)){ // If going from 0 to nonzero or vice versa...
+    UPDATE_STORAGE(tail, head, m, mynwp);
+    ToggleEdge(tail, head, mynwp);
+  }
+}
+
+WtF_CHANGESTAT_FN(f_import_binary_term_nonzero){
   GET_STORAGE(StoreNetAndModel, store);
   Model *m = store->m;
   Network *mynwp = &(store->nw);
