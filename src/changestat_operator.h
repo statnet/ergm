672a791924d4e42765fd057cade9aaf1a10bec85
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-03-25 20:05:24 +0000

    Missed a merge conflict.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/operator_terms@14708 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/changestat_operator.h
+++ src/changestat_operator.h
@@ -1,6 +1,11 @@
+#ifndef CHANGESTAT_OPERATOR_H
+#define CHANGESTAT_OPERATOR_H
+
 #include "changestat.h"
 #include "model.h"
 #include "storage.h"
 
 unsigned char *unpack_strasdouble(double **x);
 Model *unpack_Modelasdouble(double **x);
+
+#endif
