5985dab1e4105930a2b43f58ff21abd7596ac11c
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-03-30 03:25:51 +0000

    Added two auxiliary terms: * .submodel(formula) exports an arbitrary Model that is kept in sync with the current network. * .summary(formula) exports a numeric vector that is kept in sync with the current network to show summary(formula). Terms to test these terms have also been added.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/operator_terms@14722 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitErgmTerm.operator.R
+++ R/InitErgmTerm.operator.R
@@ -6,6 +6,11 @@ pack.Clistasnum <- function(Clist){
 }
 
 
+## Creates a submodel that does exactly what the model terms passed to
+## it would have done.
+##
+## FIXME: Handle curved terms.
+
 InitErgmTerm.passthrough <- function(nw, arglist, response=NULL, ...){
   a <- check.ErgmTerm(nw, arglist,
                       varnames = c("formula"),
@@ -32,5 +37,118 @@ InitErgmTerm.passthrough <- function(nw, arglist, response=NULL, ...){
     i <- i + k
   }
 
-  list(name="passthrough_term", coef.names = paste0('meta(',m$coef.names,')'), inputs=inputs, dependence=!is.dyad.independent(m), emptynwstats = gs)
+  list(name="passthrough_term", coef.names = paste0('passthrough(',m$coef.names,')'), inputs=inputs, dependence=!is.dyad.independent(m), emptynwstats = gs)
 }
+
+## Creates a submodel that tracks the given formula.
+
+InitErgmTerm..submodel <- function(nw, arglist, response=NULL, ...){
+  a <- check.ErgmTerm(nw, arglist,
+                      varnames = c("formula"),
+                      vartypes = c("formula"),
+                      defaultvalues = list(NULL),
+                      required = c(TRUE))
+  f <- a$formula
+  if(length(f)==2) f <- nonsimp.update.formula(f, nw~.)
+  else nw <- ergm.getnetwork(f)
+
+  m <- ergm.getmodel(f, nw, response=response,...)
+  Clist <- ergm.Cprepare(nw, m, response=response)
+
+  inputs <- pack.Clistasnum(Clist)
+
+  gs <- rep(0, Clist$nstats)
+  i <- 1
+  for (j in 1:length(m$terms)) {
+    tmp <- m$terms[[j]]
+    k <- tmp$inputs[2] # Number of statistics for this model term
+    if (!is.null(tmp$emptynwstats)) {
+      gs[i:(i+k-1)] <- gs[i:(i+k-1)] + tmp$emptynwstats
+    }
+    i <- i + k
+  }
+
+  list(name="_submodel_term", coef.names = c(), inputs=inputs, dependence=!is.dyad.independent(m))
+}
+
+## Tests .submodel().
+
+InitErgmTerm.submodel.test <- function(nw, arglist, response=NULL, ...){
+  a <- check.ErgmTerm(nw, arglist,
+                      varnames = c("formula"),
+                      vartypes = c("formula"),
+                      defaultvalues = list(NULL),
+                      required = c(TRUE))
+  f <- a$formula
+  if(length(f)==2) f <- nonsimp.update.formula(f, nw~.)
+  else nw <- ergm.getnetwork(f)
+
+  m <- ergm.getmodel(f, nw, response=response,...)
+  Clist <- ergm.Cprepare(nw, m, response=response)
+
+  gs <- rep(0, Clist$nstats)
+  i <- 1
+  for (j in 1:length(m$terms)) {
+    tmp <- m$terms[[j]]
+    k <- tmp$inputs[2] # Number of statistics for this model term
+    if (!is.null(tmp$emptynwstats)) {
+      gs[i:(i+k-1)] <- gs[i:(i+k-1)] + tmp$emptynwstats
+    }
+    i <- i + k
+  }
+
+  list(name="submodel_test_term", coef.names = paste0("submod.test(",m$coef.names,")"), emptynwstats = gs, dependence=!is.dyad.independent(m), auxiliaries = ~.submodel(a$formula))
+}
+
+## An auxiliary that exports a double vector that contains the current
+## summary statistics of the network for the terms given in the
+## formula.
+
+InitErgmTerm..summary <- function(nw, arglist, response=NULL, ...){
+  a <- check.ErgmTerm(nw, arglist,
+                      varnames = c("formula"),
+                      vartypes = c("formula"),
+                      defaultvalues = list(NULL),
+                      required = c(TRUE))
+  f <- a$formula
+  if(length(f)==2) f <- nonsimp.update.formula(f, nw~.)
+  else nw <- ergm.getnetwork(f)
+
+  m <- ergm.getmodel(f, nw, response=response,...)
+  Clist <- ergm.Cprepare(nw, m, response=response)
+
+  inputs <- pack.Clistasnum(Clist)
+
+  gs <- rep(0, Clist$nstats)
+  i <- 1
+  for (j in 1:length(m$terms)) {
+    tmp <- m$terms[[j]]
+    k <- tmp$inputs[2] # Number of statistics for this model term
+    if (!is.null(tmp$emptynwstats)) {
+      gs[i:(i+k-1)] <- gs[i:(i+k-1)] + tmp$emptynwstats
+    }
+    i <- i + k
+  }
+
+  list(name="_summary_term", coef.names = c(), inputs=c(inputs,gs), dependence=!is.dyad.independent(m))
+}
+
+
+## A term to test (very verbosely) the correctness of the .summary() auxiliary.
+
+InitErgmTerm.summary.test <- function(nw, arglist, response=NULL, ...){
+  a <- check.ErgmTerm(nw, arglist,
+                      varnames = c("formula"),
+                      vartypes = c("formula"),
+                      defaultvalues = list(NULL, FALSE),
+                      required = c(TRUE, FALSE))
+
+  f <- a$formula
+  if(length(f)==2) f <- nonsimp.update.formula(f, nw~.)
+  else nw <- ergm.getnetwork(f)
+
+  m <- ergm.getmodel(f, nw, response=response,...)
+  
+  list(name="summary_test_term", coef.names = 'summ.test', inputs=c(coef.length.model(m)), dependence=!is.dyad.independent(m), auxiliaries=~.summary(a$formula))
+}
+
--- src/changestats_operator.c
+++ src/changestats_operator.c
@@ -1,14 +1,14 @@
 #include "changestat_operator.h"
 
-typedef struct{Model *m; unsigned int n_stats_1, n_stats_2;} StoreModelAnd2Stats;
+/* passthrough(formula) */
 
 I_CHANGESTAT_FN(i_passthrough_term){
   double *inputs = INPUT_PARAM;
-  GET_STORAGE(Model, m); // No need to allocate it: we are only storing a pointer to a model.
+  // No need to allocate it: we are only storing a pointer to a model.
 
-  mtp->storage = m = unpack_Modelasdouble(&inputs);
+  STORAGE = unpack_Modelasdouble(&inputs);
  
-  InitStats(nwp, m);
+  InitStats(nwp, STORAGE);
 }
 
 D_CHANGESTAT_FN(d_passthrough_term){
@@ -30,6 +30,117 @@ F_CHANGESTAT_FN(f_passthrough_term){
 
   ModelDestroy(m, nwp);
 
-  mtp->storage=NULL;
+  STORAGE = NULL;
 }
+
+/* .submodel(formula) */
+
+I_CHANGESTAT_FN(i__submodel_term){
+  double *inputs = INPUT_PARAM+1;
+  // No need to allocate it: we are only storing a pointer to a model.
+
+  AUX_STORAGE = unpack_Modelasdouble(&inputs);
+
+  InitStats(nwp, AUX_STORAGE);
+}
+
+U_CHANGESTAT_FN(u__submodel_term){
+  GET_AUX_STORAGE(Model, m);
+
+  UPDATE_STORAGE(tail, head, m, nwp);
+}
+
+F_CHANGESTAT_FN(f__submodel_term){
+  GET_AUX_STORAGE(Model, m);
+
+  ModelDestroy(m, nwp);
+
+  AUX_STORAGE=NULL;
+}
+
+/* submodel.test(formula) */
+
+D_CHANGESTAT_FN(d_submodel_test_term){
+  GET_AUX_STORAGE(Model, m);
+
+  double *tmp = m->workspace;
+  m->workspace = CHANGE_STAT;
+  ChangeStats(ntoggles, tails, heads, nwp, m);
+  m->workspace = tmp;
+}
+
+/* .summary(formula) */
+
+
+I_CHANGESTAT_FN(i__summary_term){
+  double *inputs = INPUT_PARAM+1;
+  GET_STORAGE(Model, m); // No need to allocate, since we just need a pointer.
+
+  // Unpack the submodel.
+  STORAGE = m = unpack_Modelasdouble(&inputs);
+  // Initialize empty network.
+  Network nw = NetworkInitialize(NULL, NULL, 0, N_NODES, DIRECTED, BIPARTITE, 0, 0, NULL);
+
+  // Initialize storage for submodel terms 
+  InitStats(&nw, m);
+
+  ALLOC_AUX_STORAGE(m->n_stats, double, stats);
+  memcpy(stats, inputs, m->n_stats*sizeof(double));
+
+  // Evaluate the initial summary statistics (the slow way).
+  for(Vertex tail=1; tail <= N_TAILS; tail++){
+    Vertex head;
+    Edge e;
+    STEP_THROUGH_OUTEDGES(tail, e, head) {
+      ChangeStats(1, &tail, &head, &nw, m);
+      for(unsigned int k=0; k<m->n_stats; k++)
+	stats[k] += m->workspace[k];
+      UPDATE_STORAGE(tail, head, m, &nw);
+      ToggleEdge(tail, head, &nw);
+    }
+  }
+  // Note that nw is now identitical to nwp.
+  NetworkDestroy(&nw);
+}
+
+U_CHANGESTAT_FN(u__summary_term){
+  GET_STORAGE(Model, m);
+  GET_AUX_STORAGE(double, stats);
+
+  ChangeStats(1, &tail, &head, nwp, m);
+  for(unsigned int k=0; k<m->n_stats; k++)
+    stats[k] += m->workspace[k];
+
+  UPDATE_STORAGE(tail, head, m, nwp);
+}
+
+F_CHANGESTAT_FN(f__summary_term){
+  GET_STORAGE(Model, m);
+
+  ModelDestroy(m, nwp);
+
+  STORAGE=NULL;
+}
+
+/* summary.test(formula) */
+
+C_CHANGESTAT_FN(c_summary_test_term){
+  GET_AUX_STORAGE(double, stats);
+  CHANGE_STAT[0] = 0;
+  /* Rprintf("Test .summary auxiliary: ["); */
+  for(unsigned int i=0; i<INPUT_PARAM[1]; i++){
+    Rprintf(" %f", stats[i]);
+  }
+  Rprintf(" \n");
+  /* Rprintf(" ]\n"); */
+}
+
+F_CHANGESTAT_FN(f_summary_test_term){
+  GET_AUX_STORAGE(double, stats);
+  /* Rprintf("Test .summary auxiliary (last): ["); */
+  for(unsigned int i=0; i<INPUT_PARAM[1]; i++) Rprintf(" %f", stats[i]);
+  Rprintf(" \n");
+  /* Rprintf(" ]\n"); */
+}
+
 
