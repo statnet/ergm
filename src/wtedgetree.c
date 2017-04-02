364cbc91d90f3055fb7d0979fdd29b558784e161
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-04-02 06:13:24 +0000

    Changed some missed free()s.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/trunk@14785 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/wtedgetree.c
+++ src/wtedgetree.c
@@ -100,12 +100,12 @@ WtNetwork WtNetworkInitializeD(double *tails, double *heads, double *weights, Ed
  void NetworkDestroy
 *******************/
 void WtNetworkDestroy(WtNetwork *nwp) {
-  free (nwp->indegree);
-  free (nwp->outdegree);
-  free (nwp->inedges);
-  free (nwp->outedges);
+  Free(nwp->indegree);
+  Free(nwp->outdegree);
+  Free(nwp->inedges);
+  Free(nwp->outedges);
   if(nwp->duration_info.lasttoggle){
-    free (nwp->duration_info.lasttoggle);
+    Free(nwp->duration_info.lasttoggle);
     nwp->duration_info.lasttoggle=NULL;
   }
 }
