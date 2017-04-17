fb0bc10da8e8acd860362a8b90b7823022ff1e23
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-04-17 21:03:30 +0000

    Added changestats_auxnet.h to exported header files (for the structs).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/operator_terms@14831 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ inst/include/changestats_auxnet.h
@@ -0,0 +1,35 @@
+#ifndef CHANGESTATS_AUXNET_H
+#define CHANGESTATS_AUXNET_H
+
+#include "edgetree.h"
+#include "changestat.h"
+#include "storage.h"
+
+typedef struct{Network nw; double *ref_el;} StoreNetAndRefEL;
+typedef struct{Network nw; double *b;} StoreNetAndBID;
+
+I_CHANGESTAT_FN(i__isociomatrix);
+U_CHANGESTAT_FN(u__isociomatrix);
+F_CHANGESTAT_FN(f__isociomatrix);
+
+I_CHANGESTAT_FN(i__discord_net);
+U_CHANGESTAT_FN(u__discord_net);
+F_CHANGESTAT_FN(f__discord_net);
+
+I_CHANGESTAT_FN(i__intersect_net);
+U_CHANGESTAT_FN(u__intersect_net);
+F_CHANGESTAT_FN(f__intersect_net);
+
+I_CHANGESTAT_FN(i__intersect_net_toggles_in_list);
+U_CHANGESTAT_FN(u__intersect_net_toggles_in_list);
+F_CHANGESTAT_FN(f__intersect_net_toggles_in_list);
+
+I_CHANGESTAT_FN(i__union_net);
+U_CHANGESTAT_FN(u__union_net);
+F_CHANGESTAT_FN(f__union_net);
+
+I_CHANGESTAT_FN(i__blockdiag_net);
+U_CHANGESTAT_FN(u__blockdiag_net);
+F_CHANGESTAT_FN(f__blockdiag_net);
+
+#endif
