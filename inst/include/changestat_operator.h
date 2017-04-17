c8d5e6bcaf12d9732c5bd7fbc9911c11ad025213
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-04-17 19:35:18 +0000

    Updated exported header files.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/operator_terms@14825 9cb281f4-553f-406f-8d7f-11f072bbfe46

---
+++ inst/include/changestat_operator.h
@@ -0,0 +1,9 @@
+#ifndef CHANGESTAT_OPERATOR_H
+#define CHANGESTAT_OPERATOR_H
+
+#include "model.h"
+#include "storage.h"
+
+Model *unpack_Model_as_double(double **x);
+
+#endif
---
+++ inst/include/wtchangestat_operator.h
@@ -0,0 +1,9 @@
+#ifndef WTCHANGESTAT_OPERATOR_H
+#define WTCHANGESTAT_OPERATOR_H
+
+#include "wtmodel.h"
+#include "storage.h"
+
+WtModel *unpack_WtModel_as_double(double **x);
+
+#endif
