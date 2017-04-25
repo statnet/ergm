ef45937887ca2aba42117870b1749a009a445bea
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-04-25 22:40:03 +0000

    Abstracted the code to wrap a curved model into a new function, passthrough.curved.ergm.model().
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/operator_terms@14848 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/InitErgmTerm.operator.R
+++ R/InitErgmTerm.operator.R
@@ -1,3 +1,16 @@
+#' Convert a string into a "standard" format for passing as a numeric variable.
+#'
+#' This function takes a character vector of length 1 and returns a numerical vector encoding it.
+#'
+#' @param s a character vector of length 1
+#'
+#' @return a numeric vector concatenating the following:
+#' * the number of characters in the input string and
+#' * the characters in the input string encoded using ASCII encoding.
+#' 
+#' This is intended to be decoded by `unpack_str_as_double()` C routines.
+#'
+#' @export
 pack.str_as_num <- function(s) c(nchar(s), strtoi(charToRaw(s), 16L))
 
 #' Serialize model information in a `Clist` into a numeric vector
@@ -24,11 +37,40 @@ pack.Clist_as_num <- function(Clist){
   c(Clist$nterms, fnames, snames, Clist$inputs)
 }
 
+#' Wrap a submodel's curved specification (if present) for output from an `InitErgmTerm` or `InitWtErgmTerm`.
+#'
+#' Given a `ergm` model and (optionally) a function with which to wrap
+#' parameter names, wrap the calls to its `ergm.eta()` and
+#' `ergm.etagrad()` into `map()` and `gradient()` functions, similarly
+#' with the `params` element.
+#'
+#' @param m an `ergm.model` object
+#' @param namewrap an optional function taking a character string and
+#'   returning a character string, gold on the model's curved
+#'   parameter names to wrap them.
+#'
+#' @return a list with elements `map`, `gradient`, and `params`
+#'   suitable for concatenating with an `InitErgmTerm` or
+#'   `InitWtErgmTerm` output list.
+passthrough.curved.ergm.model <- function(m, namewrap = identity){
+  
+  if(is.curved(m)){
+    map <- function(x, n, ...){
+      ergm.eta(x, m$etamap)
+    }
+    gradient <- function(x, n, ...){
+      ergm.etagrad(x, m$etamap)
+    }
+    params <- rep(list(NULL), coef.length.model(m))
+    names(params) <- sapply(.coef.names.model(m, canonical=FALSE), namewrap)
+  }else map <- gradient <- params <- NULL
+
+  list(map = map, gradient = gradient, params = params)
+}
 
 ## Creates a submodel that does exactly what the model terms passed to
 ## it would have done.
 ##
-## FIXME: Handle curved terms.
 
 InitErgmTerm.passthrough <- function(nw, arglist, response=NULL, ...){
   a <- check.ErgmTerm(nw, arglist,
@@ -46,19 +88,9 @@ InitErgmTerm.passthrough <- function(nw, arglist, response=NULL, ...){
   inputs <- pack.Clist_as_num(Clist)
   
   gs <- ergm.emptynwstats.model(m)
-
-  if(is.curved(m)){
-    map <- function(x, n, ...){
-      ergm.eta(x, m$etamap)
-    }
-    gradient <- function(x, n, ...){
-      ergm.etagrad(x, m$etamap)
-    }
-    params <- rep(list(NULL), coef.length.model(m))
-    names(params) <- .coef.names.model(m, canonical=FALSE)
-  }else map <- gradient <- params <- NULL
   
-  list(name="passthrough_term", coef.names = paste0('passthrough(',m$coef.names,')'), inputs=inputs, dependence=!is.dyad.independent(m), emptynwstats = gs, map = map, gradient = gradient, params = params)
+  c(list(name="passthrough_term", coef.names = paste0('passthrough(',m$coef.names,')'), inputs=inputs, dependence=!is.dyad.independent(m), emptynwstats = gs),
+    passthrough.curved.ergm.model(m, function(x) paste0('passthrough(',x,')')))
 }
 
 ## Creates a submodel that tracks the given formula.
