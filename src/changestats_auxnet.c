99c2abee051674d6e9ff5b370a0ae31d8c6498d9
Li Wang <lxwang@uw.edu>
2017-04-28 04:55:04 +0000

    Allow auxiliary storage networks (discord, intersect, union) to export their reference edgelists.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm/branches/operator_terms@14855 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- src/changestats_auxnet.c
+++ src/changestats_auxnet.c
@@ -22,86 +22,115 @@ F_CHANGESTAT_FN(f__isociomatrix){
   FREE_AUX_SOCIOMATRIX;
 }
 
-
+// sets aux network to y0 XOR y1
 I_CHANGESTAT_FN(i__discord_net){
-  ALLOC_AUX_STORAGE(1, Network, dnwp);
+  ALLOC_AUX_STORAGE(1, StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  double *ref_el = storage->ref_el = INPUT_PARAM + 1;
   NetworkCopy(dnwp, nwp);
-  Edge nedges = INPUT_PARAM[1];
+  
+  Edge nedges = *ref_el;
   for(Edge i=0; i<nedges; i++){
-    Vertex tail=INPUT_PARAM[2+i], head=INPUT_PARAM[2+nedges+i];
+    Vertex tail=ref_el[1+i], head=ref_el[1+nedges+i];
     ToggleEdge(tail,head, dnwp);
   }
 }
 
 U_CHANGESTAT_FN(u__discord_net){
-  GET_AUX_STORAGE(Network, dnwp);
-
+  GET_AUX_STORAGE(StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  
   ToggleEdge(tail, head, dnwp);
 }
 
 F_CHANGESTAT_FN(f__discord_net){
   GET_AUX_STORAGE(Network, dnwp);
-
+  
   NetworkDestroy(dnwp);
 }
 
+// Initialize empty aux network. Then loop through the edges of y0 (ref_el).
+// If the edge also exists in y1, then toggle it off in dnwp.
+// The storage dnwp should be initialized as y0&y1 at the end.
 I_CHANGESTAT_FN(i__intersect_net){
-  ALLOC_AUX_STORAGE(1, Network, dnwp);
+  ALLOC_AUX_STORAGE(1, StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  double *ref_el = storage->ref_el = INPUT_PARAM + 1;
+  
   *dnwp = NetworkInitialize(NULL, NULL, 0, N_NODES, DIRECTED, BIPARTITE, 0, 0, NULL);
-  Edge nedges = INPUT_PARAM[1];
+  
+  Edge nedges = *ref_el;
   for(Edge i=0; i<nedges; i++){
-    Vertex tail=INPUT_PARAM[2+i], head=INPUT_PARAM[2+nedges+i];
-    if(IS_OUTEDGE(tail, head)!=0)
+    Vertex tail=ref_el[1+i], head=ref_el[1+nedges+i];
+    if(IS_OUTEDGE(tail, head)) {
       ToggleEdge(tail,head, dnwp);
+    }
   }
 }
 
 U_CHANGESTAT_FN(u__intersect_net){
-  GET_AUX_STORAGE(Network, dnwp);
-
-  if(dEdgeListSearch(tail, head, INPUT_PARAM+1))
+  GET_AUX_STORAGE(StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  double *ref_el = storage->ref_el;
+  // only toggle if the edge is in y0. otherwise changing y1 won't matter.
+  if(dEdgeListSearch(tail, head, ref_el))
     ToggleEdge(tail, head, dnwp);
 }
 
 F_CHANGESTAT_FN(f__intersect_net){
-  GET_AUX_STORAGE(Network, dnwp);
-
+  GET_AUX_STORAGE(StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  
   NetworkDestroy(dnwp);
 }
 
 I_CHANGESTAT_FN(i__intersect_net_toggles_in_list){
-  ALLOC_AUX_STORAGE(1, Network, dnwp);
+  //Rprintf("allocating intersect_net_tog\n");
+  ALLOC_AUX_STORAGE(1, StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  double *ref_el = storage->ref_el = INPUT_PARAM + 1;
+  
   *dnwp = NetworkInitialize(NULL, NULL, 0, N_NODES, DIRECTED, BIPARTITE, 0, 0, NULL);
-  Edge nedges = INPUT_PARAM[1];
+  
+  Edge nedges = *ref_el;
   for(Edge i=0; i<nedges; i++){
-    Vertex tail=INPUT_PARAM[2+i], head=INPUT_PARAM[2+nedges+i];
-    if(IS_OUTEDGE(tail, head)!=0)
+    Vertex tail=ref_el[1+i], head=ref_el[1+nedges+i];
+    if(IS_OUTEDGE(tail, head)) {
       ToggleEdge(tail,head, dnwp);
+    }
   }
 }
 
 U_CHANGESTAT_FN(u__intersect_net_toggles_in_list){
-  GET_AUX_STORAGE(Network, dnwp);
-
+  GET_AUX_STORAGE(StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  
   ToggleEdge(tail, head, dnwp);
 }
 
 F_CHANGESTAT_FN(f__intersect_net_toggles_in_list){
-  GET_AUX_STORAGE(Network, dnwp);
-
+  GET_AUX_STORAGE(StoreNetAndRefEL, storage);
+  Network *dnwp = &(storage->nw);
+  
   NetworkDestroy(dnwp);
 }
 
+// Initialize aux network to y1. Then loop through the edges of y0 (ref_el).
+// If the edge does not exists in y1, then toggle it on in aux network.
+// The storage dnwp should be initialized as y0|y1 at the end.
 I_CHANGESTAT_FN(i__union_net){
   ALLOC_AUX_STORAGE(1, StoreNetAndRefEL, storage);
   Network *dnwp = &(storage->nw);
   double *ref_el = storage->ref_el = INPUT_PARAM + 1;
+  
   NetworkCopy(dnwp, nwp);
+  
   Edge nedges = *ref_el;
   for(Edge i=0; i<nedges; i++){
     Vertex tail=ref_el[1+i], head=ref_el[1+nedges+i];
-    if(IS_OUTEDGE(tail, head)==0)
+    if(IS_OUTEDGE(tail, head)==0) {
       ToggleEdge(tail,head, dnwp);
+    }
   }
 }
 
@@ -109,7 +138,7 @@ U_CHANGESTAT_FN(u__union_net){
   GET_AUX_STORAGE(StoreNetAndRefEL, storage);
   Network *dnwp = &(storage->nw);
   double *ref_el = storage->ref_el;
-
+  // If the edge is in y0, changing y1 won't matter.
   if(dEdgeListSearch(tail, head, ref_el)==0)
     ToggleEdge(tail, head, dnwp);
 }
@@ -117,7 +146,7 @@ U_CHANGESTAT_FN(u__union_net){
 F_CHANGESTAT_FN(f__union_net){
   GET_AUX_STORAGE(StoreNetAndRefEL, storage);
   Network *dnwp = &(storage->nw);
-
+  
   NetworkDestroy(dnwp);
 }
 
