882580a8a73e423eff9b5577d67e2644e5d2d27a
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-08-21 11:40:59 +1000

    lCMB term is now shifted by -log((# total layers)!).

--- tests/multilayer.R
+++ tests/multilayer.R
@@ -69,7 +69,7 @@ m2 <- as.matrix(samplk2)
 m3 <- as.matrix(samplk3)
 msum <- m1 + m2 + m3
 diag(msum) <- NA
-(logic <- sum(lfactorial(msum) + lfactorial(3-msum), na.rm=TRUE))
+(logic <- sum(lfactorial(msum) + lfactorial(3-msum) - lfactorial(3), na.rm=TRUE))
 stopifnot(isTRUE(all.equal(layer, logic, check.attributes=FALSE)))
 
 data(florentine)
