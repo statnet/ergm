74f8721f0f2ecb97d74d8064244b7465acdb8327
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-09-13 12:45:20 +1000

    Rewrote the C MPLE to use RLE representation of toggleable dyads.

--- src/MPLE.c
+++ src/MPLE.c
@@ -9,7 +9,7 @@
  */
 #include "MPLE.h"
 #include "changestat.h"
-
+#include "ergm_dyadRLE.h"
 /* *****************
  void MPLE_wrapper
 
@@ -34,7 +34,7 @@
    tails before heads now */
 
 void MPLE_wrapper(int *tails, int *heads, int *dnedges,
-		  int *wl, int *lel,
+		  double *wl,
 		  int *dn, int *dflag, int *bipartite, int *nterms, 
 		  char **funnames, char **sonames, double *inputs,  
 		  int *responsevec, double *covmat,
@@ -46,14 +46,15 @@ void MPLE_wrapper(int *tails, int *heads, int *dnedges,
   int directed_flag = *dflag;
   Vertex bip = (Vertex) *bipartite;
   Model *m;
+  double *tmp = wl;
+  BoolRLESqMatrixD wlm = unpack_BoolRLESqMatrixD(&tmp);
 
   GetRNGstate(); /* Necessary for R random number generator */
   nw[0]=NetworkInitialize(tails, heads, n_edges,
                           n_nodes, directed_flag, bip, 0, 0, NULL);
   m=ModelInitialize(*funnames, *sonames, &inputs, *nterms);
   
-  if(*wl) MpleInit_hash_wl(responsevec, covmat, weightsvector, lel, *maxDyadTypes, nw, m); 
-  else MpleInit_hash_bl(responsevec, covmat, weightsvector, lel, *maxDyads, *maxDyadTypes, nw, m); 
+  MpleInit_hash_wl_RLE(responsevec, covmat, weightsvector, wlm, *maxDyads, *maxDyadTypes, nw, m); 
 
   ModelDestroy(m);
   NetworkDestroy(nw);
@@ -88,7 +89,7 @@ numRows should, ideally, be a power of 2, but doesn't have to be.
   return(hash % numRows);
 }
 
-/*R_INLINE*/ unsigned int insCovMatRow(double *newRow, double *matrix, unsigned int rowLength, unsigned int numRows,
+static inline unsigned int insCovMatRow(double *newRow, double *matrix, unsigned int rowLength, unsigned int numRows,
 			  int response, int *responsevec,
 			  int *weights ){
   unsigned int hash_pos = hashCovMatRow(newRow, rowLength, numRows, response), pos, round;
@@ -112,160 +113,62 @@ numRows should, ideally, be a power of 2, but doesn't have to be.
   return FALSE; /* Insertion unsuccessful: the table is full. */
 }
 
-/*****************
- void MpleInit_hash
-
- For finding the MPLE, an extra bit of initialization is required:  
- we must build the matrix of covariates to be used in the logistic 
- regression routine.  This matrix has #rows equal to the number of 
- possible edges in the network (n choose 2 for an undirected network) 
- and #cols equal to the number of network statistics in the model, 
- which is also the number of parameters.  The row for each edge in 
- the matrix should contain the changes in each of the statistics that 
- would occur if the given edge is toggled from 0 to 1, leaving all 
- other edges as they are in the observed network.  The response vector 
- for the logistic regression is simply the vector of indicators 
- giving the states of the edges in the observed network.
-
- It comes in two versions: one that iterates through all valid dyads,
- skipping those on the additional list, which is treated as a
- blacklist; and another that iterates only through dyads on the
- additional ist, which is treated as a whitelist.
-
-*****************/
-
-/* Maps a dyad ID to a tail and a head, serializing them in a
-   tail-major order.  Here, __s represent structural 0s.
-
-   Note that both undirected and directed unipartite networks are
-   serialized in the exact same manner. The t<h "filtering" or
-   swapping must be done later. (The alternative appears to require a
-   square root, which is inefficient.)
-
-   Bipartite(2 b1s, 3 b2s):
-
-          h
-
-   __ __ 00 01 02
-t  __ __ 03 04 05
-   __ __ __ __ __
-   __ __ __ __ __
-   __ __ __ __ __
-
-   Unipartite:
-
-          h
-
-   __ 00 01 02 03
-   04 __ 05 06 07
-t  08 09 __ 10 11 
-   12 13 14 __ 15 
-   16 17 18 19 __    
-
- */
-
-// Note that id MUST be an integer of some sort, or you'll get floating point division.
-#define DYADID2T(id) (BIPARTITE ? id / (N_NODES-BIPARTITE) + 1 : id / (N_NODES-1) + 1)
-#define DYADID2H(id) (BIPARTITE ? id % (N_NODES-BIPARTITE) + 1 + BIPARTITE : \
-		      id % (N_NODES-1) + 1 + (id % (N_NODES-1) >= id / (N_NODES-1)))
-
-
 // Euclid's Algorithm to compute Greatest Common Divisor of a and b.
 unsigned long int gcd(unsigned long int a, unsigned long int b){
   if(b==0) return a;
   else return gcd(b, a%b);
 }
 
-void MpleInit_hash_bl(int *responsevec, double *covmat, int *weightsvector,
-		      int *bl, 
-		      Edge maxDyads, Edge maxDyadTypes, Network *nwp, Model *m){
+void MpleInit_hash_wl_RLE(int *responsevec, double *covmat, int *weightsvector,
+			  BoolRLESqMatrixD *wl, 
+			  Edge maxDyads, Edge maxDyadTypes, Network *nwp, Model *m){
   double *newRow = (double *) R_alloc(m->n_stats,sizeof(double));
   /* Note:  This function uses macros found in changestat.h */
 
-  Dyad dc = DYADCOUNT(N_NODES, BIPARTITE, DIRECTED);
-  if(!BIPARTITE && !DIRECTED){
-    dc*=2;
-    maxDyads*=2;
-  }
+  // Number of free dyads.
+  Dyad dc = wl->ndyads;
   
-  // Find a number relatively prime with the dyad count:
+  // Find a number relatively prime with the free dyad count:
   Edge step = MAX(N_NODES/3,2);
   while(gcd(dc,step)!=1) step++;
 
-  for(Dyad d = 0, di = 0; d < MIN(maxDyads,dc); d++, di=(di+step)%dc){
-    Vertex t=DYADID2T(di), h=DYADID2H(di);
-    //Rprintf("d=%d di=%d t=%d h=%d ", d,di,t,h);
-    if((!DIRECTED && t>=h) || iEdgeListSearch(t,h,bl)) continue;
-
-    int response = IS_OUTEDGE(t,h);
-    unsigned int totalStats = 0;
-    /* Let mtp loop through each model term */
-    for (ModelTerm *mtp=m->termarray; mtp < m->termarray + m->n_terms; mtp++){
-      mtp->dstats = newRow + totalStats;
-      /* Now call d_xxx function, which updates mtp->dstats to reflect
-	 changing the current dyad.  */
-      (*(mtp->d_func))(1, &t, &h, mtp, nwp);
-      /* dstats values reflect changes in current dyad; for MPLE, 
-	 values must reflect going from 0 to 1.  Thus, we have to reverse 
-	 the sign of dstats whenever the current edge exists. */
-      if(response){
+  { // Start a scope for local variables.
+    Vertex t, h;
+    GetRandDyadRLED(&t,&h, wl); // Find a random starting point.
+    Dyad d = TH2Dyad(nwp->n_nodes, t,h);
+    RLERun r=0;
+    
+    for(Dyad i = 0; i < MIN(maxDyads,dc); i++, d=NextDyadRLED(d, step, wl, &r)){
+      TH2Dyad(nwp->n_nodes, t,h);
+      
+      int response = IS_OUTEDGE(t,h);
+      unsigned int totalStats = 0;
+      /* Let mtp loop through each model term */
+      for (ModelTerm *mtp=m->termarray; mtp < m->termarray + m->n_terms; mtp++){
+	mtp->dstats = newRow + totalStats;
+	/* Now call d_xxx function, which updates mtp->dstats to reflect
+	   changing the current dyad.  */
+	(*(mtp->d_func))(1, &t, &h, mtp, nwp);
+	/* dstats values reflect changes in current dyad; for MPLE, 
+	   values must reflect going from 0 to 1.  Thus, we have to reverse 
+	   the sign of dstats whenever the current edge exists. */
+	if(response){
 	  for(unsigned int l=0; l<mtp->nstats; l++){
 	    mtp->dstats[l] = -mtp->dstats[l];
 	  }
-      }
-      /* Update mtp->dstats pointer to skip ahead by mtp->nstats */
-      totalStats += mtp->nstats; 
-    }
-    /* In next command, if there is an offset vector then its total
-       number of entries should match the number of times through the 
-       inner loop (i.e., the number of dyads in the network) */          
-    if(!insCovMatRow(newRow, covmat, m->n_stats,
-		     maxDyadTypes, response, 
-		     responsevec, weightsvector)) {
-      warning("Too many unique dyads. MPLE is approximate, and MPLE standard errors are suspect.");
-      break;
-    }
-  }
-}
-
-void MpleInit_hash_wl(int *responsevec, double *covmat, int *weightsvector,
-		      int *wl, 
-		      Edge maxDyadTypes, Network *nwp, Model *m){
-  double *newRow = (double *) R_alloc(m->n_stats,sizeof(double));
-  /* Note:  This function uses macros found in changestat.h */
-  Edge wledges = wl[0];
-  wl++;
-
-  for(Edge wlpos = 0; wlpos < wledges; wlpos++){
-    Vertex t=wl[wlpos], h=(wl+wledges)[wlpos];
-    
-    int response = IS_OUTEDGE(t,h);
-    unsigned int totalStats = 0;
-    /* Let mtp loop through each model term */
-    for (ModelTerm *mtp=m->termarray; mtp < m->termarray + m->n_terms; mtp++){
-      mtp->dstats = newRow + totalStats;
-      /* Now call d_xxx function, which updates mtp->dstats to reflect
-	 changing the current dyad.  */
-      (*(mtp->d_func))(1, &t, &h, mtp, nwp);
-      /* dstats values reflect changes in current dyad; for MPLE, 
-	 values must reflect going from 0 to 1.  Thus, we have to reverse 
-	 the sign of dstats whenever the current edge exists. */
-      if(response){
-	for(unsigned int l=0; l<mtp->nstats; l++){
-	  mtp->dstats[l] = -mtp->dstats[l];
 	}
+	/* Update mtp->dstats pointer to skip ahead by mtp->nstats */
+	totalStats += mtp->nstats; 
       }
-      /* Update mtp->dstats pointer to skip ahead by mtp->nstats */
-      totalStats += mtp->nstats; 
-    }
-    /* In next command, if there is an offset vector then its total
-       number of entries should match the number of times through the 
-       inner loop (i.e., the number of dyads in the network) */          
-    if(!insCovMatRow(newRow, covmat, m->n_stats,
-		     maxDyadTypes, response, 
-		     responsevec, weightsvector)) {
-      warning("Too many unique dyads. MPLE is approximate, and MPLE standard errors are suspect.");
-      break;
+      /* In next command, if there is an offset vector then its total
+	 number of entries should match the number of times through the 
+	 inner loop (i.e., the number of dyads in the network) */          
+      if(!insCovMatRow(newRow, covmat, m->n_stats,
+		       maxDyadTypes, response, 
+		       responsevec, weightsvector)) {
+	warning("Too many unique dyads. MPLE is approximate, and MPLE standard errors are suspect.");
+	break;
+      }
     }
   }
 }
--- src/ergm_dyadRLE.h
+++ src/ergm_dyadRLE.h
@@ -18,9 +18,9 @@
 
    x[1] = number of nonzero dyads (i.e., sum of all run lengths)
 
-   x[2 : 2+r] = an r-vector whose 0th element is 1 and whose i'th
+   x[2 : 2+r] = an r-vector whose i'th
      element is the index of the first cell (listed in column-major
-     order, indexed from 1) of a run.
+     order, indexed from 1) of the i+1st run.
 
    x[2+r+1 : 2+r+1+r+1] = an r+1-vector of cumulative run lengths, which also serve as scaled cumulative probabilities for
      selecting each run in the R code. (The 0th element
@@ -28,6 +28,18 @@
      sum of all run lengths, etc.)
 
 */
+
+#define TH2Dyad(n, tail, head) ((tail-1)+m->n*(Dyad)(head-1) + 1)
+#define Dyad2T(n, d) ((Vertex)((d-1)%(Vertex)n) + 1)
+#define Dyad2H(n, d) ((Vertex)(((Dyad)(d-1))/(Vertex)n) + 1)
+
+static inline void Dyad2TH(Vertex *tail, Vertex *head, Dyad d, Vertex n){
+  // Quotient is the column (head) and remainder is the row (tail), counted from 0.
+  ldiv_t q = ldiv(d-1, n);
+  
+  *tail = q.rem + 1;
+  *head = q.quot + 1;
+}
 
 typedef unsigned int RLERun;
 
@@ -72,26 +84,22 @@ Generate a random dyad that belongs to a run
 @param head pointer to which to assign the head value
 @param r RLE information
 */
-static inline void GetRandDyadRLE(Vertex *tail, Vertex *head, const BoolRLESqMatrixD *m){
+static inline void GetRandDyadRLED(Vertex *tail, Vertex *head, const BoolRLESqMatrixD *m){
   // Select a dyad index at random
-  Dyad d = unif_rand() * m->ndyads + 1;
+  Dyad i = unif_rand() * m->ndyads + 1;
 
   // Find the correct run via binary search.
   // FIXME: Radix search could be faster than O(log(nruns))?
   RLERun l=1, h=m->nruns;
   while(l!=h){
     RLERun r = (l+h)/2;
-    if(d>m->cumlens[r]) l=r+1; else h=r;
+    if(i>m->cumlens[r]) l=r+1; else h=r;
   }
 
   // Dyad ID
-  d = m->starts[l-1] + (d-m->cumlens[l-1]);
+  Dyad d = m->starts[l-1] + (i-m->cumlens[l-1]);
 
-  // Quotion is the column (head) and remainder is the row (tail), counted from 0.
-  ldiv_t q = ldiv(d-1, m->n);
-  
-  *tail = q.rem + 1;
-  *head = q.quot + 1;
+  Dyad2TH(tail, head, d, m->n);
 }
 
 
@@ -100,11 +108,11 @@ Test if a dyad is present in a run
 
 @param tail tail index (from 1)
 @param head head index (from 1)
-@param r RLE information
+@param m RLE information
 */
-static inline unsigned int GetDyadRLE(Vertex tail, Vertex head, const BoolRLESqMatrixD *m){
-  Dyad d = (tail-1)+m->n*(Dyad)(head-1) + 1;
-
+static inline unsigned int GetDyadRLED(Vertex tail, Vertex head, const BoolRLESqMatrixD *m){
+  Dyad d = TH2Dyad(m->n, tail, head);
+  
   // Find the correct run via binary search.
   // FIXME: Radix search could be faster than O(log(nruns))?
   RLERun l=1, h=m->nruns;
@@ -118,6 +126,46 @@ static inline unsigned int GetDyadRLE(Vertex tail, Vertex head, const BoolRLESqM
   return (d-m->starts[l-1]) < m->cumlens[l]-m->cumlens[l-1];
 }
 
+/**
+Advance to the strideth next nonzero dyad index 
 
+@param d current dyad's number
+@param stride how much to advance
+@param m dyad matrix
+@param hint a pointer to an integer containing the RLE run, which is updated; set to 0 to initialise and to NULL to disable
+*/
+static inline Dyad NextDyadRLED(Dyad d, Dyad stride, const BoolRLESqMatrixD *m, RLERun *hint){
+  RLERun l;
+  if(hint && hint!=0){
+    l = *hint;
+  }else{
+    l = 1;
+    RLERun h=m->nruns;
+    while(l!=h){
+      RLERun r = (l+h)/2;
+      if(d<m->starts[r-1]) h=r-1; else l=r;
+    }
+  }
+
+  // 0-based index of the current dyad in the list of nonzero dyads.
+  Dyad di = d - m->starts[l-1] + m->cumlens[l-1];
+  // 0-based index of the next dyad in the list of nonzero dyads.
+  Dyad nxtdi = (di+stride) % m->ndyads;
+
+  RLERun nxtl;  
+  if(nxtdi < di){ // We've wrapped around.
+    nxtl = 1;
+  }else{ // We haven't wrapped around.
+    nxtl = l;
+  }
+
+  while(nxtdi > m->cumlens[nxtl]) nxtl++;
+  // nxtl is now the 1-based run index of the next dyad.
+
+  // Update the hint if active.
+  if(hint) *hint = nxtl;
+  
+  return nxtdi - m->cumlens[l-1] + m->starts[l-1];
+}
 
 #endif // _ERGM_DYADRLE_H_
