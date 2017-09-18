0c8e791b581720c2cef3d8d53e531b710f03f45c
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-09-18 21:44:55 +1000

    Added a debug function to print an RLE-compressed dyad matrix.

---
+++ src/ergm_dyadRLE.c
@@ -0,0 +1,25 @@
+#include <R.h>
+#include "ergm_dyadRLE.h"
+
+void PrintBoolRLESqMatrixD(const BoolRLESqMatrixD *m){
+  Rprintf("Note: the following matrix is printed transposed:\n");
+  Dyad d = 1, dmax = m->n*m->n;
+  for(RLERun r = 1; r <= m->nruns; r++){
+    while(d < m->starts[r-1]){
+      Rprintf(".");
+      if(d%m->n == 0) Rprintf("\n");
+      d++;
+    }
+    Dyad rend =  m->starts[r-1]+m->cumlens[r]-m->cumlens[r-1];
+    while(d < rend){
+      Rprintf("*");
+      if(d%m->n == 0) Rprintf("\n");
+      d++;
+    }
+  }
+  while(d <= dmax){
+    Rprintf(".");
+    if(d%m->n == 0) Rprintf("\n");
+    d++;
+  }
+}
