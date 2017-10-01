e8e82d9b785cc4089556b6fcf6583ba4001c06b8
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-10-01 22:29:43 +1100

    Converted print.ergm() to roxygen.

--- R/print.ergm.R
+++ R/print.ergm.R
@@ -21,14 +21,20 @@
 # 
 ###############################################################################
 
+
+
+#' @describeIn ergm
+#' 
+#' Automatically called when an object of class \code{\link{ergm}} is printed.
+#' Currently, summarizes the size of the MCMC sample, the \eqn{\theta}
+#' vector governing the selection of the sample, and the Monte Carlo MLE. The optional `digits` argument specifies the significant digits for coefficients
+#' @export
 print.ergm <- function (x, digits = max(3, getOption("digits") - 3), ...) {
-#  if(!is.latent(x) || is.null(x$Z.mle)) {                             
-  if(is.null(x$Z.mle)) {
    if(is.matrix(x$sample)){
 #    if(!is.matrix(x$thetasample) && !is.null(x$iterations)){
 #     cat("Newton-Raphson iterations: ", x$iterations[1], "\n")
 #    }
-    cat("MCMC sample of size", x$samplesize, "based on: \n")
+    cat("MCMC sample of size", nrow(as.matrix(x$sample)), "based on: \n")
     print.default(format(x$MCMCtheta, digits = digits), print.gap = 2, 
         quote = FALSE)
     cat("\nMonte Carlo MLE Coefficients:\n")
@@ -43,26 +49,5 @@ print.ergm <- function (x, digits = max(3, getOption("digits") - 3), ...) {
      print.default(format(x$coef, digits = digits), print.gap = 2, 
          quote = FALSE)
    }
-  }
-  else {
-#    if (!is.null(x$iterations)) {
-#      cat("Newton-Raphson iterations: ", x$iterations[1], "\n")
-#    }
-    cat("MLE and Posterior Mean of Coefficients:\n")
-    temp <- cbind(x$MCMCtheta,x$coef)
-    colnames(temp) <- c("MLE","MAP")
-    print.default(format(temp, digits = digits), print.gap = 2,
-                  quote = FALSE)
-    scale.MLE <-  0
-    for(i in 1:ncol(x$Z.mle))
-      scale.MLE <- scale.MLE + sum(outer(x$Z.mle[,i],x$Z.mle[,i],function(x,y)abs(x-y)))
-    scale.MLE <- scale.MLE / choose(nrow(x$Z.mle),2)
-    cat("Scale = ",scale.MLE,"\n")
-    n <- network.size(x$newnetwork)
-    p <- NVL3(x$Z.pmode, ncol(.), 0)
-    r <- length(x$coef) + (n) * p - p*(p+1)/2
-    n <- n*(n-1)
-    cat("\nBIC = ",2 * x$mle.lik - r*log(n),"\n")
-  }
   invisible(x)
 }
