ff1c5162ca594416ea40376fa3ff8e957b9af93d
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-12-11 11:02:16 +1100

    Some optimizations for the shared partner cache.

--- src/changestats_spcache.c
+++ src/changestats_spcache.c
@@ -2,6 +2,30 @@
 #include "ergm_wtedgetree.h"
 #include "ergm_storage.h"
 
+// Increment the value of the edge by inc.
+static inline void WtIncEdge (Vertex tail, Vertex head, double inc, WtNetwork *nwp) 
+{
+  ENSURE_TH_ORDER;
+
+  if(inc==0) return;
+  
+  // Find the out-edge
+  Edge oe=WtEdgetreeSearch(tail,head,nwp->outedges);
+  if(oe){
+    // Find the corresponding in-edge.
+    Edge ie=WtEdgetreeSearch(head,tail,nwp->inedges);
+    double weight = nwp->outedges[oe].weight + inc;
+    if(weight==0){
+      // If the function is to set the edge value to 0, just delete it.
+      WtDeleteEdgeFromTrees(tail,head,nwp);
+    }else{
+      nwp->inedges[ie].weight=nwp->outedges[oe].weight = weight;
+    }
+  }else{
+    // Otherwise, create a new edge with that weight 0+inc==inc.
+    WtAddEdgeToTrees(tail,head,inc,nwp);
+  }
+}
 
 /* Construct and maintain a directed weighted network whose (i,j)
    value is the number of directed two-paths from i to j. */
@@ -9,10 +33,10 @@
 I_CHANGESTAT_FN(i__otp_wtnet){
   ALLOC_AUX_STORAGE(1, WtNetwork, wtnwp);
   *wtnwp = WtNetworkInitialize(NULL, NULL, NULL, 0, N_NODES, DIRECTED, BIPARTITE, 0, 0, NULL);
-  EXEC_THROUGH_NET_EDGES(i, j, e1, { // Since i->j
-      EXEC_THROUGH_FOUTEDGES(j, e2, k, { // and j->k
+  EXEC_THROUGH_NET_EDGES_PRE(i, j, e1, { // Since i->j
+      EXEC_THROUGH_FOUTEDGES_PRE(j, e2, k, { // and j->k
 	  if(i!=k)
-	    WtSetEdge(i,k,WtGetEdge(i,k,wtnwp)+1,wtnwp); // increment i->k.
+	    WtIncEdge(i,k,1,wtnwp); // increment i->k.
 	});
     });
 }
@@ -23,16 +47,16 @@ U_CHANGESTAT_FN(u__otp_wtnet){
 
   {
     // Update all t->h->k two-paths.
-    EXEC_THROUGH_FOUTEDGES(head, e, k, {
+    EXEC_THROUGH_FOUTEDGES_PRE(head, e, k, {
 	if(tail!=k)
-	  WtSetEdge(tail,k,WtGetEdge(tail,k,wtnwp)+echange,wtnwp);
+	  WtIncEdge(tail,k,echange,wtnwp);
       });
   }
   {
     // Update all k->t->h two-paths.
-    EXEC_THROUGH_FINEDGES(tail, e, k, {
+    EXEC_THROUGH_FINEDGES_PRE(tail, e, k, {
 	if(k!=head)
-	  WtSetEdge(k,head,WtGetEdge(k,head,wtnwp)+echange,wtnwp);
+	  WtIncEdge(k,head,echange,wtnwp);
       });
   }
 }
@@ -50,10 +74,10 @@ F_CHANGESTAT_FN(f__otp_wtnet){
 I_CHANGESTAT_FN(i__osp_wtnet){
   ALLOC_AUX_STORAGE(1, WtNetwork, wtnwp);
   *wtnwp = WtNetworkInitialize(NULL, NULL, NULL, 0, N_NODES, FALSE, BIPARTITE, 0, 0, NULL); // Always undirected.
-  EXEC_THROUGH_NET_EDGES(i, j, e1, { // Since i->j
-      EXEC_THROUGH_FINEDGES(j, e2, k, { // and k->j
+  EXEC_THROUGH_NET_EDGES_PRE(i, j, e1, { // Since i->j
+      EXEC_THROUGH_FINEDGES_PRE(j, e2, k, { // and k->j
 	  if(i<k) // Don't double-count.
-	    WtSetEdge(i,k,WtGetEdge(i,k,wtnwp)+1,wtnwp); // increment i-k.
+	    WtIncEdge(i,k,1,wtnwp); // increment i-k.
 	});
     });
 }
@@ -63,9 +87,9 @@ U_CHANGESTAT_FN(u__osp_wtnet){
   int echange = (IS_OUTEDGE(tail, head) == 0) ? 1 : -1;
 
   // Update all t->h<-k shared partners.
-  EXEC_THROUGH_FINEDGES(head, e, k, {
+  EXEC_THROUGH_FINEDGES_PRE(head, e, k, {
       if(tail!=k)
-	WtSetEdge(tail,k,WtGetEdge(tail,k,wtnwp)+echange,wtnwp);
+	WtIncEdge(tail,k,echange,wtnwp);
     });
 }
 
@@ -81,10 +105,10 @@ F_CHANGESTAT_FN(f__osp_wtnet){
 I_CHANGESTAT_FN(i__isp_wtnet){
   ALLOC_AUX_STORAGE(1, WtNetwork, wtnwp);
   *wtnwp = WtNetworkInitialize(NULL, NULL, NULL, 0, N_NODES, FALSE, BIPARTITE, 0, 0, NULL); // Always undirected.
-  EXEC_THROUGH_NET_EDGES(i, j, e1, { // Since i->j
-      EXEC_THROUGH_FOUTEDGES(i, e2, k, { // and i->k
+  EXEC_THROUGH_NET_EDGES_PRE(i, j, e1, { // Since i->j
+      EXEC_THROUGH_FOUTEDGES_PRE(i, e2, k, { // and i->k
 	  if(j<k) // Don't double-count.
-	    WtSetEdge(j,k,WtGetEdge(j,k,wtnwp)+1,wtnwp); // increment j-k.
+	    WtIncEdge(j,k,1,wtnwp); // increment j-k.
 	});
     });
 }
@@ -94,9 +118,9 @@ U_CHANGESTAT_FN(u__isp_wtnet){
   int echange = (IS_OUTEDGE(tail, head) == 0) ? 1 : -1;
 
   // Update all h<-t->k shared partners.
-  EXEC_THROUGH_FOUTEDGES(tail, e, k, {
+  EXEC_THROUGH_FOUTEDGES_PRE(tail, e, k, {
       if(head!=k)
-	WtSetEdge(head,k,WtGetEdge(head,k,wtnwp)+echange,wtnwp);
+	WtIncEdge(head,k,echange,wtnwp);
     });
 }
 
@@ -112,14 +136,14 @@ F_CHANGESTAT_FN(f__isp_wtnet){
 I_CHANGESTAT_FN(i__utp_wtnet){
   ALLOC_AUX_STORAGE(1, WtNetwork, wtnwp);
   *wtnwp = WtNetworkInitialize(NULL, NULL, NULL, 0, N_NODES, FALSE, BIPARTITE, 0, 0, NULL); // Always undirected.
-  EXEC_THROUGH_NET_EDGES(i, j, e1, { // Since i-j
-      EXEC_THROUGH_EDGES(i, e2, k, { // and i-k
+  EXEC_THROUGH_NET_EDGES_PRE(i, j, e1, { // Since i-j
+      EXEC_THROUGH_EDGES_PRE(i, e2, k, { // and i-k
 	  if(j<k)
-	    WtSetEdge(j,k,WtGetEdge(j,k,wtnwp)+1,wtnwp); // increment j-k.
+	    WtIncEdge(j,k,1,wtnwp); // increment j-k.
 	});
-      EXEC_THROUGH_EDGES(j, e2, k, { // and j-k
+      EXEC_THROUGH_EDGES_PRE(j, e2, k, { // and j-k
 	  if(i<k)
-	    WtSetEdge(i,k,WtGetEdge(i,k,wtnwp)+1,wtnwp); // increment i-k.
+	    WtIncEdge(i,k,1,wtnwp); // increment i-k.
 	});
     });
 }
@@ -129,15 +153,15 @@ U_CHANGESTAT_FN(u__utp_wtnet){
   int echange = (IS_OUTEDGE(tail, head) == 0) ? 1 : -1;
 
   // Update all h-t-k shared partners.
-  EXEC_THROUGH_EDGES(tail, e, k, {
+  EXEC_THROUGH_EDGES_PRE(tail, e, k, {
       if(head!=k)
-	WtSetEdge(head,k,WtGetEdge(head,k,wtnwp)+echange,wtnwp);
+	WtIncEdge(head,k,echange,wtnwp);
     });
 
   // Update all t-h-k shared partners.
-  EXEC_THROUGH_EDGES(head, e, k, {
+  EXEC_THROUGH_EDGES_PRE(head, e, k, {
       if(tail!=k)
-	WtSetEdge(tail,k,WtGetEdge(tail,k,wtnwp)+echange,wtnwp);
+	WtIncEdge(tail,k,echange,wtnwp);
     });
 
 }
