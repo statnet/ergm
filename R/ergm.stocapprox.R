statnet/ergm

commit 57ec73dc911f9f1e4175c4cb91e2071ef982f37e

Simplified output of ergm_MCMC_sample() somewhat.

DESCRIPTION
@@ -1,5 +1,5 @@
 Package: ergm
-Version: 3.9.0-3993
+Version: 3.9.0-3994
 Date: 2018-04-11
 Title: Fit, Simulate and Diagnose Exponential-Family Models for Networks
 Authors@R: c(

R/ergm.stocapprox.R
@@ -88,7 +88,7 @@ ergm.stocapprox <- function(init, nw, model, Clist,
 # aDdiaginv <- a * Ddiaginv
   z <- ergm.phase12(nw, model, proposal, 
                     eta, control, verbose=TRUE)
-  nw <- z$newnetwork
+  nw <- z$networks[[1]]
 # toggle.dyads(nw, head = z$changed[,2], tail = z$changed[,3])
 # control$maxchanges <- z$maxchanges
   theta <- z$eta
@@ -113,7 +113,7 @@ ergm.stocapprox <- function(init, nw, model, Clist,
   # post-processing of sample statistics:  Shift each row,
   # attach column names
   statshift <- summary(model, nw) - model$target.stats
-  statsmatrix <- sweep(z$statsmatrix, 2, statshift, "+")
+  statsmatrix <- sweep(as.matrix(z$stats), 2, statshift, "+")
   colnames(statsmatrix) <- param_names(model,canonical=TRUE)
   #v$sample <- statsmatrix
 # ubar <- apply(z$statsmatrix, 2, mean)

R/ergm.MCMLE.R
@@ -154,9 +154,9 @@ ergm.MCMLE <- function(init, nw, model,
     # observed statistics or, if given, the alternative target.stats
     # (i.e., the estimation goal is to use the statsmatrix to find 
     # parameters that will give a mean vector of zero)
-    statsmatrices <- mapply(sweep, z$statsmatrices, statshifts, MoreArgs=list(MARGIN=2, FUN="+"), SIMPLIFY=FALSE)
+    statsmatrices <- mapply(sweep, z$stats, statshifts, MoreArgs=list(MARGIN=2, FUN="+"), SIMPLIFY=FALSE)
     for(i in seq_along(statsmatrices)) colnames(statsmatrices[[i]]) <- param_names(model,canonical=TRUE)
-    nws.returned <- z$newnetworks
+    nws.returned <- z$networks
     statsmatrix <- do.call(rbind,statsmatrices)
     
     if(verbose){
@@ -173,9 +173,9 @@ ergm.MCMLE <- function(init, nw, model,
       
       if(z.obs$status==1) stop("Number of edges in the simulated network exceeds that observed by a large factor (",control$MCMC.max.maxedges,"). This is a strong indication of model degeneracy. If you are reasonably certain that this is not the case, increase the MCMLE.density.guard control.ergm() parameter.")
       
-      statsmatrices.obs <- mapply(sweep, z.obs$statsmatrices, statshifts.obs, MoreArgs=list(MARGIN=2, FUN="+"), SIMPLIFY=FALSE)
+      statsmatrices.obs <- mapply(sweep, z.obs$stats, statshifts.obs, MoreArgs=list(MARGIN=2, FUN="+"), SIMPLIFY=FALSE)
       for(i in seq_along(statsmatrices.obs)) colnames(statsmatrices.obs[[i]]) <- param_names(model,canonical=TRUE)
-      nws.obs.returned <- z.obs$newnetworks
+      nws.obs.returned <- z.obs$networks
       statsmatrix.obs <- do.call(rbind,statsmatrices.obs)
       
       if(verbose){

R/ergm.getMCMCsample.R
@@ -14,7 +14,16 @@
 ergm.getMCMCsample <- function(nw, model, proposal, eta0, control, 
                                verbose=FALSE, response=NULL, update.nws = TRUE,...) {
   .Deprecated("ergm_MCMC_sample")
-  ergm_MCMC_sample(nw, model, proposal, eta=eta0, control=control, verbose=verbose, response=response, update.nws=update.nws, theta=list(...)$theta, ...)
+  out <- ergm_MCMC_sample(nw, model, proposal, eta=eta0, control=control, verbose=verbose, response=response, update.nws=update.nws, theta=list(...)$theta, ...)
+  
+  out$newnetworks<-out$networks
+  out$newnetwork<-out$newnetworks[[1]]
+  out$statsmatrices <- out$stats
+  out$stats <- NULL
+  out$statsmatrix <- do.call(rbind,out$statsmatrices)
+  colnames(out$statsmatrix) <- model$coef.names
+  out$statsmatrix[is.na(out$statsmatrix)] <- 0
+  out
 }
 
 #' Internal Function to Sample Networks and Network Statistics
@@ -49,12 +58,8 @@ ergm.getMCMCsample <- function(nw, model, proposal, eta0, control,
 #' @return
 #' \code{ergm_MCMC_sample} returns a list
 #'   containing:
-#' \item{statsmatrices}{a list of stats matrices for the
-#'   sampled networks, relative to the original network, one for each thread.}
-#' \item{newnetworks}{a list of final sampled networks, one for each thread.}
-#' \item{statsmatrix}{combined stats matrix for the
-#'   sampled networks, relative to the original network.}
-#' \item{newnetwork}{the final sampled network from the first (or only) thread.}
+#' \item{stats}{an [`mcmc.list`] with sampled statistics.}
+#' \item{networks}{a list of final sampled networks, one for each thread.}
 #' \item{status}{status code, propagated from `ergm.mcmcslave`.}
 #' \item{final.interval}{adaptively determined MCMC interval.}
 #'
@@ -64,8 +69,11 @@ ergm.getMCMCsample <- function(nw, model, proposal, eta0, control,
 #' change class name to prevent the resulting object from being
 #' accessed or modified by functions that do not understand it.
 #'
-#'
-
+#' @note Unlike its predecessor `ergm.getMCMCsample`,
+#'   `ergm_MCMC_sample` does not return `statsmatrix` or `newnetwork`
+#'   elements. Rather, if parallel processing is not in effect,
+#'   `stats` is an [`mcmc.list`] with one chain and
+#'   `networks` is a list with one element.
 #' @export ergm_MCMC_sample
 ergm_MCMC_sample <- function(nw, model, proposal, control, theta=NULL, 
                              response=NULL, update.nws = TRUE, verbose=FALSE,..., eta=ergm.eta(theta, model$etamap)) {
@@ -214,20 +222,14 @@ ergm_MCMC_sample <- function(nw, model, proposal, control, theta=NULL,
     }
     final.interval <- c(final.interval, z$final.interval)
   }
-  newnetwork<-newnetworks[[1]]
-  
   
   ergm.stopCluster(cl)
 
-  statsmatrix <- do.call(rbind,statsmatrices)
-  colnames(statsmatrix) <- param_names(model,canonical=TRUE)
-
-  if(verbose){message("Sample size = ",nrow(statsmatrix)," by ",
-                  control.parallel$MCMC.samplesize,".")}
+  stats <- as.mcmc.list(statsmatrices)
+  if(verbose){message("Sample size = ",niter(statsmatrices)*nchain(statsmatrices)," by ",
+                  niter(statsmatrices),".")}
   
-  statsmatrix[is.na(statsmatrix)] <- 0
-  list(statsmatrix=statsmatrix, statsmatrices=statsmatrices, newnetwork=newnetwork, newnetworks=newnetworks, status=0, final.interval=final.interval)
-
+  list(stats = stats, networks=newnetworks, status=0, final.interval=final.interval)
 }
 
 #' @include ergm-deprecated.R

R/ergm.robmon.R
@@ -68,7 +68,7 @@ ergm.robmon <- function(init, nw, model,
   steplength <- control$MCMLE.steplength
   # post-processing of sample statistics:  Shift each row by the
   # matrix model$nw.stats - model$target.stats, attach column names
-  statsmatrix <- sweep(z$statsmatrix, 2, model$nw.stats - model$target.stats, "+")
+  statsmatrix <- sweep(as.matrix(z$stats), 2, model$nw.stats - model$target.stats, "+")
   colnames(statsmatrix) <- param_names(model,canonical=TRUE)
 
   if(steplength<1){
@@ -122,7 +122,7 @@ ergm.robmon <- function(init, nw, model,
       z <- ergm_MCMC_sample(nw, model, proposal, control, eta=eta, verbose=FALSE)
       # post-processing of sample statistics:  Shift each row by the
       # matrix model$nw.stats - model$target.stats, attach column names
-      statsmatrix <- sweep(z$statsmatrix, 2, model$nw.stats - model$target.stats, "+")
+      statsmatrix <- sweep(as.matrix(z$stats), 2, model$nw.stats - model$target.stats, "+")
       colnames(statsmatrix) <- param_names(model,canonical=TRUE)
 
       thetamatrix <- rbind(thetamatrix,theta)
@@ -154,7 +154,7 @@ message(paste("theta new:",theta,""))
   z <- ergm_MCMC_sample(nw, model, proposal, control, eta=eta, verbose=FALSE)
   # post-processing of sample statistics:  Shift each row by the
   # matrix model$nw.stats - model$target.stats, attach column names
-  statsmatrix <- sweep(z$statsmatrix, 2, model$nw.stats - model$target.stats, "+")
+  statsmatrix <- sweep(as.matrix(z$stats), 2, model$nw.stats - model$target.stats, "+")
   colnames(statsmatrix) <- param_names(model,canonical=TRUE)
 
 # ubar <- apply(z$statsmatrix, 2, base::mean)

R/ergm_estfun.R
@@ -1,0 +1,46 @@
+#' Compute the Sample Estimating Function Values of an ERGM.
+#'
+#' The estimating function for an ERGM is the score function: the
+#' gradient of the log-likelihood, equalling \eqn{\eta'(\theta)^\top
+#' \{g(y)-\mu(\theta)\}}, where \eqn{g(y)} is a \eqn{p}-vector of
+#' observed network sufficient statistic, \eqn{\mu(\theta)} is the
+#' expected value of the sufficient statistic under the model for
+#' parameter value \eqn{\theta}, and \eqn{\eta'(\theta)} is the
+#' \eqn{p} by \eqn{q} Jacobian matrix of the mapping from curved
+#' parameters to natural parmeters.  If the model is linear, all
+#' non-offset statistics are passed. If the model is curved, the score
+#' estimating equations (3.1) by Hunter and Handcock (2006) are given
+#' instead.
+#'
+#' @param stats An object representing sample statistics with observed values subtracted out.
+#' @param theta Model parameter \eqn{q}-vector.
+#' @param model An [`ergm_model`] object or its `etamap` element.
+#' @param ... Additional arguments for methods.
+#'
+#' @return An object of the same class as `stats` containing
+#'   \eqn{q}-vectors of estimating function values.
+#' @export
+ergm_estfun <- function(stats, theta, model, ...){
+  UseMethod("ergm_estfun")
+}
+
+#' @describeIn ergm_estfun Method for matrices with \eqn{p} columns.
+#' @export
+ergm_estfun.matrix <- function(stats, theta, model, ...){
+  etamap <- if(is(model, "ergm_model")) model$etamap else model
+  estf <- t(ergm.etagradmult(theta,t(as.matrix(stats)),etamap))[,!etamap$offsettheta,drop=FALSE]
+  colnames(estf) <- (if(is(model, "ergm_model")) param_names(model, FALSE) else names(theta))[!etamap$offsettheta]
+  -estf
+}
+
+#' @describeIn ergm_estfun Method for [`mcmc`] objects with \eqn{p} variables.
+#' @export
+ergm_estfun.mcmc <- function(stats, theta, model, ...){
+  mcmc(ergm_estfun(as.matrix(stats), theta, model), start=start(stats), end=end(stats), thin=thin(stats))
+}
+
+#' @describeIn ergm_estfun Method for  [`mcmc.list`] objects with \eqn{p} variables.
+#' @export
+ergm_estfun.mcmc.list <- function(stats, theta, model, ...){
+  lapply.mcmc.list(stats, ergm_estfun, theta, model)
+}

R/simulate.ergm.R
@@ -259,11 +259,8 @@ simulate.formula <- function(object, nsim=1, seed=NULL,
     # ergm.getMCMCsample.
     control$MCMC.samplesize <- nsim
     z <- ergm_MCMC_sample(nw, m, proposal, control, theta=coef, verbose=verbose, response=response)
-    
-    # Post-processing:  Add term names to columns and shift each row by
-    # observed statistics.
-    colnames(z$statsmatrix) <- param_names(m,canonical=TRUE)
-    out.mat <- sweep(z$statsmatrix[seq_len(nsim),,drop=FALSE], 2, curstats, "+")
+    # Post-processing: Shift each row by observed statistics.
+    out.mat <- sweep(as.matrix(z$stats)[seq_len(nsim),,drop=FALSE], 2, curstats, "+")
   }else{
     # Create objects to store output
     if (!statsonly) { 
@@ -305,14 +302,14 @@ simulate.formula <- function(object, nsim=1, seed=NULL,
       control$MCMC.burnin <- if(i==1 || sequential==FALSE) control$MCMC.burnin else control$MCMC.interval
       z <- ergm_MCMC_sample(nw, m, proposal, control, theta=coef, verbose=verbose, response=response)
       
-      out.mat <- rbind(out.mat, curstats + z$statsmatrix)
+      out.mat <- rbind(out.mat, curstats + as.matrix(z$stats))
       
       if(!statsonly) # then store the returned network:
-        if(nthreads>1) nw.list[[length(nw.list)+1]] <- z$newnetwork else nw.list <- c(nw.list, z$newnetworks)
+        nw.list[[length(nw.list)+1]] <- z$networks[[1]]
       
       if(sequential){ # then update the network state:
-        nw <- if(nthreads>1) z$newnetwork else z$newnetworks
-        curstats <- curstats + z$statsmatrix
+        nw <- z$networks
+        curstats <- curstats + as.matrix(z$stats)
       }
 
       if(verbose){message(sprintf("Finished simulation %d of %d.",i, nsim))}

man/ergm_MCMC_sample.Rd
@@ -52,12 +52,8 @@ be used to temporarily override those in the \code{control} list.}
 \value{
 \code{ergm_MCMC_sample} returns a list
 containing:
-\item{statsmatrices}{a list of stats matrices for the
-sampled networks, relative to the original network, one for each thread.}
-\item{newnetworks}{a list of final sampled networks, one for each thread.}
-\item{statsmatrix}{combined stats matrix for the
-sampled networks, relative to the original network.}
-\item{newnetwork}{the final sampled network from the first (or only) thread.}
+\item{stats}{an \code{\link{mcmc.list}} with sampled statistics.}
+\item{networks}{a list of final sampled networks, one for each thread.}
 \item{status}{status code, propagated from \code{ergm.mcmcslave}.}
 \item{final.interval}{adaptively determined MCMC interval.}
 
@@ -91,4 +87,11 @@ routine and does minimal preprocessing.
 Note that the returned stats will be relative to the original network, i.e.,
 the calling function must shift the statistics if required. The calling
 function must also attach column names to the statistics matrix if required.
+}
+\note{
+Unlike its predecessor \code{ergm.getMCMCsample},
+\code{ergm_MCMC_sample} does not return \code{statsmatrix} or \code{newnetwork}
+elements. Rather, if parallel processing is not in effect,
+\code{stats} is an \code{\link{mcmc.list}} with one chain and
+\code{networks} is a list with one element.
 }

commit ecf00609d173b2faf4c41d869a7a0df4e612f3c0

Fixed a bug in ergm.stocapprox() introduced in 57ec73dc9 .

DESCRIPTION
@@ -1,5 +1,5 @@
 Package: ergm
-Version: 3.9.0-4049
+Version: 3.9.0-4053
 Date: 2018-04-26
 Title: Fit, Simulate and Diagnose Exponential-Family Models for Networks
 Authors@R: c(

R/ergm.stocapprox.R
@@ -88,7 +88,7 @@ ergm.stocapprox <- function(init, nw, model, Clist,
 # aDdiaginv <- a * Ddiaginv
   z <- ergm.phase12(nw, model, proposal, 
                     eta, control, verbose=TRUE)
-  nw <- z$networks[[1]]
+  nw <- z$newnetwork
 # toggle.dyads(nw, head = z$changed[,2], tail = z$changed[,3])
 # control$maxchanges <- z$maxchanges
   theta <- z$eta