7e9e8870ea4124b0319103b7c57037fa52e176cd
Pavel N. Krivitsky <pavel@uow.edu.au>
2018-04-30 19:51:32 +1000

    ergm_model() now first looks for the term in the formula's environment rather than globalenv().

--- R/ergm_model.R
+++ R/ergm_model.R
@@ -83,7 +83,7 @@ ergm_model <- function(formula, nw, response=NULL, silent=FALSE, role="static",.
       args[[1]] <- as.name("list")
     }else args <- list()
     
-    termFun<-locate.InitFunction(term, paste0(termroot,"Term"), "ERGM term")
+    termFun<-locate.InitFunction(term, paste0(termroot,"Term"), "ERGM term", env=formula.env)
 
     termCall<-as.call(list(termFun, nw, args))
     
