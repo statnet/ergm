97286a54c9e5e76e410f12bb28af9dc1e7fcd696
Pavel N. Krivitsky <pavel@uow.edu.au>
2018-09-27 17:32:01 +1000

    Standardized the _GET_OVERRIDE# variadic macro usages.

--- src/ergm_wtchangestat.h
+++ src/ergm_wtchangestat.h
@@ -35,15 +35,15 @@ typedef struct WtModelTermstruct {
 /* tell whether a particular edge exists */
 #define _WtIS_OUTEDGE2(a,b) _WtIS_OUTEDGE3(a,b,nwp)
 #define _WtIS_OUTEDGE3(a,b,nwp) (EdgetreeSearch((a),(b),nwp->outedges)!=0?1:0)
-#define WtIS_OUTEDGE(...) _GET_OVERRIDE23(__VA_ARGS__, _WtIS_OUTEDGE3, _WtIS_OUTEDGE2,)(__VA_ARGS__)
+#define WtIS_OUTEDGE(...) _GET_OVERRIDE3(__VA_ARGS__, _WtIS_OUTEDGE3, _WtIS_OUTEDGE2,)(__VA_ARGS__)
 
 #define _WtIS_INEDGE2(a,b) _WtIS_INEDGE3(a,b,nwp)
 #define _WtIS_INEDGE3(a,b,nwp) (EdgetreeSearch((a),(b),nwp->inedges)!=0?1:0)
-#define WTIS_INEDGE(...) _GET_OVERRIDE23(__VA_ARGS__, _WtIS_INEDGE3, _WtIS_INEDGE2,)(__VA_ARGS__)
+#define WTIS_INEDGE(...) _GET_OVERRIDE3(__VA_ARGS__, _WtIS_INEDGE3, _WtIS_INEDGE2,)(__VA_ARGS__)
 
 #define _WtIS_UNDIRECTED_EDGE2(a,b) _WtIS_UNDIRECTED_EDGE3(a,b,nwp)
 #define _WtIS_UNDIRECTED_EDGE3(a,b,nwp) (EdgetreeSearch(MIN((a),(b)),MAX((a),(b)),nwp->outedges)!=0?1:0)
-#define WtIS_UNDIRECTED_EDGE(...) _GET_OVERRIDE23(__VA_ARGS__, _WtIS_UNDIRECTED_EDGE3, _WtIS_UNDIRECTED_EDGE2,)(__VA_ARGS__)
+#define WtIS_UNDIRECTED_EDGE(...) _GET_OVERRIDE3(__VA_ARGS__, _WtIS_UNDIRECTED_EDGE3, _WtIS_UNDIRECTED_EDGE2,)(__VA_ARGS__)
 
 /* Return the Edge number of the smallest-labelled neighbor of the node 
    labelled "a".  Or, return the Edge number of the next-largest neighbor 
@@ -113,10 +113,10 @@ typedef struct WtModelTermstruct {
 /* Get and set the weight of the (a,b) edge. */
 #define _WtGETWT2(a,b) _WtGETWT3(a,b,nwp)
 #define _WtGETWT3(a,b,nwp) (WtGetEdge(a,b,nwp))
-#define WtGETWT(...) _GET_OVERRIDE23(__VA_ARGS__, _WtGETWT3, _WtGETWT2,)(__VA_ARGS__)
+#define WtGETWT(...) _GET_OVERRIDE3(__VA_ARGS__, _WtGETWT3, _WtGETWT2,)(__VA_ARGS__)
 #define _WtSETWT3(a,b,w) _WtSETWT4(a,b,w,nwp)
 #define _WtSETWT4(a,b,w,nwp) (WtSetEdge(a,b,w,nwp))
-#define WtSETWT(...) _GET_OVERRIDE34(__VA_ARGS__, _WtSETWT4, _WtSETWT3,)(__VA_ARGS__)
+#define WtSETWT(...) _GET_OVERRIDE4(__VA_ARGS__, _WtSETWT4, _WtSETWT3,)(__VA_ARGS__)
 
 /* Cycle through all toggles proposed for the current step, then
    make the current toggle in case of more than one proposed toggle, then
