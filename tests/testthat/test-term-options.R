3fc9b8a7752e57c1379045f5789585b2d953c16a
Pavel N. Krivitsky <pavel@uow.edu.au>
2018-10-03 18:53:37 +1000

    Loosened the term.options tests and deleted the redundant one.

--- tests/testthat/test-term-options.R
+++ tests/testthat/test-term-options.R
@@ -16,8 +16,8 @@ test_that("ergm() MPLE of a term that takes options",{
 
 e2 <- ergm(flomarriage~.edges_times, control=control.ergm(force.main=TRUE,seed=0,term.options=list(times=2)))
 test_that("ergm() MCMLE of a term that takes options",{
-  expect_equivalent(coef(e2),coef(e1), tolerance=.005)
-  expect_equivalent(logLik(e2),logLik(e1), tolerance=.005)
+  expect_equivalent(coef(e2),coef(e1), tolerance=.05)
+  expect_equivalent(logLik(e2),logLik(e1), tolerance=.05)
 })
 
 test_that("gof() of a model that had term options",{
@@ -27,10 +27,11 @@ test_that("gof() of a model that had term options",{
 options(ergm.eval.loglik=FALSE)
 e3 <- ergm(flobusiness~.edges_times, target.stats=as.vector(summary(flomarriage~edges)/2), control=control.ergm(seed=0,force.main=TRUE,term.options=list(times=2)))
 test_that("ergm() for a model with term options and mean-value parameters and log-likelihood calculation off via a global option",{
-  expect_equivalent(coef(e2),coef(e1), tolerance=.005)
+  expect_equivalent(coef(e2),coef(e1), tolerance=.05)
   expect_error(logLik(e3), "Log-likelihood was not estimated for this fit.")
 })
 
+data(sampson)
 old.opts2 <- options(ergm.term=list(times=2))
 test_that("summary() with term options globally set",{
   expect_equivalent(summary(samplike~.edges_times),summary(samplike~edges)*times)
