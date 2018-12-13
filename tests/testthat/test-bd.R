03356c2b5e8ed7497abc9298ebeb42646be90e85
Pavel N. Krivitsky <pavel@uow.edu.au>
2018-12-13 13:52:38 +1100

    Made the bd() test a bit less unpredictable.

--- tests/testthat/test-bd.R
+++ tests/testthat/test-bd.R
@@ -1,23 +1,30 @@
 context("test-bd.R")
 
 vary <- function(x, tol=sqrt(.Machine$double.eps)){
-  apply(x,2,var)>tol
+  apply(x,2,function(x)length(unique(x)))
+}
+
+compvary <- function(x, y, diff.tol = 2, vary.tol=sqrt(.Machine$double.eps)){
+  x <- vary(x, vary.tol)
+  y <- vary(y, vary.tol)
+
+  (x-y)^2/(x+y)*2 <= diff.tol
 }
 
 test_that("Bounded degree (bd()) maximum constraint for undirected networks", {
   data(florentine)
-  constr <- simulate(flomarriage~edges, coef=1, monitor=~degree(0:network.size(flomarriage)), constraints=~bd(maxout=6), nsim=1000, output="stats")
-  degr <- simulate(flomarriage~edges+degrange(7,Inf), coef=c(1,-Inf), monitor=~degree(0:network.size(flomarriage)), nsim=1000, output="stats")
+  constr <- simulate(flomarriage~edges, coef=0.5, monitor=~degree(0:network.size(flomarriage)), constraints=~bd(maxout=6), nsim=200, output="stats")
+  degr <- simulate(flomarriage~edges+degrange(7,Inf), coef=c(0.5,-Inf), monitor=~degree(0:network.size(flomarriage)), nsim=200, output="stats")
   
   expect_true(approx.hotelling.diff.test(constr,degr[,-2])$p.value > 0.01)
-  expect_true(all(vary(constr)==vary(degr[,-2])))
+  expect_true(all(compvary(constr,degr[,-2])))
 })
 
 test_that("Bounded degree (bd()) constraints for directed networks", {
   data(sampson)
-  constr <- simulate(samplike~edges, coef=-1, monitor=~odegree(0:network.size(samplike))+idegree(0:network.size(samplike)), constraints=~bd(minout=3,minin=2,maxout=6), nsim=1000, output="stats")
-  degr <- simulate(samplike~edges+odegrange(0,3)+idegrange(0,2)+odegrange(7,Inf), coef=c(-1,-Inf, -Inf, -Inf), monitor=~odegree(0:network.size(samplike))+idegree(0:network.size(samplike)), nsim=1000, output="stats")
+  constr <- simulate(samplike~edges, coef=-1, monitor=~odegree(0:network.size(samplike))+idegree(0:network.size(samplike)), constraints=~bd(minout=3,minin=2,maxout=6), nsim=200, output="stats")
+  degr <- simulate(samplike~edges+odegrange(0,3)+idegrange(0,2)+odegrange(7,Inf), coef=c(-1,-Inf, -Inf, -Inf), monitor=~odegree(0:network.size(samplike))+idegree(0:network.size(samplike)), nsim=200, output="stats")
   
   expect_true(approx.hotelling.diff.test(constr,degr[,-(2:4)])$p.value > 0.01)
-  expect_true(all(vary(constr)==vary(degr[,-(2:4)])))
+  expect_true(all(compvary(constr,degr[,-(2:4)])))
 })
