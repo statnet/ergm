27f99c77aad17fdfd83e766d0b0e3195140fb13a
Pavel N. Krivitsky <pavel@uow.edu.au>
2019-01-21 19:51:02 +1100

    san() now calculates the adaptive statistic weights using a sliding window, and ensures that no statistic will ever have 0 weight.

--- R/ergm.san.R
+++ R/ergm.san.R
@@ -136,7 +136,10 @@ san.formula <- function(object, response=NULL, reference=~Bernoulli, constraints
     nw <-  outnw
     stats <- z$s[nrow(z$s),]
     # TODO: Subtract off a smoothing of these values?
-    invcov <- ginv(cov(z$s))
+    # Use only a bit less than the second half of the sample statistics to compute the statistic weights:
+    invcov <- ginv(cov(window(z$s, start=floor(nrow(z$s)*5/8))))
+    # Ensure no statistic has weight 0:
+    diag(invcov)[abs(diag(invcov))<.Machine$double.eps] <- min(diag(invcov)[abs(diag(invcov))>=.Machine$double.eps])
     invcov <- invcov / sum(diag(invcov)) # Rescale for consistency.
     control$invcov <- invcov
     
