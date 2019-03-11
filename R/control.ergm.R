af8839106e453b36741b6ac83a1987178fb23272
Pavel N. Krivitsky <pavel@uow.edu.au>
2019-03-11 14:08:35 +1100

    Fixed a CD control parameter omission (obs.CD.samplesize_per_theta).

--- R/control.ergm.R
+++ R/control.ergm.R
@@ -387,7 +387,7 @@
 #' value, as the network passed is not necessarily a good start for CD.
 #' Therefore, these settings are in effect if there are missing dyads in the
 #' observed network, using a higher default number of steps.
-#' @param CD.samplesize.per_theta,CD.maxit,CD.conv.min.pval,CD.NR.maxit,CD.NR.reltol,
+#' @param CD.samplesize.per_theta,obs.CD.samplesize.per_theta,CD.maxit,CD.conv.min.pval,CD.NR.maxit,CD.NR.reltol,
 #' 
 #' Miscellaneous tuning parameters of the CD sampler and optimizer. These have
 #' the same meaning as their \code{MCMC.*} counterparts.
@@ -594,6 +594,7 @@ control.ergm<-function(drop=TRUE,
                        Step.samplesize=1024,
 
                        CD.samplesize.per_theta=128,
+                       obs.CD.samplesize.per_theta=128,
                        CD.nsteps=8,
                        CD.multiplicity=1,
                        CD.nsteps.obs=128,
