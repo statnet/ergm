e65c9f9574abc5456560bb5bdaa3a4e5a32504a5
Pavel N. Krivitsky <pavel@uow.edu.au>
2019-05-04 12:38:38 +1000

    MCMLE can now checkpoint and resume optimization states with MCMLE.checkpoint= and MCMLE.resume= control parameters.

---
+++ tests/testthat/test-checkpointing.R
@@ -0,0 +1,27 @@
+#  File tests/testthat/test-term-options.R in package ergm, part of the Statnet suite
+#  of packages for network analysis, https://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  https://statnet.org/attribution
+#
+#  Copyright 2003-2019 Statnet Commons
+#######################################################################
+context("test-mcmle-checkpoint.R")
+
+data(florentine)
+set.seed(0)
+tmpf <- tempfile()
+
+teardown({
+  unlink(paste0(tmpf,"_00",1:2,".RData"))
+})
+
+test_that("checkpointing works",{
+ gest <- ergm(flomarriage ~ kstar(1:2) + absdiff("wealth") + triangle, control=control.ergm(checkpoint=paste0(tmpf,"_%03d.RData")))
+})
+
+test_that("resuming works",{
+  gest <- ergm(flomarriage ~ kstar(1:2) + absdiff("wealth") + triangle, control=control.ergm(resume=paste0(tmpf,"_002.RData")))
+  expect_equal(gest$iterations, 1) # It'll take at least 2 if resume fails.
+})
