02c48739e061e4319f7e2ab55137ad6b6bd624bd
Michal Bojanowski <michal2992@gmail.com>
2019-11-06 23:09:26 +0100

    More predict.ergm() documentation

--- R/predict.ergm.R
+++ R/predict.ergm.R
@@ -1,28 +1,54 @@
 #' ERGM-based conditional tie probabilities
 #' 
-#' Calculate predicted **conditional** tie probabilities for all dyads in a
-#' network based on an ERGM model. Currently there are two methods implemented:
-#' - [predict.formula()] requires an ERGM model formula with an existing network
-#' object on the left hand side, and a vector of corresponding parameter values.
-#' - [predict.ergm()] requires only a fitted model object
+#' @description 
+#' Calculate model-predicted **conditional** tie probabilities for dyads given
+#' the rest of the graph. Currently there are two methods implemented:
+#' - Method for formula objects requires (1) an ERGM model formula with an existing
+#' network object on the left hand side and terms on the right hand side, and
+#' (2) and a vector of corresponding parameter values.
+#' - Method for `ergm` objects, as returned by [ergm()], takes both the formula
+#' and parameter values from the fitted model object.
+#' 
+#' @description 
+#' Both methods can limit calculations to specific set of dyads of interest.
 #'
 #' @param object a formula or a fitted ERGM model object
 #' @param theta numeric vector of ERGM model parameter values
 #' @param output character, type of object returned. Defaults to `"data.frame"`.
 #'   See section Value below.
-#' @param ... other arguments passed to/from other methods
+#' @param ... other arguments passed to/from other methods. For [ergm.formula()]
+#'   the arguments are passed to [ergmMPLE()]
 #'
 #' @return 
 #' Type of object returned depends on the argument `output`. If
 #' `output="data.frame"` the function will return a data frame with columns:
-#' - `tail`,`head` -- indices of nodes identifying a dyad
+#' 
+#' - `tail`, `head` -- indices of nodes identifying a dyad
 #' - `p` -- predicted conditional tie probability
+#' 
 #' If `output="matrix"` the function will return an "adjacency matrix" with the
 #' predicted conditional tie probabilities.
 #' 
 #' @method predict formula
 #'
 #' @export
+#' @examples 
+#' # A three-node empty directed network
+#' net <- network.initialize(3, directed=TRUE)
+#' 
+#' # In homogeneous Bernoulli model with odds of a tie of 1/5 all ties are
+#' # equally likely
+#' predict(net ~ edges, log(1/5))
+#' 
+#' # Let's add a tie so that `net` has 1 tie out of possible 6 (so odds of 1/5)
+#' net[1,2] <- 1
+#' 
+#' # Fit the model
+#' fit <- ergm(net ~ edges)
+#' 
+#' # The p's should be identical
+#' predict(fit)
+#' 
 
 predict.formula <- function(object, theta, output=c("data.frame", "matrix"), ...) {
   stopifnot(is.numeric(theta))
