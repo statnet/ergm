db872fe14940b20b1ef65f37739d302fde920254
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2020-04-15 13:29:56 +1000

    Added some match.arg(output) calls to the san() methods to be safe.

--- R/ergm.san.R
+++ R/ergm.san.R
@@ -131,6 +131,8 @@ san.formula <- function(object, response=NULL, reference=~Bernoulli, constraints
   check.control.class("san", "san")
   control.toplevel(...,myname="san")
 
+  output <- match.arg(output)
+
   formula <- object
 
   if(!is.null(basis)) {
@@ -324,6 +326,7 @@ san.ergm <- function(object, formula=object$formula,
                      verbose=FALSE, 
                      offset.coef=object$coef[object$offset],
                      ...) {
+  output <- match.arg(output)
   san.formula(formula, nsim=nsim, 
               target.stats=target.stats,
               basis=basis,
