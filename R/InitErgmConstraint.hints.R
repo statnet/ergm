a91829aedf4a8d312ed094da249f61167c054a01
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2020-06-01 16:08:49 +1000

    Increased the priority of the TNT hint and specified that it can be disregarded if edge-count constraints are present.

--- R/InitErgmConstraint.hints.R
+++ R/InitErgmConstraint.hints.R
@@ -1,5 +1,5 @@
 InitErgmConstraint.TNT<-function(lhs.nw, ...){
    if(length(list(...)))
      ergm_Init_abort(paste("TNT hint does not take arguments at this time."))
-   list(dependence = FALSE, priority=1)
+   list(dependence = FALSE, priority=10, impliedby=c("edges", "degrees", "edges", "idegrees", "odegrees", "b1degrees", "b2degrees", "idegreedist", "odegreedist", "degreedist", "b1degreedist", "b2degreedist"))
 }
