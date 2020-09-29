2ee52a30376e6c017be766abd79a0b863404cab1
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2020-09-30 07:52:09 +1000

    ergm now exports a macro ERGM_API, giving the version of the C API (really, the current version of the package).

--- inst/include/ergm_constants.h
+++ inst/include/ergm_constants.h
@@ -1,6 +1,9 @@
 #ifndef _ERGM_CONSTANTS_H_
 #define _ERGM_CONSTANTS_H_
 
+// Macro indicating the version of the C API.
+#define ERGM_API 3.11
+
 typedef enum MCMCStatus_enum {
   MCMC_OK = 0,
   MCMC_TOO_MANY_EDGES = 1,
