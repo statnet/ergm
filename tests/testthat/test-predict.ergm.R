e032a627107983d27c7f530a36312c71bf5aa95a
Michal Bojanowski <michal2992@gmail.com>
2020-09-29 14:30:48 +0200

    Improve tests
    References statnet/ergm#172.

--- tests/testthat/test-predict.ergm.R
+++ tests/testthat/test-predict.ergm.R
@@ -100,26 +100,62 @@ test_that("works for edges model on small graph", {
 
 
 
-test_that("predict.ergm() works for models with offset terms", {
+test_that("predict.formula(net ~ edges + offset(edges))", {
   net <- network.initialize(4, directed=FALSE)
-  net %v% "a" <- c(1,1,2,2)
+
+  # edges + offset(edges)
   expect_silent(
     # Odds = 1/4 * 4 = 1
     # P = 0.5
     p <- predict(net ~ edges + offset(edges), c(log(1/4),  log(4)))
   )
   expect_equal(p$p, rep(0.5, 6))
-  # Odds = 1/4 * 2 = 1/2
-  # P = 1/3
-  expect_silent(
-    p <- predict(net ~ edges + offset(edges), c(log(1/4),  log(2)))
+  
+  net[1,2:4] <- 1
+  expect_equal(
+    predict(ergm(net ~ edges + offset(edges), offset.coef=log(4)))$p,
+    rep(0.5, 6)
   )
-  expect_equal(p$p, rep(1/3, 6))
+})
+
+
+test_that("predict.formula(net ~ edges + offset(nodematch))", {
+  net <- network.initialize(4, directed=FALSE)
+  net %v% "a" <- a <- c(1,1,2,2)
+
   expect_silent(
-    p <- predict(net ~ edges + offset(nodematch("a", diff=FALSE)), c(log(1/4),  log(4)))
+    p <- predict(
+      net ~ edges + offset(nodematch("a", diff=FALSE)), 
+      c(log(1/4),  log(4))
+    )
+  )
+  match_on_a <-  a[p$tail] == a[p$head]
+  expect_equal(
+    p$p,
+    ifelse(match_on_a, 0.5, 0.2)
+  )
+})
+
+
+
+
+test_that("predict.formula(net ~ edges + degree(1)", {
+  net <- network.initialize(3, directed=FALSE)
+  net[1,2] <- 1
+  expect_silent(
+    p <- predict(
+      # logodds(1--2) = log(1/4) + log(4)*2
+      # odds(1--2) = 16/4 = 4
+      # P(1--2) = 4/5
+      # logodds(1--3 | 2--3) = log(1/4) + log(4) * 0
+      # odds(1--3 | 2--3) = 1/4
+      # P(1--3 | 2--3) = 1/5
+      net ~ edges + degree(1), 
+      c(log(1/4),  log(4))
+    )
   )
   expect_equal(
     p$p,
-    c(0.2, 0.5)[c(1,2,1,2,1,1)]
+    with(p, ifelse(tail == 1 & head == 2, 4/5, 1/5))
   )
 })
