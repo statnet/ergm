f735f06f6ab925c15b2a121c385c9a2c4220bc66
Michal Bojanowski <michal2992@gmail.com>
2020-09-29 02:22:14 +0200

    Testing predict.formula() with offsets
    References statnet/ergm#172.

--- tests/testthat/test-predict.ergm.R
+++ tests/testthat/test-predict.ergm.R
@@ -102,149 +102,24 @@ test_that("works for edges model on small graph", {
 
 test_that("predict.ergm() works for models with offset terms", {
   net <- network.initialize(4, directed=FALSE)
-  m <- matrix(1, 4, 4)
-  m[lower.tri(m)] <- diag(m) <- 0
-  net %n% "dcov" <- m
-  predict(net ~ edges + offset(edges), c(log(1/4),  - log(1/4)))
+  net %v% "a" <- c(1,1,2,2)
+  expect_silent(
+    # Odds = 1/4 * 4 = 1
+    # P = 0.5
+    p <- predict(net ~ edges + offset(edges), c(log(1/4),  log(4)))
+  )
+  expect_equal(p$p, rep(0.5, 6))
+  # Odds = 1/4 * 2 = 1/2
+  # P = 1/3
+  expect_silent(
+    p <- predict(net ~ edges + offset(edges), c(log(1/4),  log(2)))
+  )
+  expect_equal(p$p, rep(1/3, 6))
+  expect_silent(
+    p <- predict(net ~ edges + offset(nodematch("a", diff=FALSE)), c(log(1/4),  log(4)))
+  )
+  expect_equal(
+    p$p,
+    c(0.2, 0.5)[c(1,2,1,2,1,1)]
+  )
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Scrapbook ---------------------------------------------------------------
-
-# Some ideas for tests not [yet] implemented.
-
-if(FALSE) {
-  
-  logit <- function(p, ...) log(p / (1-p), ...)
-  expit <- function(lgit, ...) 1 / (1 + exp(-lgit))
-
-  r1 <- predict_ergm(fit, flomarriage)
-  
-  data("flo")
-  fit <- ergm(flomarriage ~ edges + gwesp(0.25, fixed = TRUE))
-  # term: indicies
-  z <- ergmMPLE( 
-    update(fit$formula, . ~ . + indices), 
-    output="matrix"
-  )
-  str(z)
-  coef(fit)
-  v <- ergm.eta(fit$coef, fit$etamap)
-  z$predictor[,1:2] %*% v -> m
-  
-  p <- predict_formula(flomarriage ~ edges + gwesp(0.25, fixed = TRUE), c(-1, 0.5))
-  
-  
-  # OTPs --------------------------------------------------------------------
-  
-  net <-
-    igraph::make_graph(~A --+ C:D --+ B +-- A) %>%
-    intergraph::asNetwork() 
-  plot(net, displaylabels=TRUE)
-  
-  # Formula
-  fit <- ergm(
-    net ~ edges + dgwesp(decay=0.5, fixed=TRUE, type="OTP"),
-    control = control.ergm(MCMLE.maxit = 2)
-  )
-  summary(fit)
-  
-  simulate(fit, nsim=1)
-
-  predict(fit)
-  predict(fit, conditional=FALSE, nsim=10)
-  
-  predict(
-    net ~ edges + dgwesp(decay=0.5, fixed=TRUE, type="OTP"),
-    theta = c(-191, 66)
-  )
-
-  simulate_formula(
-    net ~ edges + dgwesp(decay=0.5, fixed=TRUE, type="OTP"),
-    coef = c(-191, 66)
-  )
-
-  # Sampson data ------------------------------------------------------------
-  
-  # Curved ERGM
-  
-  data(sampson)
-  gest <- ergm(
-    samplike ~ edges + gwesp(decay=0.5, fixed=TRUE)
-  )
-  coef(gest)
-  summary(samplike ~ edges + gwesp(decay=0.5, fixed=TRUE))
-
-  predict(gest)
-  predict(gest, conditional=FALSE, output="matrix")
-  
-  # predict on net different than the one the model was fit to --------------
-  
-  predict(
-    net ~ edges + gwesp(decay=0.5, fixed=TRUE),
-    theta = coef(gest)
-  )  
-  
-  
-  # IBE121 - play --------------------------------------------------------
-  
-  
-  netw <- isnar::IBE121 %>%
-    igraph::delete_edges(igraph::E(.)[question != "play"]) %>%
-    intergraph::asNetwork()
-  
-  fit <- ergm(
-    netw ~ edges + nodematch("female") + gwesp(decay=0.2, fixed=FALSE),
-    control = control.ergm(MCMLE.maxit=1)
-  )
-  
-  ff <- enformulate.curved(fit)
-  
-  
-  summary(netw ~ edges + nodematch("female") + gwesp(decay=0.2, fixed=TRUE) )
-  p <- cond_probs(fit, netw)  
-
-
-
-  # Subsets -----------------------------------------------------------------
-  
-  net <- network.initialize(3, directed=TRUE)
-  net[1,2] <- 1
-  r <- predict(net ~ edges, -1.609, constraints= ~ fixallbut(net))
-  theta <- -1.609
-  predmat <- ergmMPLE(net ~ edges + indices, constraints = ~ fixallbut(net))$predictor
-  
-  
-  
-  data(sampson)
-  gest2 <- ergm(
-    samplike ~ edges + nodematch("group")
-  )
-  summary(gest2)
-  g <- gof(gest2)
-  plot(g)
-  
-  # Probs for all dyads
-  predict(gest2)
-  
-  # Probs for dyads in `net`
-
-  
-}
-
