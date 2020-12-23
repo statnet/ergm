35637c95a9d303b1a37684ec9842781df75d708f
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2020-12-23 22:02:11 +1100

    Fixed up the ergmMPLE testthat test.

--- tests/testthat/test-ergmMPLE.R
+++ tests/testthat/test-ergmMPLE.R
@@ -1,9 +1,9 @@
 data(faux.mesa.high)
 formula <- faux.mesa.high ~ nodematch("Sex")
+mplesetup <- ergmMPLE(formula)
 
 test_that("output = 'matrix' works for undirected networks", {
   local_edition(3)
-  mplesetup <- ergmMPLE(formula)
   ord <- order(mplesetup$weights)
   m <- cbind(mplesetup$weights, mplesetup$response, mplesetup$predictor)[ord,]
   expect_equal(m, matrix(c(71, 132, 10284, 10423, 1, 1, 0, 0, 0, 1, 1, 0), 4,3), ignore_attr=TRUE)
@@ -12,7 +12,7 @@ test_that("output = 'matrix' works for undirected networks", {
 test_that("output = 'fit' works for undirected networks", {
   local_edition(3)
   modelfit <- ergmMPLE(formula, output="fit")
-  alt <- glm(mplesetup$response ~ mplesetup$predictor - 1, 
+  alt <- glm(mplesetup$response ~ mplesetup$predictor - 1,
              weights = mplesetup$weights, family="binomial")
   expect_equal(coef(modelfit), coef(alt), ignore_attr=TRUE, tolerance=0.001)
 })
