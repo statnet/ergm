b1caa54930aa3e4347a250a7dc48079e09188a38
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2020-12-23 21:14:11 +1100

    Ported the ergmMPLE() tests to testthat (3rd ed.).

---
+++ tests/testthat/test-ergmMPLE.R
@@ -0,0 +1,18 @@
+data(faux.mesa.high)
+formula <- faux.mesa.high ~ nodematch("Sex")
+
+test_that("output = 'matrix' works for undirected networks", {
+  local_edition(3)
+  mplesetup <- ergmMPLE(formula)
+  ord <- order(mplesetup$weights)
+  m <- cbind(mplesetup$weights, mplesetup$response, mplesetup$predictor)[ord,]
+  expect_equal(m, matrix(c(71, 132, 10284, 10423, 1, 1, 0, 0, 0, 1, 1, 0), 4,3), ignore_attr=TRUE)
+})
+
+test_that("output = 'fit' works for undirected networks", {
+  local_edition(3)
+  modelfit <- ergmMPLE(formula, output="fit")
+  alt <- glm(mplesetup$response ~ mplesetup$predictor - 1, 
+             weights = mplesetup$weights, family="binomial")
+  expect_equal(coef(modelfit), coef(alt), ignore_attr=TRUE, tolerance=0.001)
+})
