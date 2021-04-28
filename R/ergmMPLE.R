aed56cc42fb3bfe31be7b17647cf71b43c763b57
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2021-04-28 20:09:46 +1000

    Fixed another careless mistake in ergmMPLE().

--- R/ergmMPLE.R
+++ R/ergmMPLE.R
@@ -181,7 +181,7 @@ ergmMPLE <- function(formula, constraints=~., obs.constraints=~-observed, fitmod
   output <- match.arg(output)
   if (output=="fit") {
     return(
-      ergm(formula, estimate="MPLE", control=control, verbose=verbose, constraints=constraints, obs.constraints, basis=basis, ...)
+      ergm(formula, estimate="MPLE", control=control, verbose=verbose, constraints=constraints, obs.constraints=obs.constraints, basis=basis, ...)
     )
   }
 
