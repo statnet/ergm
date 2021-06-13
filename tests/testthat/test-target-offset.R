0a819e1ca90707f8ac813a4e44b1dae6d724ebfe
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2021-06-14 01:19:44 +1000

    Updated copyright headers.

--- tests/testthat/test-target-offset.R
+++ tests/testthat/test-target-offset.R
@@ -1,4 +1,4 @@
-#  File tests/target_offset.R in package ergm, part of the
+#  File tests/testthat/test-target-offset.R in package ergm, part of the
 #  Statnet suite of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
--- tests/testthat/test-update.network.R
+++ tests/testthat/test-update.network.R
@@ -1,4 +1,4 @@
-#  File tests/utils_tests.R in package ergm, part of the
+#  File tests/testthat/test-update.network.R in package ergm, part of the
 #  Statnet suite of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
