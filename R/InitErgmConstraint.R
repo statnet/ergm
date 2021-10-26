a48e831ae72a5fd4fea88c5ebc1e96898ed947cc
Alex Wang <92829293+AlexJiahaoWang@users.noreply.github.com>
2021-10-26 21:43:44 +1100

    Update InitErgmConstraint.R

--- R/InitErgmConstraint.R
+++ R/InitErgmConstraint.R
@@ -5,7 +5,7 @@
 #  open source, and has the attribution requirements (GPL Section 7) at
 #  https://statnet.org/attribution .
 #
-#  Copyright 2003-2021 Statnet Commons
+#  Copyright 2003-2021 Statnet Commons     hh
 ################################################################################
 
 # Baseline constraint incorporating network attributes such as
