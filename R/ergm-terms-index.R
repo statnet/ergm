6089fd5ecea28b375a63046fa074588f464eafd8
Joyce Cheng <joyce.hm.cheng@gmail.com>
2021-11-18 16:41:28 +1100

    Only displaying usages of the appropriate type when binary or valued is specified.
    Fixes statnet/ergm#415

--- R/ergm-terms-index.R
+++ R/ergm-terms-index.R
@@ -590,7 +590,8 @@ search.ergmTermType <-function(term.type, search, net, keywords, name, packages)
     term<-terms[[t]]
 
     if(!all(keywords%in%term$concepts)){
-      found[t]<-FALSE }
+      found[t]<-FALSE
+    }
   }
 
   # next (optionally) restrict by search matches
@@ -637,7 +638,16 @@ search.ergmTermType <-function(term.type, search, net, keywords, name, packages)
       term<-terms[[t]]
       unique_usages <- term$usages %>% map("usage") %>% unlist %>% unique
       if (term$type %in% c('ergmTerm')) {
-        term_types <- sapply(unique_usages, function(usage) paste(term$usages[(term$usages %>% map("usage") %>% unlist) == usage] %>% map("type") %>% unlist %>% unique, collapse=", "))
+        if (!missing(keywords) && any(c("binary", "valued") %in% keywords)) {
+          term_type_to_match <- intersect(keywords, c('binary', 'valued'))
+          term_types <- sapply(unique_usages, function(usage) paste(term$usages[(term$usages %>% map("usage") %>% unlist) == usage] %>% map("type") %>% unlist %>% unique %>% intersect(term_type_to_match), collapse=", "))
+
+          unique_usages <- unique_usages[term_types != '']
+          term_types <- term_types[term_types != '']
+        } else {
+          term_types <- sapply(unique_usages, function(usage) paste(term$usages[(term$usages %>% map("usage") %>% unlist) == usage] %>% map("type") %>% unlist %>% unique, collapse=", "))
+        }
+
         unique_usages <- paste0(unique_usages, " (", term_types, ")")
       }
       outText <- paste0(paste(unique_usages, collapse="\n"), "\n    ", term$title, "\n")
