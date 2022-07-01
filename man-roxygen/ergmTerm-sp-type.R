cab657b8b91345b051ec4f2880b4219cf9fa4c0a
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2022-07-01 16:32:16 +1000

    Tweak to the SP type argument wording.

--- man-roxygen/ergmTerm-sp-type.R
+++ man-roxygen/ergmTerm-sp-type.R
@@ -1,1 +1,1 @@
-#' @param type A string indicating the type of shared partner or path to be considered: one of `"UTP"` (only option for undirected), `"OTP"` (default for directed), `"ITP"`, `"RTP"`, `"OSP"`, and `"ISP"`. See the section below on Shared partner types for details.
+#' @param type A string indicating the type of shared partner or path to be considered for directed networks: `"OTP"` (default for directed), `"ITP"`, `"RTP"`, `"OSP"`, and `"ISP"`; has no effect for undirected. See the section below on Shared partner types for details.
