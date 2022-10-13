641e7999388db7ed3c111c8e9de124a0c3b3d90a
Michał Bojanowski <michal2992@gmail.com>
2022-10-13 16:22:08 +0200

    Re-merging updates to man of san() (#492)
    * Improved Details section for san()
    
    These are changes (78e727..30adce) to the Details section of man of san() based on
    section 4 of Krivitsky et al (2022 - the computational one) that has
    been reverted due to PDF manual building errors.
    
    References #488.
    
    * Don't use \operatorname macro
    
    We can't assume it is available when running LaTeX to produce the PDF
    manual.
    
    * Fix man leading to PDF manual errors

--- R/ergm.san.R
+++ R/ergm.san.R
@@ -8,54 +8,79 @@
 #  Copyright 2003-2022 Statnet Commons
 ################################################################################
 
-#' Use Simulated Annealing to attempt to match a network to a vector of mean
-#' statistics
+#' Generate networks with a given set of network statistics
 #' 
 #' This function attempts to find a network or networks whose statistics match
-#' those passed in via the \code{target.stats} vector.
+#' those passed in via the `target.stats` vector.
 #' 
-#' @details Acceptance probabilities for proposed toggles are computed as 
-#'   we now describe.  There are two contributions: one from targeted
-#'   statistics and one from offsets.
-#'
-#' For the targeted statistics, a matrix of weights \code{W} is determined on 
-#'   each \code{san} iteration as follows.  On the first iteration, the matrix
-#'   \code{W} is the \code{n} by \code{n} identity matrix (\code{n} = number of
-#'   target statistics), divided by \code{n}.  On subsequent iterations: if 
-#'   \code{control$SAN.invcov.diag = FALSE} (the default), then the matrix 
-#'   \code{W} is the inverse of the covariance matrix of the targeted 
-#'   statistics, divided by the sum of its (the inverse's) diagonal;
-#'   if \code{control$SAN.invcov.diag = TRUE}, then \code{W} is the inverse 
-#'   of the diagonal (regarded as a matrix) of the covariance matrix of the 
-#'   targeted statistics, divided by the sum of its (the inverse's) diagonal.
-#'   In either of these two cases, the covariance matrix is computed based on 
-#'   proposals (not acceptances) made on the previous iteration, and the 
-#'   normalization for \code{W} is such that \code{sum(diag(W)) = 1}.  The 
-#'   component of the acceptance probability coming from the targeted statistics
-#'   is then computed for a given \code{W} as \code{exp([y.Wy - x.Wx]/T)} where 
-#'   \code{T} is the temperature, \code{y} the column vector of differences 
-#'   \code{network statistics - target statistics} computed before the current 
-#'   proposal is made, \code{x} the column vector of differences 
-#'   \code{network statistics - target statistics} computed assuming the current proposal 
-#'   is accepted, and \code{.} the dot product.  If \code{control$SAN.maxit > 1},
-#'   then on the \code{i}th iteration, the temperature \code{T} takes the value 
-#'   \code{control$SAN.tau * (1/i - 1/control$SAN.maxit)/(1 - 1/control$SAN.maxit)};
-#'   if \code{control$SAN.maxit = 1}, then the temperature \code{T} takes the 
-#'   value \code{0}.  Thus, \code{T} steps down from \code{control$SAN.tau} to
-#'   \code{0} and is always \code{0} on the final iteration.
-#'
-#' Offsets also contribute to the acceptance probability, as follows.  If 
-#'   \code{eta} are the canonical offsets and \code{Delta} the corresponding
-#'   change statistics for a given proposal, then the offset contribution to
-#'   the acceptance probability is simply \code{exp(eta.Delta)} where 
-#'   \code{.} denotes the dot product.  By default, finite offsets are ignored,
-#'   but this behavior can be changed by setting
-#'   \code{control$SAN.ignore.finite.offsets = FALSE}.
-#'
-#' The overall acceptance probability is the product of the targeted statistics
-#'   contribution and the offset contribution (with the product capped at one).
+#' @details The following description is an exegesis of section 4 of Krivitsky
+#'   et al. (2022).
 #' 
-#' @param object Either a [`formula`] or an [`ergm`] object. The
+#'   Let \eqn{\mathbf{g}}{g} be a vector of target statistics for the
+#'   network we wish to construct. That is, we are given an arbitrary network
+#'   \eqn{\mathbf{y}^0 \in \mathcal{Y}}{y0 ∈ Y}, and we seek a network
+#'   \eqn{\mathbf{y} \in \mathcal{Y}}{y ∈ Y} such that
+#'   \eqn{\mathbf{g}(\mathbf{y}) \approx \mathbf{g}}{g(y) ≈ g} -- ideally equality is achieved,
+#'   but in practice we may have to settle for a close approximation. The
+#'   variant of simulated annealing is as follows.
+#'   
+#'   The energy function is defined 
+#'   
+#'   \deqn{E_W (\mathbf{y}) = (\mathbf{g}(\mathbf{y}) - \mathbf{g})^\mathsf{T} W (\mathbf{g}(\mathbf{y}) - \mathbf{g}),}{E_W (y) = (g(y) - g)^T W (g(y) - g),}
+#'   
+#'   with \eqn{W} a symmetric positive (barring multicollinearity in statistics)
+#'   definite matrix of weights. This function achieves 0 only if the target is
+#'   reached. A good choice of this matrix yields a more efficient search.
+#'
+#'   A standard simulated annealing loop is used, as described below, with some
+#'   modifications. In particular, we allow the user to specify a vector of
+#'   offsets \eqn{\eta}{η} to bias the annealing, with \eqn{\eta_k = 0}{η_k = 0} 
+#'   denoting no offset. Offsets can be used with SAN to forbid certain
+#'   statistics from ever increasing or decreasing. As with [ergm()], offset
+#'   terms are specified using the [offset()] decorator and their coefficients
+#'   specified with the `offset.coef` argument. By default, finite offsets are
+#'   ignored by, but this can be overridden by setting the [control.san()]
+#'   argument `SAN.ignore.finite.offsets = FALSE`.
+#'   
+#'   The number of simulated annealing runs is specified by the `SAN.maxit`
+#'   control parameter and the initial value of the temperature \eqn{T} is set
+#'   to `SAN.tau`. The value of \eqn{T} decreases linearly until \eqn{T = 0} 
+#'   at the last run, which implies that all proposals that increase 
+#'   \eqn{E_W (\mathbf{y})}{E_W(y)} are rejected. The weight matrix \eqn{W} 
+#'   is initially set to \eqn{I_p / p}, where \eqn{I_p} is the identity matrix
+#'   of an appropriate dimension. For weight \eqn{W} and temperature \eqn{T},
+#'   the simulated annealing iteration proceeds as follows:
+#'   
+#'   1. Test if \eqn{E_W(\mathbf{y}) = 0}{E_W(y) = 0}. If so, then exit.
+#'   2. Generate a perturbed network \eqn{\mathbf{y^*}}{y*} from a proposal that
+#'      respects the model constraints. (This is typically the same proposal as
+#'      that used for MCMC.)
+#'   3. Store the quantity 
+#'      \eqn{\mathbf{g}(\mathbf{y^*}) - \mathbf{g}(\mathbf{y})}{g(y*) - g(y)} 
+#'      for later use.
+#'   4. Calculate acceptance probability
+#' 
+#'      \deqn{\alpha = \exp[ - (E_W (\mathbf{y^*}) - E_W (\mathbf{y})) / T + \eta^\mathsf{T} (\mathbf{g}(\mathbf{y^*}) - \mathbf{g}(\mathbf{y}))]}{α = exp( - E_W(y*) - E_W(y) / T + η' (g(y*) - g(y)) ).}
+#'    
+#'      (If \eqn{|\eta_k| = \infty}{|η_k| = ∞} and \eqn{g_k (\mathbf{y^*}) - g_k (\mathbf{y}) = 0}{g_k (y) - g_k (y) = 0}, their product is defined to be 0.)
+#'   5. Replace \eqn{\mathbf{y}}{y} with \eqn{\mathbf{y^*}}{y} with probability
+#'      \eqn{\min(1, \alpha)}{min(1, α)}.
+#'
+#'
+#'   After the specified number of iterations, \eqn{T} is updated as described
+#'   above, and \eqn{W} is recalculated by first computing a matrix \eqn{S}, the
+#'   sample covariance matrix of the proposed differences stored in Step 3
+#'   (i.e., whether or not they were rejected), then
+#'   \eqn{W = S^+ / tr(S^+)}{W = S+ / tr(S+)}, where \eqn{S^+}{S+} is the
+#'   Moore–Penrose pseudoinverse of \eqn{S} and \eqn{tr(S^+)}{tr(S+)} is the
+#'   trace of \eqn{S^+}{S+}. The differences in Step 3 closely reflect the
+#'   relative variances and correlations among the network statistics.
+#' 
+#'   In Step 2, the many options for MCMC proposals can provide for effective
+#'   means of speeding the SAN algorithm's search for a viable network.
+#' 
+#' @param object Either a [`formula`] or some other supported
+#'   representation of an ERGM, such as an [`ergm_model`] object.
 #'   [`formula`] should be of the form \code{y ~ <model terms>}, where
 #'   \code{y} is a network object or a matrix that can be coerced to a
 #'   [`network`] object.  For the details on the possible \code{<model
@@ -63,9 +88,16 @@
 #'   \code{\link[network]{network}} object in , use the
 #'   \code{network()} function, then add nodal attributes to it using
 #'   the \code{\%v\%} operator if necessary.
+#' 
 #' @return A network or list of networks that hopefully have network
-#'   statistics close to the \code{target.stats} vector. Additionally,
+#'   statistics close to the \code{target.stats} vector. No guarantees
+#'   are provided about their probability distribution. Additionally,
 #'   [attr()]-style attributes `formula` and `stats` are included.
+#' 
+#' @references Krivitsky, P. N., Hunter, D. R., Morris, M., & Klumb, C. (2022).
+#'   ergm 4: Computational Improvements. arXiv preprint arXiv:2203.08198.
+#' 
+#' 
 #' @keywords models
 #' @aliases san.default
 #' @export
@@ -73,12 +105,6 @@ san <- function(object, ...){
  UseMethod("san")
 }
 
-#' @noRd
-#' @export
-san.default <- function(object,...)
-{
-  stop("Either a ergm object or a formula argument must be given")
-}
 #' @describeIn san Sufficient statistics are specified by a [`formula`].
 #' 
 #' @template response
@@ -448,3 +474,46 @@ ergm_SAN_slave <- function(state, tau,control,verbose, ..., nsteps=NULL, samples
 
   z
 }
+
+
+
+# Acceptance probabilities for proposed toggles are computed as
+#   we now describe.  There are two contributions: one from targeted
+#   statistics and one from offsets.
+# 
+# For the targeted statistics, a matrix of weights \code{W} is determined on
+#   each \code{san} iteration as follows.  On the first iteration, the matrix
+#   \code{W} is the \code{n} by \code{n} identity matrix (\code{n} = number of
+#   target statistics), divided by \code{n}.  On subsequent iterations: if
+#   \code{control$SAN.invcov.diag = FALSE} (the default), then the matrix
+#   \code{W} is the inverse of the covariance matrix of the targeted
+#   statistics, divided by the sum of its (the inverse's) diagonal;
+#   if \code{control$SAN.invcov.diag = TRUE}, then \code{W} is the inverse
+#   of the diagonal (regarded as a matrix) of the covariance matrix of the
+#   targeted statistics, divided by the sum of its (the inverse's) diagonal.
+#   In either of these two cases, the covariance matrix is computed based on
+#   proposals (not acceptances) made on the previous iteration, and the
+#   normalization for \code{W} is such that \code{sum(diag(W)) = 1}.  The
+#   component of the acceptance probability coming from the targeted statistics
+#   is then computed for a given \code{W} as \code{exp([y.Wy - x.Wx]/T)} where
+#   \code{T} is the temperature, \code{y} the column vector of differences
+#   \code{network statistics - target statistics} computed before the current
+#   proposal is made, \code{x} the column vector of differences
+#   \code{network statistics - target statistics} computed assuming the current proposal
+#   is accepted, and \code{.} the dot product.  If \code{control$SAN.maxit > 1},
+#   then on the \code{i}th iteration, the temperature \code{T} takes the value
+#   \code{control$SAN.tau * (1/i - 1/control$SAN.maxit)/(1 - 1/control$SAN.maxit)};
+#   if \code{control$SAN.maxit = 1}, then the temperature \code{T} takes the
+#   value \code{0}.  Thus, \code{T} steps down from \code{control$SAN.tau} to
+#   \code{0} and is always \code{0} on the final iteration.
+# 
+# Offsets also contribute to the acceptance probability, as follows.  If
+#   \code{eta} are the canonical offsets and \code{Delta} the corresponding
+#   change statistics for a given proposal, then the offset contribution to
+#   the acceptance probability is simply \code{exp(eta.Delta)} where
+#   \code{.} denotes the dot product.  By default, finite offsets are ignored,
+#   but this behavior can be changed by setting
+#   \code{control$SAN.ignore.finite.offsets = FALSE}.
+# 
+# The overall acceptance probability is the product of the targeted statistics
+#   contribution and the offset contribution (with the product capped at one).
