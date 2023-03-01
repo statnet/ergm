4f81da63919125d7861a2d5b22b6e2259efac471
AryaKarami <karamiarya@gmail.com>
2023-03-01 06:39:23 +0330

    test-nodrop tests now use expect_warning() and are no longer optional.
    fixes statnet/ergm#515

--- tests/testthat/test-nodrop.R
+++ tests/testthat/test-nodrop.R
@@ -9,31 +9,43 @@
 ################################################################################
 ### Tests to make sure drop=FALSE works.
 
-library(statnet.common)
-opttest({
+o <- options(ergm.eval.loglik=FALSE)
 
 data(sampson)
 test_that("shouldn't need to drop", {
   # MPLE
-  summary(ergm(samplike~edges, control=control.ergm(drop=FALSE)))
+  expect_warning(ergm(samplike~edges, control=control.ergm(drop=FALSE)), NA)
   # MCMC
-  summary(ergm(samplike~edges, control=control.ergm(drop=FALSE, force.main=TRUE, MCMLE.maxit=3)))
+  expect_warning(ergm(samplike~edges, control=control.ergm(drop=FALSE, force.main=TRUE, MCMLE.maxit=2)), NA)
 })
 
 test_that("empty network", {
   y0 <- network.initialize(10)
   # MPLE
-  summary(ergm(y0~edges, control=control.ergm(drop=FALSE)))
+  expect_warning(expect_warning(expect_warning(
+    ergm(y0~edges, control=control.ergm(drop=FALSE)),
+    "The MPLE does not exist!"),
+    "Network is empty and no target stats are specified."),
+    "Observed statistic.s. edges are at their smallest attainable values and drop=FALSE. The MLE is poorly defined.")
   # MCMC
-  summary(ergm(y0~edges, control=control.ergm(drop=FALSE, force.main=TRUE, init=0, MCMLE.maxit=3)))
+  expect_warning(expect_warning(
+    ergm(y0~edges, control=control.ergm(drop=FALSE, force.main=TRUE, init=0, MCMLE.maxit=2)),
+    "Network is empty and no target stats are specified."),
+    "Observed statistic.s. edges are at their smallest attainable values and drop=FALSE. The MLE is poorly defined.")
 })
 
 test_that("full network", {
   y1 <- as.network(matrix(1,10,10))
   # MPLE
-  summary(ergm(y1~edges, control=control.ergm(drop=FALSE)))
+  expect_warning(expect_warning(
+    ergm(y1~edges, control=control.ergm(drop=FALSE)),
+    "The MPLE does not exist!"),
+    "Observed statistic.s. edges are at their greatest attainable values and drop=FALSE. The MLE is poorly defined.")
+
   # MCMC
-  summary(ergm(y1~edges, control=control.ergm(drop=FALSE, force.main=TRUE, init=0, MCMLE.maxit=3)))
+  expect_warning(
+    ergm(y1~edges, control=control.ergm(drop=FALSE, force.main=TRUE, init=0, MCMLE.maxit=2)),
+    "Observed statistic.s. edges are at their greatest attainable values and drop=FALSE. The MLE is poorly defined.")
 })
 
-}, "drop disabled")
+options(o)
