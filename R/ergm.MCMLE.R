b6ca5625b60fd9257a3278b1b5b2b431af260bee
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2023-03-25 23:36:15 +1100

    Removed a vestigial line of code.

--- R/ergm.MCMLE.R
+++ R/ergm.MCMLE.R
@@ -66,7 +66,6 @@ ergm.MCMLE <- function(init, s, s.obs,
   stats.obs.hist <- matrix(NA, 0, nparam(model, canonical=TRUE))
   steplen.hist <- c()
   steplen <- control$MCMLE.steplength
-  if(control$MCMLE.steplength=="adaptive") steplen <- 1
 
   if(is.null(control$MCMLE.samplesize)) control$MCMLE.samplesize <- max(control$MCMLE.samplesize.min,control$MCMLE.samplesize.per_theta*nparam(model,canonical=FALSE, offset=FALSE))
   if(obs && is.null(control$obs.MCMLE.samplesize)) control$obs.MCMLE.samplesize <- max(control$obs.MCMLE.samplesize.min,control$obs.MCMLE.samplesize.per_theta*nparam(model,canonical=FALSE, offset=FALSE))
