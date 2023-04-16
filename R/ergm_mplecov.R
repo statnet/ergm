bded7718d5d10f625cb9506590baba32649d7242
schmid86 <songhyo86@gmail.com>
2023-04-16 18:28:49 +0200

    testing

--- R/ergm_mplecov.R
+++ R/ergm_mplecov.R
@@ -1,6 +1,7 @@
 #  File R/ergm_mplecov.R in package ergm, part of the Statnet suite
 #  of packages for network analysis, https://statnet.org .
 #
+
 #  This software is distributed under the GPL-3 license.  It is free,
 #  open source, and has the attribution requirements (GPL Section 7) at
 #  https://statnet.org/attribution
