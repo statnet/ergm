547b42bb3728fceae2e9e28c0038792f71fc3c0c
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2023-05-10 14:29:33 +1000

    Some label and tolerance fixes for MPLE covariance tests.

--- tests/testthat/test-mple-cov.R
+++ tests/testthat/test-mple-cov.R
@@ -20,31 +20,31 @@ test_that("Godambe covariance method for MPLE", {
   set.seed(111)
   m1 <- ergm(init.sim ~ edges + triangles, estimate = "MPLE",
               control=control.ergm(MPLE.covariance.method = "Godambe"))
-  StdErr1 <- round(sqrt(diag(vcov(m1))), 3)
-  expect_equal(StdErr1, c(0.255, 0.059), ignore_attr = TRUE)
+  StdErr1 <- sqrt(diag(vcov(m1)))
+  expect_equal(StdErr1, c(0.255, 0.059), ignore_attr = TRUE, tolerance=.01)
 })
 
 test_that("Inverse Hessian from logistic regression model", {
   set.seed(222) # However, this method is not stochastic
   m2 <- ergm(init.sim ~ edges+triangles, estimate = "MPLE",
                 control=control.ergm(MPLE.covariance.method = "invHess"))
-  StdErr2 <- round(sqrt(diag(vcov(m2))), 3)
-  expect_equal(StdErr2, c(0.155, 0.034), ignore_attr = TRUE)
+  StdErr2 <- sqrt(diag(vcov(m2)))
+  expect_equal(StdErr2, c(0.155, 0.034), ignore_attr = TRUE, tolerance=.01)
 })
 
 test_that("Bootstrap covariance method for MPLE", {
   set.seed(333)
   m3 <- ergm(init.sim ~ edges + triangles, estimate = "MPLE",
              control=control.ergm(MPLE.covariance.method = "bootstrap"))
-  StdErr3 <- round(sqrt(diag(vcov(m3))), 3)
-  expect_equal(StdErr3, c(0.257, 0.060), ignore_attr = TRUE)
+  StdErr3 <- sqrt(diag(vcov(m3)))
+  expect_equal(StdErr3, c(0.257, 0.060), ignore_attr = TRUE, tolerance=.01)
 })
 
-test_that("Bootstrap covariance method for MPLE", {
+test_that("Bootstrap covariance method for MPLE with offsets", {
   set.seed(445)
   m4 <- ergm(init.sim ~ edges + triangles + offset(triangles), offset.coef=1,
              estimate = "MPLE",
              control=control.ergm(MPLE.covariance.method = "InvHess"))
-  StdErr4 <- round(sqrt(diag(vcov(m4))), 3)
-  expect_equal(round(coef(summary(m4))[,2], 3), c(0.155, 0.034, 0), ignore_attr = TRUE)
+  StdErr4 <- sqrt(diag(vcov(m4)))
+  expect_equal(StdErr4, c(0.155, 0.034, 0), ignore_attr = TRUE, tolerance=.01)
 })
