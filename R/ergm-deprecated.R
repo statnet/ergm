ec9c6ff50d82675f5e2d5b48752e2cd205d8455b
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2023-11-28 15:01:55 +1100

    Removed unused arguments to deprecated functions in the documentation.
    references statnet/ergm#542

--- R/ergm-deprecated.R
+++ R/ergm-deprecated.R
@@ -11,7 +11,7 @@
 #' @rdname ergm-deprecated
 #' @title Functions that will no longer be supported in future releases of the package
 #' @description Functions that have been superceed, were never documented, or will be removed from the package for other reasons
-#' @param nw,arglist,...,fname,varnames,vartypes,defaultvalues,required,nw.bipartiteflag,requirement,extramessage,nw.directedflag,m,response,formula,object,new,from.new,x,model,proposal,eta0,control,verbose,update.nws,Clist,prev.run,burnin,samplesize,interval,maxedges,attrname,label,coord,jitter,thresh,usearrows,mode,displayisolates,interactive,xlab,ylab,xlim,ylim,pad,label.pad,displaylabels,boxed.labels,label.pos,label.bg,vertex.sides,vertex.rot,arrowhead.cex,label.cex,loop.cex,vertex.cex,edge.col,label.col,vertex.col,label.border,vertex.border,edge.lty,label.lty,vertex.lty,edge.lwd,label.lwd,edge.len,edge.curve,edge.steps,loop.steps,object.scale,uselen,usecurve,suppress.axes,vertices.last,layout.par,cex.main,cex.sub,seed,latent.control,colornames,latent,preserve.eattr
+#' @param nw,arglist,...,formula,object,x
 #' Arguments to deprecated functions.
 #'
 #' @keywords misc internal
