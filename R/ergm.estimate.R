ff704e48e27715a85a190a56728784a55ae7dcf9
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2024-01-22 22:37:06 +1100

    In ergm.estimate(), if the steplen < 1 and the unshrunken Hessian is not SNND, return NA in its place.
    fixes statnet/ergm#551

--- R/ergm.estimate.R
+++ R/ergm.estimate.R
@@ -337,9 +337,9 @@ ergm.estimate<-function(init, model, statsmatrices, statsmatrices.obs=NULL,
                         eta0=eta0, etamap=etamap.no
                         )
     }
-    
+
     covar <- matrix(NA, ncol=length(theta), nrow=length(theta))
-    covar[!model$etamap$offsettheta, !model$etamap$offsettheta] <- sginv(-Lout$hessian, tol=.Machine$double.eps^(3/4))
+    covar[!model$etamap$offsettheta, !model$etamap$offsettheta] <- ERRVL(try(sginv(-Lout$hessian, tol=.Machine$double.eps^(3/4)), TRUE), NA)
     dimnames(covar) <- list(names(theta),names(theta))
     He <- matrix(NA, ncol=length(theta), nrow=length(theta))
     He[!model$etamap$offsettheta, !model$etamap$offsettheta] <- Lout$hessian
