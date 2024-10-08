a57dd06010249198bdeb7a5855a341b8076a6943
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2024-10-08 17:38:30 +1100

    Updated parallel controls documentation template.

--- man-roxygen/control_MCMC_parallel.R
+++ man-roxygen/control_MCMC_parallel.R
@@ -8,13 +8,13 @@
 #  Copyright 2003-2024 Statnet Commons
 ################################################################################
 #' @param parallel Number of threads in which to run the sampling. Defaults to
-#' 0 (no parallelism). See the entry on [parallel processing][ergm-parallel]
-#' for details and troubleshooting.
-#' @param parallel.type API to use for parallel processing. Supported values
-#' are \code{"MPI"} and \code{"PSOCK"}. Defaults to using the \code{parallel}
-#' package with PSOCK clusters. See \code{\link{ergm-parallel}}
-#' @param parallel.version.check Logical: If TRUE, check that the version of
-#' \pkg{ergm} running on the slave nodes is the same as
-#' that running on the master node.
+#' 0 (no parallelism). See [`ergm-parallel`]
+#'   for details and troubleshooting.
+#' @param parallel.type API to use for parallel processing. Defaults
+#'   to using the \pkg{parallel} package with PSOCK clusters. See
+#'   [`ergm-parallel`].
+#' @param parallel.version.check Logical: If TRUE, check that the
+#'   version of \CRANpkg{ergm} running on the slave
+#'   nodes is the same as that running on the master node.
 #' @param parallel.inherit.MT Logical: If TRUE, slave nodes and
-#' processes inherit the [set.MT_terms()] setting.
+#'   processes inherit the [set.MT_terms()] setting.
