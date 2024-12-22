e04e0563bd3bc951c3223079068eaea8c52c75ce
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2024-12-23 00:08:11 +1100

    Fixed a typo exposed by a fix in statnet.common::trim_env() that affected initialization of constrained MH proposal when it requested auxiliaries.

--- R/ergm.R
+++ R/ergm.R
@@ -491,7 +491,7 @@ ergm <- function(formula, response=NULL,
   model.obs <- NULL
   if(identical(proposal$auxiliaries, proposal.obs$auxiliaries)){
     ## Reuse auxiliaries from the unconstrained proposal if identical.
-    proposal.obs$slots.extra.aux <- model$slots.extra.aux$proposal
+    proposal.obs$aux.slots <- model$slots.extra.aux$proposal
   }else if(!is.null(proposal.obs$auxiliaries)){
     if (verbose) message("Constrained proposal requires different auxiliaries: reinitializing model...")
     model.obs <- ergm_model(formula, nw, extra.aux = NVL3(proposal.obs$auxiliaries,list(proposal=.)), term.options=control$term.options)
