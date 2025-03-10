9f4154a77d3d887033f7fe6e3190b709954a19a4
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-03-10 14:25:09 +1100

    The _GET_OVERRIDE*() family of macros are now in their own header file (ergm_variadic_macros.h).

---
+++ inst/include/ergm_variadic_macros.h
@@ -0,0 +1,17 @@
+#ifndef _ERGM_VARIADIC_MACROS_
+#define _ERGM_VARIADIC_MACROS_
+
+#define _GET_OVERRIDE1(_1, NAME, ...) NAME
+#define _GET_OVERRIDE2(_1, _2, NAME, ...) NAME
+#define _GET_OVERRIDE3(_1, _2, _3, NAME, ...) NAME
+#define _GET_OVERRIDE4(_1, _2, _3, _4, NAME, ...) NAME
+#define _GET_OVERRIDE5(_1, _2, _3, _4, _5, NAME, ...) NAME
+#define _GET_OVERRIDE6(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
+#define _GET_OVERRIDE7(_1, _2, _3, _4, _5, _6, _7, NAME, ...) NAME
+#define _GET_OVERRIDE8(_1, _2, _3, _4, _5, _6, _7, _8, NAME, ...) NAME
+#define _GET_OVERRIDE9(_1, _2, _3, _4, _5, _6, _7, _8, _9, NAME, ...) NAME
+#define _GET_OVERRIDE10(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, NAME, ...) NAME
+#define _GET_OVERRIDE11(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, NAME, ...) NAME
+#define _GET_OVERRIDE12(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, NAME, ...) NAME
+
+#endif
