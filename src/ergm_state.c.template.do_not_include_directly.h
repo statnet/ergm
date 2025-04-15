e0dcc994c3502d9e7d5705b57c88e41008fd3bb4
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-04-15 11:04:06 +1000

    Templated out ergm_state.c and ergm_wtstate.c into ergm_state.c.template.do_not_include_directly.h .

--- src/ergm_state.c.template.do_not_include_directly.h
+++ src/ergm_state.c.template.do_not_include_directly.h
@@ -8,14 +8,13 @@
  *  Copyright 2003-2025 Statnet Commons
  */
 #include "ergm_kvec.h"
-#include "ergm_wtstate.h"
 #include "ergm_constants.h"
 
-static kvec_t(WtErgmState *) ergm_wtstate_array = kv_blank;
+static kvec_t(ETYPE(ErgmState) *) ETYPE(ergm_,state_array) = kv_blank;
 
-WtErgmState *WtErgmStateInit(SEXP stateR,
+ETYPE(ErgmState) *ETYPE(ErgmStateInit)(SEXP stateR,
                              unsigned int flags){
-  WtErgmState *s = R_Calloc(1, WtErgmState);
+  ETYPE(ErgmState) *s = R_Calloc(1, ETYPE(ErgmState));
 
   /* Save a reference to the corresponding R object */
   s->R = stateR;
@@ -25,27 +24,27 @@ WtErgmState *WtErgmStateInit(SEXP stateR,
   s->stats = length(tmp) ? REAL(tmp) : NULL;
 
   /* Form the network */
-  s->nwp=Redgelist2WtNetwork(getListElement(stateR,"el"), flags & ERGM_STATE_EMPTY_NET);
+  s->nwp=ETYPE(Redgelist2,Network)(getListElement(stateR,"el"), flags & ERGM_STATE_EMPTY_NET);
 
   /* Initialize the model */
   s->m=NULL;
   tmp = getListElement(stateR, "model");
   if(s->nwp && length(tmp)){ // Model also requires network.
     if(asInteger(getListElement(stateR, "ext.flag"))==ERGM_STATE_R_CHANGED) error("R ergm_state has changed in R but has not been reconciled.");
-    s->m = WtModelInitialize(tmp, getListElement(stateR, "ext.state"), s->nwp, flags & ERGM_STATE_NO_INIT_S);
+    s->m = ETYPE(ModelInitialize)(tmp, getListElement(stateR, "ext.state"), s->nwp, flags & ERGM_STATE_NO_INIT_S);
   }
 
   /* Initialize the M-H proposal */
   s->MHp=NULL;
   if(!(flags & ERGM_STATE_NO_INIT_PROP) && s->m && length(tmp = getListElement(stateR, "proposal"))) // Proposal also requires model's auxiliaries.
-    s->MHp = WtMHProposalInitialize(tmp, s->nwp, s->m->termarray->aux_storage);
+    s->MHp = ETYPE(MHProposalInitialize)(tmp, s->nwp, s->m->termarray->aux_storage);
 
-  kv_push(WtErgmState*, ergm_wtstate_array, s);
+  kv_push(ETYPE(ErgmState)*, ETYPE(ergm_,state_array), s);
 
   return s;
 }
 
-SEXP WtErgmStateRSave(WtErgmState *s){
+SEXP ETYPE(ErgmStateRSave)(ETYPE(ErgmState) *s){
   SEXP startR = s->R;
 
   // Duplicate state
@@ -55,13 +54,13 @@ SEXP WtErgmStateRSave(WtErgmState *s){
     SET_VECTOR_ELT(outl, i, VECTOR_ELT(startR, i));
 
   // Network state
-  if(s->nwp) setListElement(outl, "el", WtNetwork2Redgelist(s->nwp));
+  if(s->nwp) setListElement(outl, "el", ETYPE(Network2Redgelist)(s->nwp));
 
   // Extended state
   if(s->m){{ // To limit the scope of the variables.
       SEXP ext_l = PROTECT(allocVector(VECSXP, s->m->n_terms));
       unsigned int i=0;
-      WtEXEC_THROUGH_TERMS(s->m, {
+      ETYPE(EXEC_THROUGH_TERMS)(s->m, {
           if(mtp->w_func) SET_VECTOR_ELT(ext_l, i, mtp->w_func(mtp, s->nwp));
           i++;
         });
@@ -84,22 +83,22 @@ SEXP WtErgmStateRSave(WtErgmState *s){
   return outl;
 }
 
-void WtErgmStateDestroy(WtErgmState *s){
+void ETYPE(ErgmStateDestroy)(ETYPE(ErgmState) *s){
   // Find and clear the corresponding element in the active state
   // array. Note that most of the time, this will be the first element
   // in the array.
   unsigned int i=0;
-  while(kv_A(ergm_wtstate_array, i) != s) i++;
-  kv_del_plug(ergm_wtstate_array, i);
+  while(kv_A(ETYPE(ergm_,state_array), i) != s) i++;
+  kv_del_plug(ETYPE(ergm_,state_array), i);
 
-  if(s->MHp) WtMHProposalDestroy(s->MHp, s->nwp);
-  if(s->m) WtModelDestroy(s->nwp, s->m);
-  if(s->nwp) WtNetworkDestroy(s->nwp);
+  if(s->MHp) ETYPE(MHProposalDestroy)(s->MHp, s->nwp);
+  if(s->m) ETYPE(ModelDestroy)(s->nwp, s->m);
+  if(s->nwp) ETYPE(NetworkDestroy)(s->nwp);
   R_Free(s);
 }
 
-SEXP ErgmWtStateArrayClear(void){
-  while(kv_size(ergm_wtstate_array)) WtErgmStateDestroy(kv_A(ergm_wtstate_array, 0));
-  kv_destroy(ergm_wtstate_array);
+SEXP ETYPE(Ergm,StateArrayClear)(void){
+  while(kv_size(ETYPE(ergm_,state_array))) ETYPE(ErgmStateDestroy)(kv_A(ETYPE(ergm_,state_array), 0));
+  kv_destroy(ETYPE(ergm_,state_array));
   return R_NilValue;
 }
