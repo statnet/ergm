04baa72765d06bf6b6723927f167caf86483c972
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-05-28 12:27:22 +1000

    Simplified some of the code in c.network.list() and fixed formatting.

--- R/network.list.R
+++ R/network.list.R
@@ -130,30 +130,27 @@ c.network.list <- function(..., check_attr = TRUE) {
   dots <- list(...)
 
   # Merge network lists without attributes
-  lapply(dots, function(x) {
-    attributes(x) <- NULL
-    x
-  }) -> l_networks
-  rval <- do.call("c", l_networks)
+  rval <- do.call("c", map(dots, unclass))
 
   # Check attributes
-  if(check_attr) {
+  if (check_attr) {
     # Names of attributes to check with `all.equal()`
     attr_names <- c("coefficients", "control", "response",
                     "formula", "constraints", "reference")
-    for(an in attr_names) {
+    for (an in attr_names) {
       al <- map(dots, ~ attr(.x, an))
       ok <- all_identical(al, all.equal)
-      if(!ok) stop(paste0("network lists do not have equal values on attribute ", an))
+      if (!ok) stop("network lists do not have equal values on attribute ", an)
     }
 
     # Check if "stats" have identical columns
     l_stats <- map(dots, ~ attr(.x, "stats"))
     ok <- all_identical(
-      lapply(l_stats, function(x) colnames(x)),
+      map(l_stats, function(x) colnames(x)),
       .p = identical
     )
-    if(!ok) stop("network lists do not have identical columns of 'stats' attribute")
+    if (!ok) stop("network lists do not have identical columns of ",
+                  sQuote("stats"), " attribute")
   }
   # Return the list of networks with attributes merged or taken from the
   # first object
