3dd7628e968a0f5b240c89fe7898fa34ba81a2c5
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-06-12 21:31:15 +1000

    Implemented helper functions b1.size(), b2.size(), and {chr,int,dbl,lgl}_along(). The first two should be moved to 'network', the rest to 'statnet.common'.

--- R/ergm.utility.R
+++ R/ergm.utility.R
@@ -431,3 +431,13 @@ split_len <- function(x, l) {
   stopifnot(length(x) == sum(l))
   unname(split(x, factor(rep.int(seq_along(l), l), levels = seq_along(l))))
 }
+
+dbl_along <- function(x) numeric(length(x))
+int_along <- function(x) integer(length(x))
+chr_along <- function(x) character(length(x))
+lgl_along <- function(x) logical(length(x))
+
+## This should be in 'network'.
+
+b1.size <- function(x) if(is.bipartite(x)) x %n% "bipartite" else FALSE
+b2.size <- function(x) if(is.bipartite(x)) network.size(x) - b1.size(x) else FALSE
