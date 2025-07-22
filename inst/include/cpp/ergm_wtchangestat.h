9188302180671a0db8f98b98d1ce46cde4d81b5a
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-07-22 14:31:42 +1000

    Refactored ErgmCpp*ModelTerm classes into a template class, and mt.aux_storage[i] now works as hoped.

--- inst/include/cpp/ergm_wtchangestat.h
+++ inst/include/cpp/ergm_wtchangestat.h
@@ -1,6 +1,8 @@
+
 #pragma once
 
 #include "FixedArray.h"
+#include "ergm_modelterm_base.h"
 
 extern "C" {
 #include "../ergm_wtchangestat.h"
@@ -10,50 +12,15 @@ extern "C" {
   extern "C" void c_ ## name (Vertex tail, Vertex head, double weight, WtModelTerm *mtp, WtNetwork *nwp, double edgestate) { \
     ErgmCppWtNetwork nw(nwp);                                             \
     ErgmCppWtModelTerm<__VA_ARGS__> mt(mtp);                                           \
-    {impl;}                                                             \
+    impl;                                                             \
   }
 
 #define WtS_CHANGESTAT_CPP(name, impl, ...)                                    \
   extern "C" void s_ ## name (WtModelTerm *mtp, WtNetwork *nwp) { \
     ErgmCppWtNetwork nw(nwp);                                             \
     ErgmCppWtModelTerm<__VA_ARGS__> mt(mtp);                                           \
-    {impl;}                                                             \
+    impl;                                                             \
   }
-
 
 template<typename StorageType = void>
-class ErgmCppWtModelTerm {
-public:
-  ErgmCppWtModelTerm(WtModelTerm* mtp)
-    : mtp_(mtp),
-      stat(mtp->dstats, mtp->nstats),
-      dinput(mtp->inputparams, mtp->ninputparams),
-      iinput(mtp->iinputparams, mtp->niinputparams),
-      dattrib(mtp->attrib, (mtp->attrib && mtp->inputparams) ? (mtp->ninputparams - (mtp->attrib - mtp->inputparams)) : 0),
-      iattrib(mtp->iattrib, (mtp->iattrib && mtp->iinputparams) ? (mtp->niinputparams - (mtp->iattrib - mtp->iinputparams)) : 0),
-      storage(static_cast<StorageType*>(mtp->storage))
-  {}
-
-  FixedArray<double> stat;
-  const FixedArray<double> dinput;
-  const FixedArray<int> iinput;
-  const FixedArray<double> dattrib;
-  const FixedArray<int> iattrib;
-  StorageType* storage;
-
-  void set_storage(StorageType* ptr) {
-    storage = ptr;
-    mtp_->storage = static_cast<void*>(ptr);
-  }
-
-  // Encapsulate aux_storage access
-  void* aux_storage(size_t i) const {
-    return mtp_->aux_storage[mtp_->aux_slots[i]];
-  }
-  void* operator[](size_t i) const {
-    return aux_storage(i);
-  }
-
-private:
-  WtModelTerm* mtp_;
-};
+using ErgmCppWtModelTerm = ErgmCppModelTermBase<WtModelTerm, StorageType>;
