82671ccd8d6a866da7f09a781963c293b38ae8a1
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-12-22 14:31:44 +1100

    Updated copyright headers.

--- inst/include/ergm_MHproposal_changestat.h
+++ inst/include/ergm_MHproposal_changestat.h
@@ -1,3 +1,12 @@
+/*  File inst/include/ergm_MHproposal_changestat.h in package ergm, part of the
+ *  Statnet suite of packages for network analysis, https://statnet.org .
+ *
+ *  This software is distributed under the GPL-3 license.  It is free, open
+ *  source, and has the attribution requirements (GPL Section 7) at
+ *  https://statnet.org/attribution .
+ *
+ *  Copyright 2003-2025 Statnet Commons
+ */
 #ifndef _ERGM_MHPROPOSAL_CHANGESTAT_H_
 #define _ERGM_MHPROPOSAL_CHANGESTAT_H_
 
--- inst/include/inc/ergm_MHproposal_changestat.h.template.do_not_include_directly.h
+++ inst/include/inc/ergm_MHproposal_changestat.h.template.do_not_include_directly.h
@@ -1,3 +1,14 @@
+/*  File
+ *  inst/include/inc/ergm_MHproposal_changestat.h.template.do_not_include_direct
+ *  ly.h in package ergm, part of the Statnet suite of packages for network
+ *  analysis, https://statnet.org .
+ *
+ *  This software is distributed under the GPL-3 license.  It is free, open
+ *  source, and has the attribution requirements (GPL Section 7) at
+ *  https://statnet.org/attribution .
+ *
+ *  Copyright 2003-2025 Statnet Commons
+ */
 #include "ergm_Rutil.h"
 
 #ifdef __cplusplus
