59119e721188c655ce485c52507e9c14a0496051
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2026-02-17 14:31:39 +1100

    Simplified some of the {cli} progress bar C macros.

--- inst/include/ergm_cli.h
+++ inst/include/ergm_cli.h
@@ -17,21 +17,22 @@
 
 
 #define CLI_BAR_IF_VERBOSE(name, max, text)             \
-  SEXP name = PROTECT(VERBOSE_CLI_BAR(verbose) ?        \
-                      cli_progress_bar(max, NULL) :     \
-                      R_NilValue);                      \
+  SEXP name = NULL;                                     \
   if (VERBOSE_CLI_BAR(verbose)) {                       \
     VERBOSE_DECODE_CLI_BAR(verbose);                    \
+    name = PROTECT(cli_progress_bar(max, NULL));        \
     cli_progress_set_name(name, text);                  \
   }
 
 
 #define CLI_BAR_SET(name, value)                                        \
-  if (name != R_NilValue && CLI_SHOULD_TICK) cli_progress_set(name, value);
+  if (name && CLI_SHOULD_TICK) cli_progress_set(name, value);
 
 
 #define CLI_BAR_FINISH(name)                            \
-  if (name != R_NilValue) cli_progress_done(name);      \
-  UNPROTECT(1);
+  if (name) {                                           \
+    cli_progress_done(name);                            \
+    UNPROTECT(1);                                       \
+  }
 
 #endif // _ERGM_CLI_H_
