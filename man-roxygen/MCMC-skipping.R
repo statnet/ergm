aa8f57349a2a1b12073ff364a73f2fbf2844579d
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2026-04-13 16:32:00 +1000

    Grammar fix in documentation.

--- man-roxygen/MCMC-skipping.R
+++ man-roxygen/MCMC-skipping.R
@@ -16,10 +16,11 @@
 #' not, with offset coefficient set to \code{-\link{Inf}}
 #' (\code{+\link{Inf}}). Sometimes, however, a permitted configuration
 #' can be reached by "passing through" a forbidden one. For example,
-#' if isolates are possible but not nodes with degree exactly 1. Then,
-#' an offset term with coefficient [`NaN`] (not [`NA`]!) will cause
-#' the MCMC to not terminate as long as the value of that offset term
-#' is different from 0.
+#' the sample space may contain networks with isolates but not
+#' networks with nodes with degree exactly 1. Then, an offset term
+#' with coefficient [`NaN`] (not [`NA`]!) will cause the MCMC to not
+#' terminate as long as the value of that offset term is different
+#' from 0.
 #'
 #' Note that this means that MCMC is not guaranteed to terminate, and
 #' there are very few safeguards at this time.
