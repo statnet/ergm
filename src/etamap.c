e417275f47d448f78f57d664f836ed3f4262c550
handcock <handcock@ucla.edu>
2026-05-10 17:01:02 -0700

    Fix call to undeclared function 'SET_TYPEOF'
    Fix the call to undeclared function 'SET_TYPEOF'

--- src/etamap.c
+++ src/etamap.c
@@ -1,32 +1,53 @@
-/*  File src/etamap.c in package ergm, part of the
- *  Statnet suite of packages for network analysis, https://statnet.org .
+/*  File src/etamap.c in package ergm, part of the Statnet suite of packages
+ *  for network analysis, https://statnet.org .
  *
- *  This software is distributed under the GPL-3 license.  It is free,
- *  open source, and has the attribution requirements (GPL Section 7) at
+ *  This software is distributed under the GPL-3 license.  It is free, open
+ *  source, and has the attribution requirements (GPL Section 7) at
  *  https://statnet.org/attribution .
  *
- *  Copyright 2003-2022 Statnet Commons
+ *  Copyright 2003-2026 Statnet Commons
  */
 #include "ergm_etamap.h"
+#include <Rversion.h>
 
-#define SETUP_CALL(fun)                         \
-  SEXP cm = VECTOR_ELT(curved, i);              \
-  SEXP toR = getListElement(cm, "to");          \
-  unsigned int to = INTEGER(toR)[0];            \
-  unsigned int nto = length(toR);               \
-  SEXP fromR = getListElement(cm, "from");      \
-  unsigned int from = INTEGER(fromR)[0];        \
-  unsigned int nfrom = length(fromR);           \
-  SEXP cov = getListElement(cm, "cov");         \
-  SEXP fun = getListElement(cm, #fun);          \
-                                                \
+/* UINT_MAX's are there to segfault as soon as possible if empty from
+   and to vectors are actually used rather than return misleading
+   results. */
+#define SETUP_CALL(fun)                                                 \
+  SEXP cm = VECTOR_ELT(curved, i);                                      \
+  SEXP toR = getListElement(cm, "to");                                  \
+  unsigned int nto = length(toR);                                       \
+  unsigned int to = nto ? INTEGER(toR)[0] : UINT_MAX;                   \
+  SEXP fromR = getListElement(cm, "from");                              \
+  unsigned int nfrom = length(fromR);                                   \
+  unsigned int from = nfrom ? INTEGER(fromR)[0] : UINT_MAX;             \
+  SEXP cov = getListElement(cm, "cov");                                 \
+  SEXP fun = getListElement(cm, #fun);                                  \
+                                                                        \
   SEXP pos = call, arg;                                                 \
   SETCAR(pos, fun); pos = CDR(pos);                                     \
   SETCAR(pos, (arg = allocVector(REALSXP, nfrom))); pos = CDR(pos); /* Don't need to PROTECT the vector this way. */ \
-  memcpy(REAL(arg), theta1+from, nfrom*sizeof(double));                 \
+  if(nfrom) memcpy(REAL(arg), theta1+from, nfrom*sizeof(double));       \
   SETCAR(pos, ScalarInteger(nto)); pos = CDR(pos);                      \
   SETCAR(pos, cov);
 
+
+/*
+   A local implementation of allocLang(), recommended by Writing R
+   Extensions.
+
+   TODO: Delete in mid 2026.
+*/
+#if R_VERSION < R_Version(4, 4, 1)
+static inline SEXP allocLang(int n)
+{
+    if (n > 0)
+	  return LCONS(R_NilValue, allocList(n - 1));
+    else
+	  return R_NilValue;
+}
+#endif
+/* End local implementation of allocLang(). */
 
 SEXP ergm_eta_wrapper(SEXP thetaR, SEXP etamap){
   unsigned int neta = asInteger(getListElement(etamap, "etalength"));
@@ -56,9 +77,8 @@ void ergm_eta(double *theta, SEXP etamap, double *eta){
   unsigned int ncurved = length(curved);
 
   if(ncurved){
-    SEXP call = PROTECT(allocList(4));
-    SET_TYPEOF(call, LANGSXP);
-    
+    SEXP call = PROTECT(allocLang(4));
+
     for(unsigned int i = 0; i < ncurved; i++){
       SETUP_CALL(map);
       memcpy(eta1+to, REAL(eval(call, R_EmptyEnv)), nto*sizeof(double));
@@ -100,11 +120,11 @@ void ergm_etagrad(double *theta, SEXP etamap, double *etagrad){
   unsigned int ncurved = length(curved);
 
   if(ncurved){
-    SEXP call = PROTECT(allocList(4));
-    SET_TYPEOF(call, LANGSXP);
-    
+    SEXP call = PROTECT(allocLang(4));
+
     for(unsigned int i = 0; i < ncurved; i++){
       SETUP_CALL(gradient);
+      if(nfrom == 0) continue;
       double *g = REAL(eval(call, R_EmptyEnv));
       double *dest = etagrad1+from+to*ntheta;
       for(unsigned int j=0; j<nto; j++, dest+=ntheta, g+=nfrom)
@@ -158,11 +178,11 @@ void ergm_etagradmult(double *theta, double *v, unsigned int nv, SEXP etamap, do
   unsigned int ncurved = length(curved);
 
   if(ncurved){
-    SEXP call = PROTECT(allocList(4));
-    SET_TYPEOF(call, LANGSXP);
-    
+    SEXP call = PROTECT(allocLang(4));
+
     for(unsigned int i = 0; i < ncurved; i++){
       SETUP_CALL(gradient);
+      if(nfrom == 0) continue;
       double *g = REAL(eval(call, R_EmptyEnv));
       double *g1 = g - 1 - nfrom;
       for(unsigned int j=1; j<=nfrom; j++){
