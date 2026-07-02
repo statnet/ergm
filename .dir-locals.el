bdcc34b4c17dea54b039b1b988e6021c3b20cb5f
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2026-07-02 14:09:00 +1000

    Added some helpful configs for emacs.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((c++-mode . ((flycheck-gcc-include-path . ("/usr/share/R/include" "../inst/include"))))
+ (c-mode   . ((flycheck-gcc-include-path . ("/usr/share/R/include" "../inst/include"))))
+ (c++-mode . ((flycheck-clang-include-path . ("/usr/share/R/include" "../inst/include"))))
+ (c-mode   . ((flycheck-clang-include-path . ("/usr/share/R/include" "../inst/include")))))
